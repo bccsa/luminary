b96676a3c50ded97c7d43ec3f51f8b51a101ac14
Laurens Groeneveld <me@laurens.io>
2024-01-11 16:43:27 +0200

    Fix ports for app and CMS
    So we can rely on it for auth and testing purposes

--- cms/vite.config.ts
+++ cms/vite.config.ts
@@ -11,4 +11,8 @@ export default defineConfig({
             "@": fileURLToPath(new URL("./src", import.meta.url)),
         },
     },
+    server: {
+        port: 4175,
+        strictPort: true,
+    },
 });
