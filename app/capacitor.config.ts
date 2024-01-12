18509171ad76678f793f033573edea737b418d6a
Laurens Groeneveld <me@laurens.io>
2024-01-12 16:01:00 +0200

    Add Capacitor to app

---
+++ app/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'africa.activechristianity.app',
+  appName: 'ActiveChristianity',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
--- app/src/App.vue
+++ app/src/App.vue
@@ -3,11 +3,11 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-    <header class="flex items-center gap-8 p-8">
+    <header class="flex flex-col items-center space-x-8 p-8 md:flex-row">
         <img alt="logo" class="w-64" src="@/assets/logo.svg" />
 
         <div>
-            <nav class="flex gap-2">
+            <nav class="flex space-x-2">
                 <RouterLink to="/" class="underline">Home</RouterLink>
                 <RouterLink to="/about" class="underline">About</RouterLink>
             </nav>
