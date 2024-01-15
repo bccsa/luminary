f1ed20624d1823ec37357a63f8799293a20b79b3
Laurens Groeneveld <me@laurens.io>
2024-01-15 17:23:06 +0200

    Add auth to CMS
    Using the same Auth0 as the BCC Africa app for now

--- cms/src/main.ts
+++ cms/src/main.ts
@@ -3,6 +3,8 @@ import "./assets/main.css";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
+import { createAuth0 } from "@auth0/auth0-vue";
+
 import App from "./App.vue";
 import router from "./router";
 
@@ -10,5 +12,16 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+
+app.use(
+    createAuth0({
+        domain: import.meta.env.VITE_AUTH0_DOMAIN,
+        clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
+        authorizationParams: {
+            audience: import.meta.env.VITE_AUTH0_AUDIENCE,
+            redirect_uri: window.location.origin,
+        },
+    }),
+);
 
 app.mount("#app");
