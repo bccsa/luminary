adab72d5e3e6af082f23cfa9c25ab9ae41684623
Ivan Slabbert <ivanslabbert@users.noreply.github.com>
2024-01-19 17:11:03 +0200

    Add Card component

---
+++ cms/src/components/forms/AcCard.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{ title?: string }>();
+</script>
+
+<template>
+    <div class="overflow-hidden rounded-md border border-solid border-gray-200 bg-white shadow-sm">
+        <div v-if="title" class="border-b border-gray-200 bg-gray-50 px-4 py-5 sm:px-6">
+            <h3 class="text-base font-semibold leading-6 text-gray-900">Job Postings</h3>
+        </div>
+        <div class="px-4 py-5 sm:px-6">
+            <slot />
+        </div>
+    </div>
+</template>
