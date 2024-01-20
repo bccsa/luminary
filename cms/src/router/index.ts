32dcb81d3e932b5db92b79f5c3bbfdeea9697b20
Laurens Groeneveld <me@laurens.io>
2024-01-20 13:21:16 +0200

    Always scroll to top on route change except when using browser buttons
    https://router.vuejs.org/guide/advanced/scroll-behavior.html

--- cms/src/router/index.ts
+++ cms/src/router/index.ts
@@ -6,6 +6,13 @@ import { useGlobalConfigStore } from "@/stores/globalConfig";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
+    scrollBehavior(to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition;
+        } else {
+            return { top: 0 };
+        }
+    },
     routes: [
         {
             path: "/",
