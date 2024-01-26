40a5b2c0c2a03039005530501527b5ff449bb00e
Laurens Groeneveld <me@laurens.io>
2024-01-26 13:45:47 +0200

    hm

--- api/scripts/start-couchdb-in-ci.sh
+++ api/scripts/start-couchdb-in-ci.sh
@@ -1,28 +1,15 @@
 #!/bin/sh
-# Adapted from https://github.com/cobot/couchdb-action/blob/master/entrypoint.sh
 
 echo "Starting Docker..."
-sh -c "docker run -d -p 5984:5984 -p 5986:5986 --tmpfs /ram_disk couchdb:3.3.3"
-
-# CouchDB container name
-export NAME=`docker ps --format "{{.Names}}" --last 1`
-
-docker exec $NAME sh -c 'mkdir -p /opt/couchdb/etc/local.d && echo "[couchdb]\ndatabase_dir = /ram_disk\nview_index_dir = /ram_disk\ndelayed_commits = true\n[httpd]\nsocket_options = [{nodelay, true}]" >> /opt/couchdb/etc/local.d/01-github-action-custom.ini'
+sh -c "docker run -p 5984:5984 -e COUCHDB_USER=admin -e COUCHDB_PASSWORD=password -d couchdb:3.3.3"
 
 wait_for_couchdb() {
   echo "Waiting for CouchDB..."
-  hostip=$(ip route show | awk '/default/ {print $3}')
 
-  while ! curl -f http://$hostip:5984/ &> /dev/null
+  while ! curl -f http://127.0.0.1:5984/ &> /dev/null
   do
     echo "."
     sleep 1
   done
 }
 wait_for_couchdb
-
-# Set up system databases
-echo "Setting up CouchDB system databases..."
-docker exec $NAME curl -sS 'http://127.0.0.1:5984/_users' -X PUT -H 'Content-Type: application/json' --data '{"id":"_users","name":"_users"}' > /dev/null
-docker exec $NAME curl -sS 'http://127.0.0.1:5984/_global_changes' -X PUT -H 'Content-Type: application/json' --data '{"id":"_global_changes","name":"_global_changes"}' > /dev/null
-docker exec $NAME curl -sS 'http://127.0.0.1:5984/_replicator' -X PUT -H 'Content-Type: application/json' --data '{"id":"_replicator","name":"_replicator"}' > /dev/null
