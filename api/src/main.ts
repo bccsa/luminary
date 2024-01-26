3f351001f7f9110c24a206fa763c5edeb4b00320
Ivan Slabbert <ivanslabbert@users.noreply.github.com>
2024-01-26 10:13:37 +0200

    Create database seeding and view creating functions and documents (#16)

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { upsertDesignDocs, upsertSeedingDocs } from "./db/db.seedingFunctions";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -12,5 +13,15 @@ async function bootstrap() {
         ],
     });
     await app.listen(process.env.PORT);
+
+    // Create or update database design docs on api startup
+    upsertDesignDocs().catch((err) => {
+        console.error(err.message);
+    });
+
+    // TMP: Seed database with demo / initial data
+    upsertSeedingDocs().catch((err) => {
+        console.error(err.message);
+    });
 }
 bootstrap();
