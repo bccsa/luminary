0256da557388b32be20666e22cb86d47bdbda38e
Laurens Groeneveld <me@laurens.io>
2024-02-02 11:12:05 +0200

    Setup store

--- cms/src/stores/post.ts
+++ cms/src/stores/post.ts
@@ -1,40 +1,38 @@
 import { defineStore } from "pinia";
 import type { Post, Content, PostDto } from "@/types";
 import { db } from "@/db";
+import { ref, type Ref } from "vue";
 
-export const usePostStore = defineStore("post", {
-    state: () => ({
-        posts: [] as Post[],
-    }),
+export const usePostStore = defineStore("post", () => {
+    const posts: Ref<Post[]> = ref([]);
 
-    actions: {
-        async savePosts(data: PostDto[]) {
-            await db.posts.bulkPut(data);
+    async function savePosts(data: PostDto[]) {
+        await db.posts.bulkPut(data);
 
-            return this.updatePosts(data);
-        },
+        return updatePosts(data);
+    }
 
-        async updatePosts(data?: PostDto[]) {
-            if (!data) {
-                data = await db.posts.toArray();
+    async function updatePosts(data?: PostDto[]) {
+        if (!data) {
+            data = await db.posts.toArray();
+        }
+
+        const content = await db.content.toArray();
+
+        posts.value = data.map((dto) => {
+            const post = dto as unknown as Post;
+
+            if (dto.content) {
+                post.content = dto.content.map(
+                    (contentId) => content.find((c) => c._id == contentId) as unknown as Content,
+                );
+
+                post.defaultTitle = post.content[0].title;
             }
 
-            const content = await db.content.toArray();
+            return post;
+        });
+    }
 
-            this.posts = data.map((dto) => {
-                const post = dto as unknown as Post;
-
-                if (dto.content) {
-                    post.content = dto.content.map(
-                        (contentId) =>
-                            content.find((c) => c._id == contentId) as unknown as Content,
-                    );
-
-                    post.defaultTitle = post.content[0].title;
-                }
-
-                return post;
-            });
-        },
-    },
+    return { posts, savePosts, updatePosts };
 });
