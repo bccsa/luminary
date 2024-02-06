ab2bf4c2feea68f1d6f0fe96e211329331f59656
Laurens Groeneveld <me@laurens.io>
2024-02-06 14:14:16 +0200

    Simplify test

--- cms/src/components/OnlineIndicator.spec.ts
+++ cms/src/components/OnlineIndicator.spec.ts
@@ -7,17 +7,16 @@ import { createTestingPinia } from "@pinia/testing";
 describe("OnlineIndicator", () => {
     it("renders the online status", async () => {
         const store = useSocketConnectionStore(createTestingPinia());
-        store.isConnected = true;
 
         const wrapper = mount(OnlineIndicator);
 
-        expect(wrapper.html()).toContain("Online");
-        expect(wrapper.html()).not.toContain("Offline");
-
-        store.isConnected = false;
-        await wrapper.vm.$nextTick();
-
         expect(wrapper.html()).not.toContain("Online");
         expect(wrapper.html()).toContain("Offline");
+
+        store.isConnected = true;
+        await wrapper.vm.$nextTick();
+
+        expect(wrapper.html()).toContain("Online");
+        expect(wrapper.html()).not.toContain("Offline");
     });
 });
