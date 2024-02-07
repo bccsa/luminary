937ba5efb3d7f5b18a943ca48c51b964bbc6c682
Laurens Groeneveld <me@laurens.io>
2024-02-07 17:29:18 +0200

    Add test for language badge

--- cms/src/pages/posts/PostOverview.spec.ts
+++ cms/src/pages/posts/PostOverview.spec.ts
@@ -3,8 +3,10 @@ import { mount } from "@vue/test-utils";
 import { createTestingPinia } from "@pinia/testing";
 import PostOverview from "./PostOverview.vue";
 import { usePostStore } from "@/stores/post";
-import { ContentStatus, DocType } from "@/types";
 import EmptyState from "@/components/EmptyState.vue";
+import { mockLanguage, mockPost } from "@/tests/mockData";
+import AcBadge from "@/components/common/AcBadge.vue";
+import { useLanguageStore } from "@/stores/language";
 
 vi.mock("vue-router", () => ({
     resolve: vi.fn(),
@@ -18,39 +20,11 @@ describe("PostOverview", () => {
 
     it("displays posts from the store", async () => {
         const pinia = createTestingPinia();
-        const store = usePostStore();
+        const postStore = usePostStore();
+        const languageStore = useLanguageStore();
 
-        store.posts = [
-            {
-                type: DocType.Post,
-                _id: "post-post1",
-                updatedTimeUtc: 3,
-                memberOf: [],
-                content: [
-                    {
-                        _id: "content-post1-eng",
-                        type: DocType.Content,
-                        updatedTimeUtc: 3,
-                        memberOf: [],
-                        language: {
-                            _id: "lang-eng",
-                            type: DocType.Language,
-                            updatedTimeUtc: 3,
-                            memberOf: [],
-                            languageCode: "eng",
-                            name: "English",
-                        },
-                        status: ContentStatus.Published,
-                        slug: "post1-eng",
-                        title: "English translation title",
-                        summary: "This is an example post",
-                        text: "In the quiet town of Willowdale, little Lily wept as her beloved cat, Whiskers, went missing. Frantically searching the neighborhood, she stumbled upon Fireman Jake, known for his kind heart. With a reassuring smile, he promised to help. Lily clung to hope as they combed the streets together. Beneath a dusty porch, they found Whiskers, scared but unharmed. Grateful tears filled Lily's eyes as Fireman Jake handed her the rescued feline. Their small town echoed with cheers as Lily hugged her furry friend, and from that day forward, Fireman Jake became a hero in her heart and the community's beloved guardian.",
-                        publishDate: 3,
-                    },
-                ],
-                tags: [],
-            },
-        ];
+        postStore.posts = [mockPost];
+        languageStore.languages = [mockLanguage];
 
         const wrapper = mount(PostOverview, {
             global: {
@@ -59,6 +33,10 @@ describe("PostOverview", () => {
         });
 
         expect(wrapper.html()).toContain("English translation title");
+
+        // Assert there is a badge that indicates a published translation
+        const badge = await wrapper.findComponent(AcBadge);
+        expect(badge.props().variant).toBe("success");
     });
 
     it("displays an empty state if there are no posts", async () => {
