f2c6730927827fa0cc127e6b796ba02afb1d3ca0
Laurens Groeneveld <me@laurens.io>
2024-02-07 15:59:23 +0200

    Remove unused code

--- cms/tailwind.config.ts
+++ cms/tailwind.config.ts
@@ -8,15 +8,6 @@ export default {
             fontFamily: {
                 sans: ["Inter var", ...defaultTheme.fontFamily.sans],
             },
-            animation: {
-                "fade-in": "fade-in 0.3s",
-            },
-            keyframes: {
-                "fade-in": {
-                    "0%": { opacity: "0" },
-                    "100%": { opacity: "1" },
-                },
-            },
         },
     },
     plugins: [require("@tailwindcss/forms")],
