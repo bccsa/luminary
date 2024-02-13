4b108fc90af59f6ef864133adbf270fbc5fc59f7
Laurens Groeneveld <me@laurens.io>
2024-02-13 14:35:10 +0200

    Add icon support to Tabs

--- cms/src/components/common/AcTabs.vue
+++ cms/src/components/common/AcTabs.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { useId } from "@/hooks/use-id";
+import type { Component } from "vue";
 
 type Tab = {
     title: string;
     key: string;
+    icon?: Component | Function | string;
 };
 
 type Props = {
@@ -48,14 +50,20 @@ const id = useId().toString();
                             tab.key == currentTab
                                 ? 'border-gray-500 text-gray-950'
                                 : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
-                            'cursor-pointer whitespace-nowrap border-b-2 px-1 py-4 text-sm font-medium',
+                            'flex cursor-pointer items-center gap-2 whitespace-nowrap border-b-2 px-1 py-4 text-sm font-medium',
                         ]"
                         :aria-current="tab.key == currentTab ? 'page' : undefined"
                     >
+                        <component
+                            :is="tab.icon"
+                            v-if="tab.icon"
+                            class="h-4 w-4"
+                            :class="[tab.key == currentTab ? 'text-gray-800' : 'text-gray-400']"
+                        />
                         {{ tab.title }}
                     </span>
                 </nav>
-                <div class="mt-3 sm:mt-0" v-show="$slots.default"><slot /></div>
+                <div class="mt-3 sm:mt-0" v-if="$slots.default"><slot /></div>
             </div>
         </div>
         <div>
