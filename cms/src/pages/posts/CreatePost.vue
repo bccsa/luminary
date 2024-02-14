493b41199f7d6833d1523fc67b6104fc7ace2805
Laurens Groeneveld <me@laurens.io>
2024-02-14 11:10:31 +0200

    Add transition to language select

--- cms/src/pages/posts/CreatePost.vue
+++ cms/src/pages/posts/CreatePost.vue
@@ -62,53 +62,67 @@ async function save() {
 
                     <AcInput label="Permissions" placeholder="Not implemented yet" disabled />
 
-                    <div class="space-y-2" v-if="!chosenLanguage">
-                        <FormLabel>Select language for first translation</FormLabel>
-                        <div class="rounded-lg border-2 border-gray-100">
-                            <button
-                                v-for="language in languageStore.languages"
-                                :key="language.languageCode"
-                                class="group flex w-full items-center justify-between gap-4 px-4 py-3 text-left text-sm text-gray-950 hover:bg-gray-50 active:bg-gray-100"
-                                @click="chooseLanguage(language)"
-                            >
-                                <div class="flex items-center gap-2">
-                                    <AcBadge type="language" no-icon>
-                                        {{ language.languageCode }}
-                                    </AcBadge>
-                                    {{ language.name }}
-                                </div>
-                                <div class="hidden text-xs text-gray-600 sm:group-hover:block">
-                                    Select
-                                </div>
-                            </button>
+                    <transition
+                        enter-active-class="transition ease-out duration-150"
+                        enter-from-class="transform opacity-0 -translate-y-2"
+                        enter-to-class="transform opacity-100 translate-y-0"
+                        leave-active-class="hidden"
+                    >
+                        <div class="space-y-2" v-if="!chosenLanguage">
+                            <FormLabel>Select language for first translation</FormLabel>
+                            <div class="rounded-lg border-2 border-gray-100">
+                                <button
+                                    v-for="language in languageStore.languages"
+                                    :key="language.languageCode"
+                                    class="group flex w-full items-center justify-between gap-4 px-4 py-3 text-left text-sm text-gray-950 hover:bg-gray-50 active:bg-gray-100"
+                                    @click="chooseLanguage(language)"
+                                >
+                                    <div class="flex items-center gap-2">
+                                        <AcBadge type="language" no-icon>
+                                            {{ language.languageCode }}
+                                        </AcBadge>
+                                        {{ language.name }}
+                                    </div>
+                                    <div class="hidden text-xs text-gray-600 sm:group-hover:block">
+                                        Select
+                                    </div>
+                                </button>
+                            </div>
                         </div>
-                    </div>
+                    </transition>
 
-                    <div v-if="chosenLanguage" class="space-y-6">
-                        <AcInput
-                            v-model="title"
-                            label="Title"
-                            :placeholder="chosenLanguage.name"
-                            required
-                        />
+                    <transition
+                        enter-active-class="transition ease-out duration-150"
+                        enter-from-class="transform opacity-0 translate-y-2"
+                        enter-to-class="transform opacity-100 translate-y-0"
+                        leave-active-class="hidden"
+                    >
+                        <div v-if="chosenLanguage" class="space-y-6">
+                            <AcInput
+                                v-model="title"
+                                label="Title"
+                                :placeholder="chosenLanguage.name"
+                                required
+                            />
 
-                        <div class="flex flex-col gap-4 sm:flex-row sm:justify-between">
-                            <button
-                                class="text-xs text-gray-600 hover:text-gray-800"
-                                @click="resetLanguage"
-                            >
-                                Select different language
-                            </button>
-                            <AcButton
-                                variant="primary"
-                                :icon="ArrowRightIcon"
-                                icon-right
-                                @click="save"
-                            >
-                                Save as draft & continue
-                            </AcButton>
+                            <div class="flex flex-col gap-4 sm:flex-row sm:justify-between">
+                                <button
+                                    class="text-xs text-gray-600 hover:text-gray-800"
+                                    @click="resetLanguage"
+                                >
+                                    Select different language
+                                </button>
+                                <AcButton
+                                    variant="primary"
+                                    :icon="ArrowRightIcon"
+                                    icon-right
+                                    @click="save"
+                                >
+                                    Save as draft & continue
+                                </AcButton>
+                            </div>
                         </div>
-                    </div>
+                    </transition>
                 </div>
             </AcCard>
         </div>
