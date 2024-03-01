3abf8f9c27168c7360761fc35e76fea5d816164d
Laurens Groeneveld <me@laurens.io>
2024-03-01 14:34:42 +0200

    Undo change to protected

--- cms/src/db/repositories/baseRepository.ts
+++ cms/src/db/repositories/baseRepository.ts
@@ -3,19 +3,19 @@ import { db } from "../baseDatabase";
 import { DateTime } from "luxon";
 
 export class BaseRepository {
-    protected whereType(docType: DocType) {
+    whereType(docType: DocType) {
         return db.docs.where("type").equals(docType);
     }
 
-    protected whereId(id: Uuid) {
+    whereId(id: Uuid) {
         return db.docs.where("_id").equals(id);
     }
 
-    protected whereIds(ids: Uuid[]) {
+    whereIds(ids: Uuid[]) {
         return db.docs.where("_id").anyOf(ids);
     }
 
-    protected whereParentId(parentId: Uuid) {
+    whereParentId(parentId: Uuid) {
         return db.docs.where("parentId").equals(parentId);
     }
 
