02fe0e61fa610d8c369d931cb6116c3ea15905fb
Laurens Groeneveld <me@laurens.io>
2024-03-06 11:28:33 +0200

    Add custom language selector

--- cms/src/pages/posts/EditPost.vue
+++ cms/src/pages/posts/EditPost.vue
@@ -1,17 +1,14 @@
 <script setup lang="ts">
 import BasePage from "@/components/BasePage.vue";
 import ContentForm from "@/components/content/ContentForm.vue";
-import LSelect from "@/components/forms/LSelect.vue";
-import { useLanguageStore } from "@/stores/language";
+import LanguageSelector from "@/components/content/LanguageSelector.vue";
 import { usePostStore } from "@/stores/post";
-import { storeToRefs } from "pinia";
 import { computed, ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
 
 const route = useRoute();
 const router = useRouter();
 const postStore = usePostStore();
-const { languages } = storeToRefs(useLanguageStore());
 
 const postId = route.params.postId as string;
 const language = route.params.language as string;
@@ -22,20 +19,6 @@ const content = computed(() => {
     return post.value?.content.find((c) => c.language.languageCode == selectedLanguage.value);
 });
 
-const languageOptions = computed(() => {
-    if (!post.value) {
-        return [];
-    }
-
-    const languages = post.value.content.map((c) => c.language);
-
-    return languages.map((language) => {
-        return {
-            label: language.name,
-            value: language.languageCode,
-        };
-    });
-});
 const selectedLanguage = ref("eng");
 
 if (language) {
@@ -48,7 +31,7 @@ if (language) {
 <template>
     <BasePage :title="content?.title" :loading="isLoading">
         <template #actions>
-            <LSelect :options="languageOptions" v-model="selectedLanguage" />
+            <LanguageSelector :post="post" v-model="selectedLanguage" />
         </template>
 
         <ContentForm :post="post" :content="content" v-if="content" @save="postStore.updatePost" />
