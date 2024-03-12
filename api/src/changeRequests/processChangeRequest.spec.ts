2a8f0a753fefbc98e06efbf1dd4bb941ca0891bb
Ivan Slabbert <ivanslabbert@users.noreply.github.com>
2024-03-12 16:57:01 +0200

    Validate and automatically rectify content document's slug in API (#62)

--- api/src/changeRequests/processChangeRequest.spec.ts
+++ api/src/changeRequests/processChangeRequest.spec.ts
@@ -4,6 +4,7 @@ import { createTestingModule } from "../test/testingModule";
 import { processChangeRequest } from "./processChangeRequest";
 import { isDeepStrictEqual } from "util";
 import { PermissionSystem } from "../permissions/permissions.service";
+import { changeRequest_content } from "../test/changeRequestDocuments";
 
 describe("processChangeRequest", () => {
     let db: DbService;
@@ -117,5 +118,66 @@ describe("processChangeRequest", () => {
         );
         expect(processResult.message).toBe("Document is identical to the one in the database");
         expect(processResult.changes).toBeUndefined();
+    });
+
+    it("can validate a unique slug for a content document that does not exists", async () => {
+        const changeRequest = changeRequest_content();
+        changeRequest.doc.parentId = "post-post1";
+        changeRequest.doc._id = "test-slug-1";
+        changeRequest.doc.slug = "this-is-a-test-slug";
+
+        const res = await processChangeRequest("", changeRequest, ["group-super-admins"], db);
+        const dbDoc = await db.getDoc(changeRequest.doc._id);
+
+        expect(res.ok).toBe(true);
+        expect(dbDoc.docs[0].slug).toBe("this-is-a-test-slug");
+    });
+
+    it("can validate a unique slug for a content document that exists", async () => {
+        const changeRequest = changeRequest_content();
+        changeRequest.doc.parentId = "post-post1";
+        changeRequest.doc._id = "test-slug-1";
+        changeRequest.doc.slug = "this-is-a-test-slug";
+
+        await processChangeRequest("", changeRequest, ["group-super-admins"], db); // ensure that the slug is already in use
+        const res = await processChangeRequest("", changeRequest, ["group-super-admins"], db);
+        const dbDoc = await db.getDoc(changeRequest.doc._id);
+
+        expect(res.ok).toBe(true);
+        expect(dbDoc.docs[0].slug).toBe("this-is-a-test-slug");
+    });
+
+    it("can rectify a non-unique slug by appending a random number to the end of the slug", async () => {
+        // ensure that the slug is already in use
+        const changeRequest1 = changeRequest_content();
+        changeRequest1.doc.parentId = "post-post1";
+        changeRequest1.doc._id = "test-slug-1";
+        changeRequest1.doc.slug = "this-is-a-test-slug";
+        await processChangeRequest("", changeRequest1, ["group-super-admins"], db);
+
+        // Create a new change request with the same slug
+        const changeRequest2 = changeRequest_content();
+        changeRequest2.doc.parentId = "post-post1";
+        changeRequest2.doc._id = "test-slug-2";
+        changeRequest2.doc.slug = "this-is-a-test-slug";
+
+        const res = await processChangeRequest("", changeRequest2, ["group-super-admins"], db);
+        const dbDoc = await db.getDoc(changeRequest2.doc._id);
+
+        expect(res.ok).toBe(true);
+        expect(dbDoc.docs[0].slug).toMatch(/this-is-a-test-slug-[0-9](0-9)*/);
+    });
+
+    it("can automatically rectify a non-valid slug", async () => {
+        const changeRequest = changeRequest_content();
+        changeRequest.doc.parentId = "post-post1";
+        changeRequest.doc._id = "test-slug-1";
+        changeRequest.doc.slug = 'Invalid Slug! 123 無效的 Bør ikke være tilladt "#$%&/()=?`';
+
+        const res = await processChangeRequest("", changeRequest, ["group-super-admins"], db);
+        const dbDoc = await db.getDoc(changeRequest.doc._id);
+
+        expect(res.ok).toBe(true);
+        expect(dbDoc.docs[0].slug).toBe("invalid-slug-123-wu-xiao-de-bor-ikke-vaere-tilladt");
     });
 });
