40759689e472461357ab5ad7702727c1446bd332
Laurens Groeneveld <me@laurens.io>
2024-03-15 10:31:23 +0200

    Fix test

--- cms/src/pages/SettingsPage.spec.ts
+++ cms/src/pages/SettingsPage.spec.ts
@@ -1,6 +1,9 @@
-import { afterEach, describe, expect, it, vi } from "vitest";
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 import SettingsPage from "./SettingsPage.vue";
-import { mount } from "@vue/test-utils";
+import { flushPromises, mount } from "@vue/test-utils";
+import { setActivePinia } from "pinia";
+import { createTestingPinia } from "@pinia/testing";
+import { useSocketConnectionStore } from "@/stores/socketConnection";
 
 const purgeMock = vi.hoisted(() => vi.fn());
 
@@ -14,13 +17,25 @@ const reloadMock = vi.hoisted(() => vi.fn());
 window.location = { reload: reloadMock };
 
 describe("purgeLocalDatabase", () => {
+    beforeEach(() => {
+        setActivePinia(createTestingPinia());
+    });
+
     afterEach(() => {
         vi.clearAllMocks();
     });
 
-    it("purges the local database", async () => {
+    it("purges the local database when connected", async () => {
+        const socketConnectionStore = useSocketConnectionStore();
         const wrapper = mount(SettingsPage);
 
+        await wrapper.find("button[data-test='deleteLocalDatabase']").trigger("click");
+
+        expect(purgeMock).not.toHaveBeenCalled();
+
+        socketConnectionStore.isConnected = true;
+
+        await wrapper.vm.$nextTick();
         await wrapper.find("button[data-test='deleteLocalDatabase']").trigger("click");
 
         expect(purgeMock).toHaveBeenCalledOnce();
