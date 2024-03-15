ded7772630506c29c25e9e121656dd2c4e7babf5
Laurens Groeneveld <me@laurens.io>
2024-03-15 12:00:58 +0200

    Add basic auth to app

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -1,23 +1,45 @@
 import { createRouter, createWebHistory } from "vue-router";
+import { authGuard } from "@auth0/auth0-vue";
+import { nextTick } from "vue";
+import { useGlobalConfigStore } from "@/stores/globalConfig";
 import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
+    scrollBehavior(to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition;
+        } else {
+            return { top: 0 };
+        }
+    },
     routes: [
         {
             path: "/",
             name: "home",
             component: HomeView,
+            meta: {
+                title: "Home",
+            },
         },
         {
+            beforeEnter: authGuard,
             path: "/about",
             name: "about",
-            // route level code-splitting
-            // this generates a separate chunk (About.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
             component: () => import("../views/AboutView.vue"),
+            meta: {
+                title: "About",
+            },
         },
     ],
+});
+
+router.afterEach((to) => {
+    const { appName } = useGlobalConfigStore();
+
+    nextTick(() => {
+        document.title = to.meta.title ? `${to.meta.title} - ${appName}` : appName;
+    });
 });
 
 export default router;
