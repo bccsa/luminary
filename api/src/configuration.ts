6dfd66a80e6ffad0c87d286508ee784dd9590b57
Ivan Slabbert <ivanslabbert@users.noreply.github.com>
2024-03-15 12:37:10 +0200

    Add type for configuration function, and add permissionMap property

--- api/src/configuration.ts
+++ api/src/configuration.ts
@@ -12,16 +12,25 @@ export type SyncConfig = {
     tolerance: number;
 };
 
-export default () => ({
-    auth: {
-        jwtSecret: process.env.JWT_SECRET,
-    } as AuthConfig,
-    database: {
-        connectionString: process.env.DB_CONNECTION_STRING,
-        database: process.env.DB_DATABASE,
-        maxSockets: parseInt(process.env.DB_MAX_SOCKETS, 10) || 512,
-    } as DatabaseConfig,
-    sync: {
-        tolerance: parseInt(process.env.PORT, 10) || 1000,
-    } as SyncConfig,
-});
+export type Configuration = {
+    permissionMap: any;
+    auth?: AuthConfig;
+    database?: DatabaseConfig;
+    sync?: SyncConfig;
+};
+
+export default () =>
+    ({
+        auth: {
+            jwtSecret: process.env.JWT_SECRET,
+        } as AuthConfig,
+        database: {
+            connectionString: process.env.DB_CONNECTION_STRING,
+            database: process.env.DB_DATABASE,
+            maxSockets: parseInt(process.env.DB_MAX_SOCKETS, 10) || 512,
+        } as DatabaseConfig,
+        sync: {
+            tolerance: parseInt(process.env.PORT, 10) || 1000,
+        } as SyncConfig,
+        permissionMap: process.env.PERMISSION_MAP,
+    }) as Configuration;
