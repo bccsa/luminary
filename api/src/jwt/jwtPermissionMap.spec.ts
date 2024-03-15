1038b8fbab0cefb9722a1d7ce30a103ab8b435c9
Ivan Slabbert <ivanslabbert@users.noreply.github.com>
2024-03-15 12:40:30 +0200

    Add JWT based authentication via socket.io in API

---
+++ api/src/jwt/jwtPermissionMap.spec.ts
@@ -0,0 +1,24 @@
+import configuration, { Configuration } from "../configuration";
+import { getJwtPermission, parsePermissionMap } from "./jwtPermissionMap";
+
+describe("jwtPremissionMap", () => {
+    let config: Configuration;
+    beforeAll(() => {
+        // This test relies on the .env.test file configured as per .env.test.example
+        config = configuration();
+    });
+
+    it("can parse a jwt permission map", async () => {
+        const parsed = parsePermissionMap(config.permissionMap);
+
+        expect(parsed.jwt["groups"]["group-super-admins"]("any jwt data")).toBe(true);
+    });
+
+    it("can evaluate a permission map", async () => {
+        const parsed = parsePermissionMap(config.permissionMap);
+        const evaluated = getJwtPermission("any jwt data", parsed);
+
+        expect(evaluated.groups).toContain("group-super-admins");
+        expect(evaluated.userId).toBe("user-super-admin");
+    });
+});
