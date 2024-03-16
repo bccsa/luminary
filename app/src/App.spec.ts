6bed9234dc489eed40e94a411cd3ff19b05846ed
Laurens Groeneveld <me@laurens.io>
2024-03-16 10:02:57 +0200

    Add App tests

---
+++ app/src/App.spec.ts
@@ -0,0 +1,30 @@
+import { describe, it, expect, vi, afterEach, beforeEach } from "vitest";
+import { mount } from "@vue/test-utils";
+import App from "./App.vue";
+import * as auth0 from "@auth0/auth0-vue";
+import { ref } from "vue";
+import { createTestingPinia } from "@pinia/testing";
+import LoadingSpinner from "./components/LoadingSpinner.vue";
+import { setActivePinia } from "pinia";
+
+vi.mock("@auth0/auth0-vue");
+
+describe("App", () => {
+    beforeEach(() => {
+        setActivePinia(createTestingPinia());
+    });
+
+    afterEach(() => {
+        vi.clearAllMocks();
+    });
+
+    it("renders a loading spinner when not authenticated", () => {
+        (auth0 as any).useAuth0 = vi.fn().mockReturnValue({
+            isAuthenticated: ref(false),
+        });
+
+        const wrapper = mount(App);
+
+        expect(wrapper.findComponent(LoadingSpinner).exists()).toBe(true);
+    });
+});
