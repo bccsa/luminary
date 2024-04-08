63c3810b589c5ed31ccd19a29580fcc3d92bdea9
Ivan Slabbert <ivanslabbert@users.noreply.github.com>
2024-04-08 16:10:05 +0200

    Add test for HorizontalScrollableTagViewer

---
+++ app/src/components/tags/HorizontalScrollableTagViewer.spec.ts
@@ -0,0 +1,30 @@
+import { describe, it, expect, vi, afterEach, beforeEach } from "vitest";
+import { mount } from "@vue/test-utils";
+import HorizontalScrollableTagViewer from "./HorizontalScrollableTagViewer.vue";
+import { setActivePinia, createPinia } from "pinia";
+import { usePostStore } from "@/stores/post";
+import { mockCategory, mockPost } from "@/tests/mockData";
+import { useTagStore } from "@/stores/tag";
+
+describe("HorizontalScrollableTagViewer", () => {
+    beforeEach(() => {
+        setActivePinia(createPinia());
+    });
+
+    afterEach(() => {
+        vi.clearAllMocks();
+    });
+
+    it("displays the categories", async () => {
+        const postStore = usePostStore();
+        const tagStore = useTagStore();
+        postStore.posts = [mockPost];
+        tagStore.tags = [mockCategory];
+
+        const wrapper = mount(HorizontalScrollableTagViewer, {
+            props: { tag: mockCategory },
+        });
+
+        expect(wrapper.html()).toContain(mockCategory.content[0].title);
+    });
+});
