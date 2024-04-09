61ff02eaf999d5a3f5e9ab249e91076fcb010100
Laurens Groeneveld <me@laurens.io>
2024-04-09 12:31:39 +0200

    Fix SinglePost tests

--- app/src/pages/SinglePost.spec.ts
+++ app/src/pages/SinglePost.spec.ts
@@ -3,7 +3,7 @@ import { mount } from "@vue/test-utils";
 import SinglePost from "./SinglePost.vue";
 import { setActivePinia, createPinia } from "pinia";
 import { usePostStore } from "@/stores/post";
-import { mockPost } from "@/tests/mockData";
+import { mockEnglishContent, mockPost } from "@/tests/mockData";
 
 vi.mock("vue-router", () => ({
     resolve: vi.fn(),
@@ -26,13 +26,39 @@ describe("SinglePost", () => {
         vi.clearAllMocks();
     });
 
-    it("displays the post", async () => {
+    it("displays a video post", async () => {
         const postStore = usePostStore();
         postStore.posts = [mockPost];
 
-        const wrapper = mount(SinglePost);
+        const wrapper = mount(SinglePost, {
+            shallow: true,
+        });
 
         expect(wrapper.html()).toContain(mockPost.content[0].title);
         expect(wrapper.html()).toContain(mockPost.tags[0].content[0].title);
+        expect(wrapper.html()).toContain("video-player-stub");
+    });
+
+    it("displays a text post", async () => {
+        const postStore = usePostStore();
+        postStore.posts = [
+            {
+                ...mockPost,
+                content: [
+                    {
+                        ...mockEnglishContent,
+                        video: undefined,
+                        text: "Test content",
+                    },
+                ],
+            },
+        ];
+
+        const wrapper = mount(SinglePost, {
+            shallow: true,
+        });
+
+        expect(wrapper.html()).not.toContain("video-player-stub");
+        expect(wrapper.text()).toContain("Test content");
     });
 });
