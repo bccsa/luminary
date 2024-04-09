1b7a7d43fb3ca76333ff58eed523165235cbbc6b
Laurens Groeneveld <me@laurens.io>
2024-04-09 14:42:12 +0200

    Update logo

--- app/src/components/navigation/TopBar.vue
+++ app/src/components/navigation/TopBar.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { PlayIcon } from "@heroicons/vue/24/solid";
 import ProfileMenu from "./ProfileMenu.vue";
 </script>
 
@@ -8,7 +9,10 @@ import ProfileMenu from "./ProfileMenu.vue";
             class="mx-auto flex max-w-7xl flex-row items-center justify-between space-x-8 px-4 py-5"
         >
             <RouterLink to="/">
-                <img alt="logo" class="w-52" src="@/assets/logo.svg" />
+                <div class="flex items-center gap-2 text-xl hover:text-zinc-600">
+                    <PlayIcon class="h-6 w-6 text-yellow-500" />
+                    <div><span class="font-semibold">BCC Africa</span> <span>App</span></div>
+                </div>
             </RouterLink>
 
             <div>
