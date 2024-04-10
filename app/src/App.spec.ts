082986a7cb022814244efe90570fc7c82de0fd12
Laurens Groeneveld <me@laurens.io>
2024-04-10 16:12:24 +0200

    Fix App tests

--- app/src/App.spec.ts
+++ app/src/App.spec.ts
@@ -20,24 +20,37 @@ describe("App", () => {
         vi.clearAllMocks();
     });
 
-    it("renders a loading spinner when not authenticated", () => {
-        (auth0 as any).useAuth0 = vi.fn().mockReturnValue({
-            isAuthenticated: ref(false),
-            getAccessTokenSilently: vi.fn(),
-        });
-
-        const wrapper = mount(App);
-
-        expect(wrapper.findComponent(LoadingSpinner).exists()).toBe(true);
-    });
-
     it("registers the socket connection events", async () => {
+        (auth0 as any).useAuth0 = vi.fn().mockReturnValue({
+            isLoading: ref(false),
+            isAuthenticated: ref(false),
+        });
         const socketConnectionStore = useSocketConnectionStore();
 
-        mount(App);
+        mount(App, {
+            shallow: true,
+        });
 
         await waitForExpect(() => {
             expect(socketConnectionStore.bindEvents).toHaveBeenCalledOnce();
+        });
+    });
+
+    it("only gets the token when authenticated", async () => {
+        const getAccessTokenSilently = vi.fn();
+
+        (auth0 as any).useAuth0 = vi.fn().mockReturnValue({
+            isLoading: ref(false),
+            isAuthenticated: ref(true),
+            getAccessTokenSilently,
+        });
+
+        mount(App, {
+            shallow: true,
+        });
+
+        await waitForExpect(() => {
+            expect(getAccessTokenSilently).toHaveBeenCalledOnce();
         });
     });
 });
