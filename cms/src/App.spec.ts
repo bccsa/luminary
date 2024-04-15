5b7b5734db9585ce1fa0515f248ee95ebb5e0587
Laurens Groeneveld <me@laurens.io>
2024-04-15 16:40:40 +0200

    Wait until Auth0 is loaded in CMS (#111)
    + Adjust colors on BCC button

--- cms/src/App.spec.ts
+++ cms/src/App.spec.ts
@@ -23,6 +23,7 @@ describe("App", () => {
 
     it("renders a loading spinner when not authenticated", () => {
         (auth0 as any).useAuth0 = vi.fn().mockReturnValue({
+            isLoading: ref(false),
             isAuthenticated: ref(false),
             getAccessTokenSilently: vi.fn(),
         });
@@ -32,8 +33,30 @@ describe("App", () => {
         expect(wrapper.findComponent(LoadingSpinner).exists()).toBe(true);
     });
 
+    it("only gets the token when authenticated", async () => {
+        const getAccessTokenSilently = vi.fn();
+
+        (auth0 as any).useAuth0 = vi.fn().mockReturnValue({
+            isLoading: ref(false),
+            isAuthenticated: ref(true),
+            getAccessTokenSilently,
+        });
+
+        mount(App, {
+            shallow: true,
+        });
+
+        await waitForExpect(() => {
+            expect(getAccessTokenSilently).toHaveBeenCalledOnce();
+        });
+    });
+
     it("registers the socket connection events", async () => {
         const socketConnectionStore = useSocketConnectionStore();
+        (auth0 as any).useAuth0 = vi.fn().mockReturnValue({
+            isLoading: ref(false),
+            getAccessTokenSilently: vi.fn(),
+        });
 
         mount(App);
 
@@ -44,6 +67,10 @@ describe("App", () => {
 
     it("watches for syncable changes", async () => {
         const localChangeStore = useLocalChangeStore();
+        (auth0 as any).useAuth0 = vi.fn().mockReturnValue({
+            isLoading: ref(false),
+            getAccessTokenSilently: vi.fn(),
+        });
 
         mount(App);
 
