7b634a14cc76cbdc735702facb3d0a2cd67917b4
Ivan Slabbert <ivanslabbert@users.noreply.github.com>
2024-05-17 23:11:22 +0200

    draft

--- cms/src/db/repositories/imageRepository.ts
+++ cms/src/db/repositories/imageRepository.ts
@@ -1,52 +1,39 @@
-import { ImageDto, Uuid, Image, DocType, ImageUploadDto } from "@/types";
+import { type ImageDto, type Uuid, type Image, DocType } from "@/types";
 import { BaseRepository } from "./baseRepository";
 import { v4 as uuidv4 } from "uuid";
 import { db } from "../baseDatabase";
 import { useObservable } from "@vueuse/rxjs";
 import { liveQuery } from "dexie";
+import type { Observable } from "rxjs";
+import type { Ref } from "vue";
 
 export class ImageRepository extends BaseRepository {
-    find(id: Uuid) {
-        return this.fromDto(this.whereId(id).first() as unknown as ImageDto);
+    async find(id: Uuid) {
+        const res = (await this.whereId(id).first()) as unknown as ImageDto;
+        return this.fromDto(res);
     }
 
-    async getAll() {
-        this.whereType(DocType.Image).toArray((dtos) => {
-            Promise.all(this.fromDtos(dtos as ImageDto[]));
-        });
-    }
-    // getAll = useObservable (
-    //     liveQuery(async () => {
-    //         return (await this.whereType(DocType.Image).toArray())?.map((dto) =>
-    //             this.fromDto(dto as ImageDto),
-    //         );
-    //     })
-    // )
+    getAll: Readonly<Ref<Image[] | undefined>> = useObservable(
+        liveQuery(() =>
+            this.whereType(DocType.Image).toArray((dtos) => {
+                Promise.all(this.fromDtos(dtos as ImageDto[]));
+            }),
+        ) as unknown as Observable<Image[]>,
+    );
 
-    async create(UploadData?: ImageUploadDto) {
-        const imageId = uuidv4();
-        const image: ImageDto = {
-            _id: imageId,
+    new(): Image {
+        return this.fromDto({
+            _id: uuidv4(),
             updatedTimeUtc: Date.now(),
             type: DocType.Image,
-            name: "",
+            name: "New Image",
             description: "",
             files: [],
-            uploadData: UploadData,
-            memberOf: [],
-        };
-
-        await db.docs.put(image);
-
-        // Save change, will be sent to the API later
-        await db.localChanges.put({
-            doc: image,
+            memberOf: ["group-private-content"], // TODO set right group
         });
-
-        return imageId;
     }
 
-    async update(image: Image) {
+    async upsert(image: Image) {
         const imageDto = this.toDto(image);
         await db.docs.put(imageDto);
 
