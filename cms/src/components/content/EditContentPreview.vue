eb210b035e7f8749fa08ff091624062868d48653
Ivan Slabbert <ivanslabbert@users.noreply.github.com>
2024-06-05 15:12:46 +0200

    Add content view button

---
+++ cms/src/components/content/EditContentPreview.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import LButton from "@/components/button/LButton.vue";
+import LCard from "@/components/common/LCard.vue";
+import { EyeIcon, ArrowTopRightOnSquareIcon } from "@heroicons/vue/20/solid";
+import { ContentStatus, type ContentDto } from "@/types";
+import { computed } from "vue";
+import { useGlobalConfigStore } from "@/stores/globalConfig";
+
+type Props = {
+    content: ContentDto;
+};
+
+const props = defineProps<Props>();
+
+const { clientAppUrl } = useGlobalConfigStore();
+const liveUrl = computed(() => new URL(props.content.slug, clientAppUrl));
+</script>
+
+<template>
+    <LCard title="View" :icon="EyeIcon" v-if="content.status == ContentStatus.Published">
+        <LButton :icon="ArrowTopRightOnSquareIcon" iconRight is="a" :href="liveUrl" target="_blank"
+            >View live version</LButton
+        >
+    </LCard>
+</template>
