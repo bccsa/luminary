d2f7962822293ea08d7b5d7ae3b02f6056df5c9b
Johan Bell <belljohan3@gmail.com>
2024-06-13 17:37:23 +0100

    add test for expiry shorcuts button

--- cms/src/components/content/EditContentBasic.spec.ts
+++ cms/src/components/content/EditContentBasic.spec.ts
@@ -8,6 +8,8 @@ import { useUserAccessStore } from "@/stores/userAccess";
 import { ref } from "vue";
 import EditContentBasic from "./EditContentBasic.vue";
 import type { ContentDto } from "@/types";
+import { DateTime } from "luxon";
+import waitForExpect from "wait-for-expect";
 
 describe("EditContentBasic.vue", () => {
     beforeAll(async () => {
@@ -56,7 +58,10 @@ describe("EditContentBasic.vue", () => {
     });
 
     it("sets expiry date when shortcut buttons are clicked", async () => {
-        const content = ref<ContentDto>(mockEnglishContentDto);
+        const content = ref<ContentDto>({
+            ...mockEnglishContentDto,
+            publishDate: DateTime.now().toMillis(),
+        });
         const wrapper = mount(EditContentBasic, {
             props: {
                 disabled: false,
@@ -64,15 +69,111 @@ describe("EditContentBasic.vue", () => {
             },
         });
 
-        // Click the expiry shortcut button
-        const oneButton = wrapper.find('[data-test="1"]');
-        await oneButton.trigger("click");
-        const weekButton = wrapper.find('[data-test="W"]');
-        await weekButton.trigger("click");
+        // Numbers shorcuts buttons
+        const one = wrapper.find("[name='1']");
+        const two = wrapper.find("[name='2']");
+        const three = wrapper.find("[name='3']");
+        const six = wrapper.find("[name='6']");
 
-        // Wait for Vue to process the state change
-        expect(content.value.expiryDate).toBeGreaterThan(
-            content.value.publishDate as unknown as number,
+        // Units shorcuts values
+        const week = wrapper.find("[name='W']");
+        const month = wrapper.find("[name='M']");
+        const year = wrapper.find("[name='Y']");
+
+        // TEST FOR WEEKS
+
+        // 1 week shorcut test
+        one.trigger("click");
+        week.trigger("click");
+        const publishDate = DateTime.fromMillis(content.value.publishDate!);
+        const expiryDate = DateTime.fromMillis(content.value.expiryDate!);
+        expect(expiryDate).toStrictEqual(publishDate.plus({ weeks: 1 }));
+
+        // 2 weeks shorcut test
+        two.trigger("click");
+        week.trigger("click");
+        expect(DateTime.fromMillis(content.value.expiryDate!)).toStrictEqual(
+            DateTime.fromMillis(content.value.publishDate!).plus({ weeks: 2 }),
         );
+
+        // 3 weeks shorcut test
+        three.trigger("click");
+        week.trigger("click");
+        expect(DateTime.fromMillis(content.value.expiryDate!)).toStrictEqual(
+            DateTime.fromMillis(content.value.publishDate!).plus({ weeks: 3 }),
+        );
+
+        // 6 weeks shorcut test
+        six.trigger("click");
+        week.trigger("click");
+        expect(DateTime.fromMillis(content.value.expiryDate!)).toStrictEqual(
+            DateTime.fromMillis(content.value.publishDate!).plus({ weeks: 6 }),
+        );
+
+        // TEST FOR MONTHS
+
+        // 1 month shorcut test
+        one.trigger("click");
+        month.trigger("click");
+        expect(DateTime.fromMillis(content.value.expiryDate!)).toStrictEqual(
+            DateTime.fromMillis(content.value.publishDate!).plus({ months: 1 }),
+        );
+
+        // 2 months shorcut test
+        two.trigger("click");
+        month.trigger("click");
+        expect(DateTime.fromMillis(content.value.expiryDate!)).toStrictEqual(
+            DateTime.fromMillis(content.value.publishDate!).plus({ months: 2 }),
+        );
+
+        // 3 months shorcut test
+        three.trigger("click");
+        month.trigger("click");
+        expect(DateTime.fromMillis(content.value.expiryDate!)).toStrictEqual(
+            DateTime.fromMillis(content.value.publishDate!).plus({ months: 3 }),
+        );
+
+        // 6 months shorcut test
+        six.trigger("click");
+        month.trigger("click");
+        expect(DateTime.fromMillis(content.value.expiryDate!)).toStrictEqual(
+            DateTime.fromMillis(content.value.publishDate!).plus({ months: 6 }),
+        );
+
+        // TEST FOR YEARS
+
+        // 1 year shorcut test
+        one.trigger("click");
+        year.trigger("click");
+        expect(DateTime.fromMillis(content.value.expiryDate!)).toStrictEqual(
+            DateTime.fromMillis(content.value.publishDate!).plus({ year: 1 }),
+        );
+
+        // 2 years shorcut test
+        two.trigger("click");
+        year.trigger("click");
+        expect(DateTime.fromMillis(content.value.expiryDate!)).toStrictEqual(
+            DateTime.fromMillis(content.value.publishDate!).plus({ years: 2 }),
+        );
+
+        // 3 years shorcut test
+        three.trigger("click");
+        year.trigger("click");
+        expect(DateTime.fromMillis(content.value.expiryDate!)).toStrictEqual(
+            DateTime.fromMillis(content.value.publishDate!).plus({ years: 3 }),
+        );
+
+        // 6 years shorcut test
+        six.trigger("click");
+        year.trigger("click");
+        expect(DateTime.fromMillis(content.value.expiryDate!)).toStrictEqual(
+            DateTime.fromMillis(content.value.publishDate!).plus({ years: 6 }),
+        );
+
+        // TEST FOR CLEAR BUTTON
+        // Clear button
+        const clearButton = wrapper.find("[name='clear']");
+        await clearButton.trigger("click");
+        expect(content.value.expiryDate).toBeUndefined();
     });
 });
