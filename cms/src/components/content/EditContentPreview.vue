cb7d3f5fac70b946ebddc5b52b5584b189152d0b
Ivan Slabbert <ivanslabbert@users.noreply.github.com>
2024-06-13 14:44:16 +0200

    fix tests

--- cms/src/components/content/EditContentPreview.vue
+++ cms/src/components/content/EditContentPreview.vue
@@ -5,6 +5,7 @@ import { EyeIcon, ArrowTopRightOnSquareIcon } from "@heroicons/vue/20/solid";
 import { ContentStatus, type ContentDto } from "@/types";
 import { computed } from "vue";
 import { useGlobalConfigStore } from "@/stores/globalConfig";
+import { client } from "@auth0/auth0-vue/dist/typings/plugin";
 
 type Props = {
     content: ContentDto;
@@ -16,8 +17,8 @@ const { clientAppUrl } = useGlobalConfigStore();
 const liveUrl = computed(() => {
     if (!props.content) return "";
     console.log("props.content.slug: ", props.content.slug);
-    console.log("clientAppUrl: ", clientAppUrl);
-    const url = new URL(props.content.slug, clientAppUrl);
+    console.log("clientAppUrl: ", clientAppUrl ? clientAppUrl : "http://localhost");
+    const url = new URL(props.content.slug, clientAppUrl ? clientAppUrl : "http://localhost");
     return url.toString();
 });
 </script>
