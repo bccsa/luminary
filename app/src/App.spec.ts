13553dbc177718eb5dffd0dd692e80aaebf6fcb8
Ivan Slabbert <ivanslabbert@users.noreply.github.com>
2024-07-08 21:57:59 +0200

    Implement socket.io from the shared library, and fix a few tests

--- app/src/App.spec.ts
+++ app/src/App.spec.ts
@@ -1,3 +1,4 @@
+import "fake-indexeddb/auto";
 import { describe, it, expect, vi, afterEach, beforeEach } from "vitest";
 import { mount } from "@vue/test-utils";
 import App from "./App.vue";
@@ -5,7 +6,6 @@ import * as auth0 from "@auth0/auth0-vue";
 import { ref } from "vue";
 import { createTestingPinia } from "@pinia/testing";
 import { setActivePinia } from "pinia";
-import { useSocketConnectionStore } from "./stores/socketConnection";
 import waitForExpect from "wait-for-expect";
 
 vi.mock("@auth0/auth0-vue");
@@ -17,22 +17,6 @@ describe("App", () => {
 
     afterEach(() => {
         vi.clearAllMocks();
-    });
-
-    it("registers the socket connection events", async () => {
-        (auth0 as any).useAuth0 = vi.fn().mockReturnValue({
-            isLoading: ref(false),
-            isAuthenticated: ref(false),
-        });
-        const socketConnectionStore = useSocketConnectionStore();
-
-        mount(App, {
-            shallow: true,
-        });
-
-        await waitForExpect(() => {
-            expect(socketConnectionStore.bindEvents).toHaveBeenCalledOnce();
-        });
     });
 
     it("only gets the token when authenticated", async () => {
