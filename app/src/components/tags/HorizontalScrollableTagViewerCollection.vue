63146b94c6191c2b6373ebf7c3498a7f9ac4f9c8
Ivan Slabbert <ivanslabbert@users.noreply.github.com>
2024-07-15 15:26:47 +0200

    Load homepage content per selected appLanguage

---
+++ app/src/components/tags/HorizontalScrollableTagViewerCollection.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+// Helper component to load multiple HorizontalScrollableTagViewer components only after the app langauge is available.
+import HorizontalScrollableTagViewer from "@/components/tags/HorizontalScrollableTagViewer.vue";
+import { db, type queryOptions, type TagType } from "luminary-shared";
+
+type Props = {
+    tagType: TagType;
+    tagQueryOptions: queryOptions;
+    contentQueryOptions: queryOptions;
+};
+const props = defineProps<Props>();
+
+if (!props.tagQueryOptions.languageId) {
+    console.error("Language ID is required as an option for tagQueryOptions");
+}
+
+const categories = db.tagsWhereTagTypeAsRef(props.tagType, props.tagQueryOptions);
+</script>
+
+<template>
+    <HorizontalScrollableTagViewer
+        v-for="category in categories"
+        :key="category._id"
+        :tag="category"
+        :queryOptions="contentQueryOptions"
+    />
+</template>
