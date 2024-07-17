eaf26cea39d49764df202b002e46ec62ce3e7fee
Johan Bell <belljohan3@gmail.com>
2024-07-17 22:09:45 +0100

    add more test for content status

--- cms/src/components/content/EditContentValidation.spec.ts
+++ cms/src/components/content/EditContentValidation.spec.ts
@@ -3,6 +3,7 @@ import { describe, it, expect } from "vitest";
 import { mount } from "@vue/test-utils";
 import * as mockData from "@/tests/mockdata";
 import EditContentValidation from "./EditContentValidation.vue";
+import { PublishStatus } from "luminary-shared";
 
 describe("EditContentValidation.vue", () => {
     it("don't show validation error if no errors", async () => {
@@ -62,5 +63,55 @@ describe("EditContentValidation.vue", () => {
         });
 
         expect(wrapper.text()).toContain("Français");
+    });
+
+    describe("Content status", () => {
+        it("show published status", async () => {
+            const wrapper = mount(EditContentValidation, {
+                props: {
+                    languages: [mockData.mockLanguageDtoFra],
+                    content: mockData.mockFrenchContentDto,
+                },
+            });
+
+            expect(wrapper.text()).toContain("Français");
+            expect(wrapper.text()).toContain("Published");
+        });
+
+        it("shows expired status", async () => {
+            const wrapper = mount(EditContentValidation, {
+                props: {
+                    languages: [mockData.mockLanguageDtoSwa],
+                    content: { ...mockData.mockSwahiliContentDto, expiryDate: Date.now() - 1 },
+                },
+            });
+
+            expect(wrapper.text()).toContain("Swahili");
+            expect(wrapper.text()).toContain("Expired");
+        });
+
+        it("shows scheduled status", async () => {
+            const wrapper = mount(EditContentValidation, {
+                props: {
+                    languages: [mockData.mockLanguageDtoEng],
+                    content: { ...mockData.mockEnglishContentDto, publishDate: Date.now() + 10 },
+                },
+            });
+
+            expect(wrapper.text()).toContain("English");
+            expect(wrapper.text()).toContain("Scheduled");
+        });
+
+        it("shows draft status", async () => {
+            const wrapper = mount(EditContentValidation, {
+                props: {
+                    languages: [mockData.mockLanguageDtoEng],
+                    content: { ...mockData.mockEnglishContentDto, status: PublishStatus.Draft },
+                },
+            });
+
+            expect(wrapper.text()).toContain("English");
+            expect(wrapper.text()).toContain("Draft");
+        });
     });
 });
