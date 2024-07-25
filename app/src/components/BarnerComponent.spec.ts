82eb0e72fbe5634213d5b1c483c2713a1fee23a0
Johan Bell <belljohan3@gmail.com>
2024-07-25 12:37:17 +0100

    Changed OnlineIndicator to BarnerComponent and make it generic

---
+++ app/src/components/BarnerComponent.spec.ts
@@ -0,0 +1,32 @@
+import "fake-indexeddb/auto";
+import { describe, it, expect } from "vitest";
+import { mount } from "@vue/test-utils";
+import BarnerComponent from "./BannerComponent.vue";
+import { isConnected } from "luminary-shared";
+import { SignalSlashIcon } from "@heroicons/vue/20/solid";
+
+describe("OnlineIndicator", async () => {
+    it("renders the offline status and verifies classes and icon", async () => {
+        isConnected.value = false;
+
+        const wrapper = mount(BarnerComponent, {
+            props: {
+                icon: SignalSlashIcon,
+                message: "Test Offline",
+                bgColor: "bg-green-100",
+            },
+        });
+        await wrapper.vm.$nextTick();
+
+        // Check if the wrapper contains the message
+        expect(wrapper.html()).toContain("Test Offline");
+
+        // Check if the first div has the class bg-green-100
+        const firstDiv = wrapper.find("div");
+        expect(firstDiv.classes()).toContain("bg-green-100");
+
+        // Check if the SignalSlashIcon component exists
+        const iconComponent = wrapper.findComponent(SignalSlashIcon);
+        expect(iconComponent.exists()).toBe(true);
+    });
+});
