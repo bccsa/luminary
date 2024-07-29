591258ceb7c7f8a39522a4b0c5d13d1e13a3a48c
Johan Bell <belljohan3@gmail.com>
2024-07-29 09:34:33 +0100

    Minors changes

--- app/src/components/LBanner.vue
+++ app/src/components/LBanner.vue
@@ -17,18 +17,26 @@ const isBannerVisible = ref(isConnected);
 <template>
     <div
         v-if="!isBannerVisible"
-        class="inset-x-0 top-0 z-50 text-zinc-900 shadow-lg"
+        class="shadow-top inset-x-0 top-0 z-50 text-zinc-900"
         :class="bgColor"
     >
-        <div class="container mx-auto flex items-center justify-between px-4 py-2">
+        <div class="flex items-center justify-between px-6 py-2 sm:px-6 sm:py-1">
             <div class="flex items-center gap-2">
                 <component :is="icon" class="h-6 w-6" />
-                <span>{{ message }}</span>
+                <span class="text-lg sm:text-sm">{{ message }}</span>
             </div>
             <XMarkIcon
                 @click="isBannerVisible = true"
-                class="h-8 w-8 cursor-pointer text-sm underline"
+                class="h-8 w-8 cursor-pointer text-sm underline sm:h-6 sm:w-6"
             />
         </div>
     </div>
 </template>
+
+<style scoped>
+.shadow-top {
+    box-shadow:
+        0 -10px 15px -3px rgba(0, 0, 0, 0.1),
+        0 -4px 6px -2px rgba(0, 0, 0, 0.05);
+}
+</style>
