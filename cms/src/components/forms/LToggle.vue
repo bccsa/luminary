8ff9493f676a3fbbeb4c77ec8c96517ab3499271
Johan Bell <belljohan3@gmail.com>
2024-08-03 22:02:31 +0100

    show "selected" tick-mark icon on selectedLanguage & improve toggle

--- cms/src/components/forms/LToggle.vue
+++ cms/src/components/forms/LToggle.vue
@@ -17,14 +17,13 @@ const emit = defineEmits(["update:modelValue"]);
 
 const toggled = computed({
     get() {
-        return modelValue.value;
+        return !!modelValue.value;
     },
     set(value: boolean) {
         emit("update:modelValue", value);
     },
 });
 </script>
-
 <template>
     <Switch
         v-model="toggled"
@@ -35,30 +34,24 @@ const toggled = computed({
                 'cursor-pointer': !disabled,
                 'bg-zinc-500': disabled && toggled,
                 'bg-zinc-100': disabled && !toggled,
-                'bg-green-100': !disabled && toggled,
-                'bg-red-100': !disabled && !toggled,
+                'bg-green-400': !disabled && toggled,
+                'bg-red-400': !disabled && !toggled,
             },
         ]"
     >
         <span class="sr-only">Toggle</span>
         <span
             :class="[
-                toggled
-                    ? 'translate-x-12 bg-green-800 px-5 text-[10px]'
-                    : 'translate-x-0  bg-red-800 ',
-                'pointer-events-none relative inline-block h-5 w-1/2 transform rounded-full bg-white text-white shadow ring-0 transition duration-200 ease-in-out',
-                toggled
-                    ? 'translate-x-12 bg-green-800 px-5 text-[10px]'
-                    : 'translate-x-0  bg-red-800 ',
-                'pointer-events-none relative inline-block h-5 w-1/2 transform rounded-full bg-white text-white shadow ring-0 transition duration-200 ease-in-out',
+                'pointer-events-none relative inline-block h-5 w-1/2 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out',
+                toggled ? 'translate-x-12 bg-green-800 text-[10px]' : 'translate-x-0 bg-red-800',
             ]"
         >
             <span
                 :class="[
+                    'absolute inset-0 flex h-full w-full items-center justify-center text-black transition-opacity',
                     toggled
                         ? 'opacity-0 duration-100 ease-out'
                         : 'opacity-100 duration-200 ease-in',
-                    'absolute inset-0 flex h-full w-full items-center justify-center transition-opacity',
                 ]"
                 aria-hidden="true"
             >
@@ -66,10 +59,10 @@ const toggled = computed({
             </span>
             <span
                 :class="[
+                    'absolute inset-0 flex h-full w-full items-center justify-center text-black transition-opacity',
                     toggled
                         ? 'opacity-100 duration-200 ease-in'
                         : 'opacity-0 duration-100 ease-out',
-                    'absolute inset-0 flex h-full w-full items-center justify-center transition-opacity',
                 ]"
                 aria-hidden="true"
             >
