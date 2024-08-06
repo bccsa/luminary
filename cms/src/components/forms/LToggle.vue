6b8016bc22f86b09333f3be88617d4f90662d2b9
Johan Bell <belljohan3@gmail.com>
2024-08-06 11:17:19 +0100

    revert changes of LToggle component

--- cms/src/components/forms/LToggle.vue
+++ cms/src/components/forms/LToggle.vue
@@ -11,7 +11,7 @@ const props = withDefaults(defineProps<Props>(), {
     disabled: false,
 });
 
-const { modelValue, disabled } = toRefs(props);
+const { modelValue } = toRefs(props);
 
 const emit = defineEmits(["update:modelValue"]);
 
@@ -24,49 +24,66 @@ const toggled = computed({
     },
 });
 </script>
+
 <template>
     <Switch
         v-model="toggled"
         :disabled="disabled"
         :class="[
-            'relative inline-flex h-6 w-24 flex-shrink-0 rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-zinc-950 focus:ring-offset-2',
+            'relative inline-flex h-6 w-11 flex-shrink-0 rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-zinc-950 focus:ring-offset-2',
             {
                 'cursor-pointer': !disabled,
                 'bg-zinc-500': disabled && toggled,
                 'bg-zinc-100': disabled && !toggled,
-                'bg-green-400': !disabled && toggled,
-                'bg-red-400': !disabled && !toggled,
+                'bg-zinc-600': !disabled && toggled,
+                'bg-zinc-200': !disabled && !toggled,
             },
         ]"
     >
         <span class="sr-only">Toggle</span>
         <span
             :class="[
-                'pointer-events-none relative inline-block h-5 w-1/2 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out',
-                toggled ? 'translate-x-12 bg-green-800 text-[10px]' : 'translate-x-0 bg-red-800',
+                toggled ? 'translate-x-5' : 'translate-x-0',
+                'pointer-events-none relative inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out',
             ]"
         >
             <span
                 :class="[
-                    'absolute inset-0 flex h-full w-full items-center justify-center text-black transition-opacity',
                     toggled
                         ? 'opacity-0 duration-100 ease-out'
                         : 'opacity-100 duration-200 ease-in',
+                    'absolute inset-0 flex h-full w-full items-center justify-center transition-opacity',
                 ]"
                 aria-hidden="true"
             >
-                Draft
+                <svg
+                    fill="none"
+                    viewBox="0 0 12 12"
+                    :class="['h-3 w-3 ', disabled ? 'text-zinc-300' : 'text-zinc-400']"
+                >
+                    <path
+                        d="M4 8l2-2m0 0l2-2M6 6L4 4m2 2l2 2"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                    />
+                </svg>
             </span>
             <span
                 :class="[
-                    'absolute inset-0 flex h-full w-full items-center justify-center text-black transition-opacity',
                     toggled
                         ? 'opacity-100 duration-200 ease-in'
                         : 'opacity-0 duration-100 ease-out',
+                    'absolute inset-0 flex h-full w-full items-center justify-center transition-opacity',
                 ]"
                 aria-hidden="true"
             >
-                Publish
+                <svg class="h-3 w-3 text-zinc-600" fill="currentColor" viewBox="0 0 12 12">
+                    <path
+                        d="M3.707 5.293a1 1 0 00-1.414 1.414l1.414-1.414zM5 8l-.707.707a1 1 0 001.414 0L5 8zm4.707-3.293a1 1 0 00-1.414-1.414l1.414 1.414zm-7.414 2l2 2 1.414-1.414-2-2-1.414 1.414zm3.414 2l4-4-1.414-1.414-4 4 1.414 1.414z"
+                    />
+                </svg>
             </span>
         </span>
     </Switch>
