a3fa39445b4bc75f2effff9407e43d7ac4a4aa58
Johan Bell <belljohan3@gmail.com>
2024-08-21 09:11:21 +0100

    add test for offline banner indication

--- app/src/App.spec.ts
+++ app/src/App.spec.ts
@@ -7,6 +7,9 @@ import { ref } from "vue";
 import waitForExpect from "wait-for-expect";
 import { setActivePinia } from "pinia";
 import { createTestingPinia } from "@pinia/testing";
+import { SignalSlashIcon } from "@heroicons/vue/24/solid";
+import { isConnected } from "luminary-shared";
+import { useNotificationStore } from "./stores/notification";
 
 vi.mock("@auth0/auth0-vue");
 
@@ -34,6 +37,44 @@ describe("App", () => {
 
         await waitForExpect(() => {
             expect(getAccessTokenSilently).toHaveBeenCalledOnce();
+        });
+    });
+
+    it("shows the banner when offline", async () => {
+        const addNotification = vi.fn();
+        const removeNotification = vi.fn();
+
+        vi.spyOn(isConnected, "value", "get").mockReturnValue(false);
+        vi.spyOn(useNotificationStore(), "addNotification").mockImplementation(addNotification);
+        vi.spyOn(useNotificationStore(), "removeNotification").mockImplementation(
+            removeNotification,
+        );
+
+        mount(App, {
+            shallow: true,
+        });
+
+        // Wait for the setTimeout and watch to trigger
+        await waitForExpect(() => {
+            expect(addNotification).toHaveBeenCalledWith({
+                id: "offlineBanner",
+                title: "You are offline",
+                description:
+                    "You can still use the app and browse through offline content, but some content (like videos) might not be available.",
+                state: "warning",
+                type: "banner",
+                icon: SignalSlashIcon,
+            });
+        });
+
+        // Now test removing the notification when reconnected
+        vi.spyOn(isConnected, "value", "get").mockReturnValue(true);
+
+        // Trigger the watch manually by updating the value
+        isConnected.value = true;
+
+        await waitForExpect(() => {
+            expect(removeNotification).toHaveBeenCalledWith("offlineBanner");
         });
     });
 });
