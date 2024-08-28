5c4ae788f8ace71414c2a378b41ba544f8fd1dc9
DirkDevFreelance <dirkdevbusiness@gmail.com>
2024-08-28 16:52:57 +0200

    Add filter UI

--- cms/src/components/content/ContentOverview.vue
+++ cms/src/components/content/ContentOverview.vue
@@ -7,6 +7,7 @@ import {
     ArrowUpIcon,
     ArrowDownIcon,
     ArrowPathIcon,
+    XMarkIcon,
 } from "@heroicons/vue/20/solid";
 import { RouterLink } from "vue-router";
 import {
@@ -18,7 +19,7 @@ import {
     type Uuid,
     hasAnyPermission,
 } from "luminary-shared";
-import { computed, onMounted, onUnmounted, ref, watch } from "vue";
+import { computed, nextTick, onMounted, onUnmounted, ref, watch } from "vue";
 import ContentTable from "@/components/content/ContentTable.vue";
 import LSelect from "../forms/LSelect.vue";
 import { capitaliseFirstLetter } from "@/util/string";
@@ -30,6 +31,15 @@ type Props = {
     docType: DocType.Post | DocType.Tag;
     tagType?: TagType;
 };
+
+type Filter = "type" | "status" | "translation" | undefined;
+
+interface FilterInput {
+    id: string;
+    filter: string;
+    by: Filter;
+}
+
 const props = defineProps<Props>();
 
 const tagType = Object.entries(TagType).some((t) => t[1] == props.tagType)
@@ -86,44 +96,116 @@ const titleType = tagTypeString ? tagTypeString : props.docType;
 router.currentRoute.value.meta.title = `${capitaliseFirstLetter(titleType)} overview`;
 
 const searchTerm = ref("");
+const filterInput = ref<string>("");
+
+const filters = ref<FilterInput[]>([]);
+
 const showSortOptions = ref(false);
+
 const sortOptionsRef = ref(null);
+
+const filterBtnRef = ref(null);
+const filterInputRef = ref(null);
+
 const sortOption = ref<"title" | "updatedTimeUtc" | "publishDate" | "expiryDate" | undefined>(
     undefined,
 );
 
+const handleResetSorting = () => (queryOptions.value.orderBy = undefined);
+const handleSortByTitle = () => (queryOptions.value.orderBy = "title");
+const handleSortByUpdated = () => (queryOptions.value.orderBy = "updatedTimeUtc");
+const handleSortAscending = () => (queryOptions.value.orderDirection = "asc");
+const handleSortDescending = () => (queryOptions.value.orderDirection = "desc");
+
+const handleSearch = () => {
+    queryOptions.value.search = searchTerm.value;
+    searchTerm.value = "";
+};
+
+const handleNewFilter = (event: Event) => {
+    const target = event.target as HTMLElement;
+    target.classList.add("hidden");
+
+    const filterInput = document.getElementById("filterInput");
+    filterInput?.classList.remove("hidden");
+    filterInput?.focus();
+};
+
+const handleAddNewFilter = () => {
+    const filterBtn = document.getElementById("addFilterBtn");
+    const filterInputEl = document.getElementById("filterInput");
+
+    if (filterInput.value) {
+        const input = filterInput.value.trim().toLowerCase();
+        console.log(input);
+        const newInput = input.split(":");
+        console.log(newInput);
+
+        const newId =
+            filters.value.length > 0
+                ? (Math.max(...filters.value.map((f) => parseInt(f.id))) + 1).toString()
+                : "1";
+
+        const newFilter: FilterInput = {
+            id: newId,
+            filter: newInput[0],
+            by: newInput[1] as Filter,
+        };
+        console.log(newFilter);
+
+        filterInput.value = "";
+        filters.value.push(newFilter);
+    }
+
+    filterBtn?.classList.remove("hidden");
+    filterInputEl?.classList.add("hidden");
+};
+
+const handleFilterDelete = async (event: Event) => {
+    const element = event.target as HTMLElement;
+
+    if (element.tagName === "BUTTON") {
+        const idToDelete = element.id;
+        console.log("Id to delete:", idToDelete);
+        const currentFilters = [...filters.value];
+        filters.value = currentFilters.filter((val) => {
+            console.log("Checking filter with ID:", val.id);
+            return idToDelete !== val.id;
+        });
+
+        await nextTick();
+    }
+
+    console.log(filters.value);
+};
+
 watch(sortOption, () => {
     queryOptions.value.orderBy = sortOption.value;
+});
+
+watch(filters, () => {
+    filters.value.map((filter) => {
+        if (filter.filter == "translation")
+            queryOptions.value.translationStatus = filter.by as
+                | "all"
+                | "translated"
+                | "untranslated"
+                | undefined;
+        console.log(filter);
+    });
 });
 
 onClickOutside(sortOptionsRef, () => {
     showSortOptions.value = false;
 });
 
-function handleResetSorting() {
-    queryOptions.value.orderBy = undefined;
-}
+onClickOutside(filterInputRef, () => {
+    const filterBtn = document.getElementById("addFilterBtn");
+    const filterInput = document.getElementById("filterInput");
 
-function handleSortByTitle() {
-    queryOptions.value.orderBy = "title";
-}
-
-function handleSortByUpdated() {
-    queryOptions.value.orderBy = "updatedTimeUtc";
-}
-
-function handleSearch() {
-    queryOptions.value.search = searchTerm.value;
-    searchTerm.value = "";
-}
-
-function handleSortAscending() {
-    queryOptions.value.orderDirection = "asc";
-}
-
-function handleSortDescending() {
-    queryOptions.value.orderDirection = "desc";
-}
+    filterBtn?.classList.remove("hidden");
+    filterInput?.classList.add("hidden");
+});
 </script>
 
 <template>
@@ -179,96 +261,126 @@ function handleSortDescending() {
             :buttonPermission="canCreateNew"
             data-test="no-content"
         /> -->
-        <div
-            class="flex h-12 justify-between gap-2 rounded-t-lg border-x-[1px] border-t-[1px] bg-white p-[0.25rem] pl-2 shadow-sm"
-        >
-            <input
-                type="text"
-                class="h-full w-3/4 border-none p-4 focus:border-none focus:bg-zinc-200 focus:outline-none focus:ring-0"
-                name="search"
-                placeholder="Search..."
-                v-model="searchTerm"
-                @keydown.enter="handleSearch"
-            />
-            <div>
-                <div class="relative flex h-full gap-1">
-                    <button
-                        class="flex h-full w-10 flex-row content-center justify-center rounded-md border-[1px] focus:border-2 focus:border-black focus:outline-none"
-                        @click="handleResetSorting"
-                    >
-                        <ArrowPathIcon class="w-2/4" />
-                    </button>
-                    <button
-                        class="flex h-full w-10 flex-row content-center justify-center rounded-md border-[1px] focus:border-2 focus:border-black focus:outline-none"
-                        @click="() => (showSortOptions = true)"
-                    >
-                        <ArrowsUpDownIcon class="w-2/4" />
-                    </button>
-                    <div
-                        ref="sortOptionsRef"
-                        class="absolute right-0 top-full mt-2 h-max w-40 rounded-lg border border-gray-300 bg-white p-2 shadow-lg"
-                        v-if="showSortOptions"
-                    >
-                        <h4>Sort By:</h4>
-                        <div class="flex flex-col">
-                            <label>
-                                <input
-                                    class="ml-2 text-zinc-800 focus:border-0 focus:outline-none focus:ring-0"
-                                    type="radio"
-                                    name="sortOption"
-                                    value="title"
-                                    v-model="sortOption"
-                                />
-                                Title
-                            </label>
-                            <label>
-                                <input
-                                    class="ml-2 text-zinc-800 focus:border-0 focus:outline-none focus:ring-0"
-                                    type="radio"
-                                    name="sortOption"
-                                    value="expiryDate"
-                                    v-model="sortOption"
-                                />
-                                Expiry Date
-                            </label>
-                            <label>
-                                <input
-                                    class="ml-2 text-zinc-800 focus:border-0 focus:outline-none focus:ring-0"
-                                    type="radio"
-                                    name="sortOption"
-                                    value="publishDate"
-                                    v-model="sortOption"
-                                />
-                                Publish Date
-                            </label>
-                            <label>
-                                <input
-                                    class="ml-2 text-zinc-800 focus:border-0 focus:outline-none focus:ring-0"
-                                    type="radio"
-                                    name="sortOption"
-                                    value="updatedTimeUtc"
-                                    v-model="sortOption"
-                                />
-                                Last Updated
-                            </label>
-                        </div>
-                        <hr class="my-2" />
-                        <div class="flex flex-col gap-1">
-                            <button
-                                @click="handleSortAscending"
-                                class="flex w-full gap-1 rounded-md p-2 text-zinc-950 hover:bg-zinc-300"
-                            >
-                                <ArrowUpIcon class="w-6" /> Ascending
-                            </button>
-                            <button
-                                class="flex w-full gap-1 rounded-md p-2 hover:bg-zinc-300"
-                                @click="handleSortDescending"
-                            >
-                                <ArrowDownIcon class="w-6" /> Descending
-                            </button>
+        <div class="shadow-md">
+            <div
+                class="flex h-12 justify-between gap-2 rounded-t-lg border-[1px] bg-white p-[0.25rem] pl-2"
+            >
+                <input
+                    type="text"
+                    class="h-full w-3/4 border-none p-4 focus:border-none focus:bg-zinc-200 focus:outline-none focus:ring-0"
+                    name="search"
+                    placeholder="Search..."
+                    v-model="searchTerm"
+                    @keydown.enter="handleSearch"
+                />
+                <div>
+                    <div class="relative flex h-full gap-1">
+                        <button
+                            class="flex h-full w-10 flex-row content-center justify-center rounded-md border-[1px] focus:border-2 focus:border-black focus:outline-none"
+                            @click="handleResetSorting"
+                        >
+                            <ArrowPathIcon class="w-2/4" />
+                        </button>
+                        <button
+                            class="flex h-full w-10 flex-row content-center justify-center rounded-md border-[1px] focus:border-2 focus:border-black focus:outline-none"
+                            @click="() => (showSortOptions = true)"
+                        >
+                            <ArrowsUpDownIcon class="w-2/4" />
+                        </button>
+                        <div
+                            ref="sortOptionsRef"
+                            class="absolute right-0 top-full mt-2 h-max w-40 rounded-lg border border-gray-300 bg-white p-2 shadow-lg"
+                            v-if="showSortOptions"
+                        >
+                            <h4>Sort By:</h4>
+                            <div class="flex flex-col">
+                                <label>
+                                    <input
+                                        class="ml-2 text-zinc-800 focus:border-0 focus:outline-none focus:ring-0"
+                                        type="radio"
+                                        name="sortOption"
+                                        value="title"
+                                        v-model="sortOption"
+                                    />
+                                    Title
+                                </label>
+                                <label>
+                                    <input
+                                        class="ml-2 text-zinc-800 focus:border-0 focus:outline-none focus:ring-0"
+                                        type="radio"
+                                        name="sortOption"
+                                        value="expiryDate"
+                                        v-model="sortOption"
+                                    />
+                                    Expiry Date
+                                </label>
+                                <label>
+                                    <input
+                                        class="ml-2 text-zinc-800 focus:border-0 focus:outline-none focus:ring-0"
+                                        type="radio"
+                                        name="sortOption"
+                                        value="publishDate"
+                                        v-model="sortOption"
+                                    />
+                                    Publish Date
+                                </label>
+                                <label>
+                                    <input
+                                        class="ml-2 text-zinc-800 focus:border-0 focus:outline-none focus:ring-0"
+                                        type="radio"
+                                        name="sortOption"
+                                        value="updatedTimeUtc"
+                                        v-model="sortOption"
+                                    />
+                                    Last Updated
+                                </label>
+                            </div>
+                            <hr class="my-2" />
+                            <div class="flex flex-col gap-1">
+                                <button
+                                    @click="handleSortAscending"
+                                    class="flex w-full gap-1 rounded-md p-2 text-zinc-950 hover:bg-zinc-300"
+                                >
+                                    <ArrowUpIcon class="w-6" /> Ascending
+                                </button>
+                                <button
+                                    class="flex w-full gap-1 rounded-md p-2 hover:bg-zinc-300"
+                                    @click="handleSortDescending"
+                                >
+                                    <ArrowDownIcon class="w-6" /> Descending
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
+            </div>
+            <!--Filter Row -->
+            <div class="flex h-max w-full flex-wrap content-center gap-1 bg-white p-2 px-4">
+                <button
+                    v-for="filter in filters"
+                    :key="filter.id"
+                    :id="filter.id"
+                    @click="handleFilterDelete"
+                    class="flex w-max flex-row content-center justify-center gap-1 rounded-xl border-[1px] p-1 px-2"
+                    title="Click to delete filter"
+                >
+                    {{ filter.by }}:{{ filter.filter }}
+                </button>
+                <button
+                    id="addFilterBtn"
+                    ref="filterBtnRef"
+                    class="flex w-32 flex-row content-center justify-center gap-1 rounded-xl border-[1px] p-1 px-2"
+                    @click="handleNewFilter"
+                >
+                    Add Filter
+                </button>
+                <input
+                    ref="filterInputRef"
+                    id="filterInput"
+                    class="hidden h-5 w-32 self-center border-none focus:ring-0"
+                    @keydown.enter="handleAddNewFilter"
+                    v-model="filterInput"
+                />
             </div>
         </div>
 
