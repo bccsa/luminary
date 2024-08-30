710e7b6c169223901de56e880570f64a8adb2f43
Johan Bell <50105980+belljohan3@users.noreply.github.com>
2024-08-30 10:44:24 +0100

    refactoring notification system to support link (#312)
    * refactoring notification system to support link
    
    * add test
    
    * small changes
    
    * make suggested changes
    
    * .
    
    * fix test
    
    * small change
    
    * add close

--- app/src/components/notifications/NotificationBanner.vue
+++ app/src/components/notifications/NotificationBanner.vue
@@ -3,6 +3,7 @@ import { ref, type FunctionalComponent } from "vue";
 import { CheckCircleIcon, ExclamationCircleIcon } from "@heroicons/vue/24/outline";
 import { XMarkIcon } from "@heroicons/vue/20/solid";
 import { type Notification, useNotificationStore } from "@/stores/notification";
+import { RouterLink } from "vue-router";
 
 type Props = {
     notification: Notification;
@@ -52,15 +53,20 @@ switch (props.notification.state) {
 <template>
     <div v-if="show" class="inset-x-0 top-0 z-50 text-zinc-900" :class="color">
         <div class="flex items-center justify-between px-6 py-1">
-            <div class="flex items-center gap-2">
-                <component :is="icon" class="h-5 min-h-5 w-5 min-w-5" />
-                <div class="flex flex-col md:inline-block md:align-middle">
-                    <span class="text-md md:text-sm">{{ notification.title }}</span>
-                    <span v-if="notification.description" class="text-xs md:ml-3">{{
-                        notification.description
-                    }}</span>
+            <component
+                :is="notification.routerLink ? RouterLink : 'div'"
+                :to="notification.routerLink"
+            >
+                <div class="flex items-center gap-2">
+                    <component :is="icon" class="h-5 w-5 min-w-5" />
+                    <div class="flex flex-col md:inline-block md:align-middle">
+                        <span class="text-md md:text-sm">{{ notification.title }}</span>
+                        <span v-if="notification.description" class="text-xs md:ml-3">{{
+                            notification.description
+                        }}</span>
+                    </div>
                 </div>
-            </div>
+            </component>
 
             <button
                 type="button"
