da94c68240ff4762b5a5b711f87d818be071d8b0
Johan Bell <belljohan3@gmail.com>
2024-09-16 10:43:35 +0100

    hide the curent post from the sidebar and remove summary

--- app/src/pages/SingleContent.vue
+++ app/src/pages/SingleContent.vue
@@ -189,6 +189,7 @@ function selectTag(parentId: Uuid) {
                     )"
                     :key="tag._id"
                     :tag="tag"
+                    :currentPostId="content._id"
                     :queryOptions="{
                         filterOptions: { docType: DocType.Post },
                         languageId: appLanguageIdAsRef,
