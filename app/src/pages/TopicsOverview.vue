07ce1aeca247d90794222763b20e96157eb00c3e
Johan Bell <50105980+belljohan3@users.noreply.github.com>
2024-09-18 08:32:21 +0100

    add topic overview and details page (#325)
    * add topic overview and details page
    
    * minors changes
    
    * small changes

---
+++ app/src/pages/TopicsOverview.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { appLanguageIdAsRef } from "@/globalConfig";
+import { db, DocType, TagType, type ContentDto } from "luminary-shared";
+import { ref, watchEffect } from "vue";
+
+// Fetch topics based on language
+const topics = db.tagsWhereTagTypeAsRef(TagType.Topic, { languageId: appLanguageIdAsRef.value });
+
+// Get content of topics
+const topicContent = ref<ContentDto[]>([]);
+
+watchEffect(async () => {
+    const topicId = topics.value.map((t) => t._id);
+    topicContent.value = await db.whereParent(topicId, DocType.Tag, appLanguageIdAsRef.value);
+});
+</script>
+
+<template>
+    <div>
+        <h1 class="mb-8 text-3xl font-medium leading-tight md:text-4xl">Topics</h1>
+    </div>
+
+    <div class="flex flex-wrap gap-4">
+        <div
+            class="ease-out-expo group aspect-video h-32 w-full transition hover:brightness-[1.15] sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5"
+            v-for="topic in topicContent"
+            :key="topic._id"
+        >
+            <RouterLink
+                :to="{ name: 'topic-detail', params: { id: topic.parentId } }"
+                class="avoid-inside hover:shadow-outline ease-out-expo relative flex h-full w-full cursor-pointer items-center justify-center overflow-hidden rounded-lg p-2 transition-transform duration-500 group-hover:scale-[101%]"
+            >
+                <img
+                    :src="topic.parentImage"
+                    class="absolute h-full w-full object-cover object-center opacity-100 transition"
+                />
+                <div class="absolute inset-0 bg-black opacity-50"></div>
+                <h6
+                    class="z-10 content-end break-words text-center text-sm font-bold leading-tight text-white"
+                >
+                    {{ topic.title }}
+                </h6>
+            </RouterLink>
+        </div>
+    </div>
+</template>
