6dffcaf9f9189b44ef3ab48501a21077d6caadaf
Johan Bell <belljohan3@gmail.com>
2024-09-18 20:22:14 +0100

    change the background color

--- app/src/components/navigation/MobileMenu.vue
+++ app/src/components/navigation/MobileMenu.vue
@@ -22,15 +22,15 @@ const commonNavigation = computed(() => {
 });
 </script>
 <template>
-    <div class="flex flex-row justify-center gap-4 bg-slate-500 py-3">
+    <div class="flex flex-row justify-center gap-4 bg-zinc-600 py-3 dark:bg-zinc-500">
         <button
             v-for="item in commonNavigation"
             :key="item.name"
             @click="item.action"
-            class="flex cursor-pointer flex-col items-center rounded-md px-2 py-1 hover:bg-zinc-400"
+            class="flex cursor-pointer flex-col items-center rounded-md px-2 py-1 text-white hover:bg-zinc-400"
         >
             <component :is="item.icon" class="h-6 w-6" />
-            <span class="text-sm font-medium">{{ item.name }}</span>
+            <span class="text-sm font-medium text-white">{{ item.name }}</span>
         </button>
     </div>
 </template>
