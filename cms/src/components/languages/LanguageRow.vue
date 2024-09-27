8964dd0bdcf2048ec6ef6b2f14c75ec100dd50fd
Johan Bell <belljohan3@gmail.com>
2024-09-27 11:26:17 +0100

    add language page

---
+++ cms/src/components/languages/LanguageRow.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { db, DocType, type LanguageDto, AclPermission, verifyAccess } from "luminary-shared";
+import LBadge from "../common/LBadge.vue";
+import { DateTime } from "luxon";
+import LButton from "../button/LButton.vue";
+import { EyeIcon, PencilSquareIcon } from "@heroicons/vue/20/solid";
+
+type props = {
+    languagesDoc: LanguageDto;
+};
+
+defineProps<props>();
+</script>
+
+<template>
+    <tr>
+        <!-- name -->
+        <td class="whitespace-wrap py-2 pl-4 pr-3 text-sm font-medium text-zinc-900 sm:pl-6">
+            {{ languagesDoc.name }}
+        </td>
+
+        <!-- languag code -->
+        <td class="whitespace-nowrap py-2 pl-4 pr-3 text-sm font-medium text-zinc-700 sm:pl-3">
+            <LBadge>{{ languagesDoc.languageCode.toLocaleUpperCase() }} </LBadge>
+        </td>
+
+        <!-- updated -->
+        <td class="whitespace-nowrap py-2 pl-4 pr-3 text-sm font-medium text-zinc-700 sm:pl-3">
+            {{ db.toDateTime(languagesDoc.updatedTimeUtc).toLocaleString(DateTime.DATETIME_SHORT) }}
+        </td>
+
+        <!-- actions -->
+        <td
+            class="flex justify-end whitespace-nowrap py-2 text-sm font-medium text-zinc-700 sm:pl-3"
+        >
+            <LButton
+                v-if="verifyAccess(languagesDoc.memberOf, DocType.Language, AclPermission.View)"
+                variant="tertiary"
+                :icon="
+                    verifyAccess(languagesDoc.memberOf, DocType.Language, AclPermission.Edit)
+                        ? PencilSquareIcon
+                        : EyeIcon
+                "
+                class="flex justify-end"
+                data-test="edit-button"
+            ></LButton>
+        </td>
+    </tr>
+</template>
---
+++ cms/src/components/languages/LanguagesPage.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import BasePage from "@/components/BasePage.vue";
+import LanguageTable from "@/components/languages/LanguageTable.vue";
+import LButton from "../button/LButton.vue";
+import { AclPermission, DocType, hasAnyPermission } from "luminary-shared";
+import { computed } from "vue";
+import { PlusIcon } from "@heroicons/vue/24/outline";
+
+const canCreateNew = computed(() => hasAnyPermission(DocType.Language, AclPermission.Edit));
+</script>
+
+<template>
+    <BasePage title="Language overview">
+        <template #actions>
+            <div class="flex gap-4">
+                <LButton v-if="canCreateNew" variant="primary" :icon="PlusIcon">
+                    Create Language
+                </LButton>
+            </div>
+        </template>
+
+        <LanguageTable />
+    </BasePage>
+</template>
