c86130b2d38122b411a21ca01dc6b529961ba46e
Johan Bell <belljohan3@gmail.com>
2024-10-09 08:47:24 +0100

    update dto in api according to the draw

--- api/src/dto/PostDto.ts
+++ api/src/dto/PostDto.ts
@@ -1,36 +1,12 @@
-import { PostType, Uuid } from "../enums";
-import {
-    IsArray,
-    IsOptional,
-    IsBoolean,
-    IsString,
-    ValidateNested,
-    IsNotEmpty,
-    IsEnum,
-} from "class-validator";
-import { _contentBaseDto } from "./_contentBaseDto";
-import { Expose, Type } from "class-transformer";
-import { ImageDto } from "./ImageDto";
+import { PostType } from "../enums";
+import { IsNotEmpty, IsEnum } from "class-validator";
+import { Expose } from "class-transformer";
+import { _contentParentDto } from "./_contentParentDto";
 
 /**
  * Database structured Post object
  */
-export class PostDto extends _contentBaseDto {
-    @IsOptional()
-    @ValidateNested()
-    @Type(() => ImageDto)
-    @Expose()
-    imageData?: ImageDto;
-
-    @IsArray()
-    @IsString({ each: true })
-    @Expose()
-    tags: Uuid[];
-
-    @IsBoolean()
-    @Expose()
-    publishDateVisible: boolean;
-
+export class PostDto extends _contentParentDto {
     @IsNotEmpty()
     @IsEnum(PostType)
     @Expose()
