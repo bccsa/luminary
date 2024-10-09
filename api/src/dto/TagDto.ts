b9d2122c8fe2a3176b1203f9a7c8f9a59161b9f5
Johan Bell <belljohan3@gmail.com>
2024-10-09 08:47:24 +0100

    update dto in api according to the draw

--- api/src/dto/TagDto.ts
+++ api/src/dto/TagDto.ts
@@ -1,12 +1,12 @@
 import { IsBoolean, IsEnum, IsNotEmpty } from "class-validator";
 import { TagType } from "../enums";
 import { Expose } from "class-transformer";
-import { PostDto } from "./PostDto";
+import { _contentParentDto } from "./_contentParentDto";
 
 /**
  * Database structured Tag object
  */
-export class TagDto extends PostDto {
+export class TagDto extends _contentParentDto {
     @IsNotEmpty()
     @IsEnum(TagType)
     @Expose()
