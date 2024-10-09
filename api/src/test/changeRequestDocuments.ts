616916a32fcfeaad2d1ef85521960e5bad457cf3
Johan Bell <belljohan3@gmail.com>
2024-10-09 09:45:50 +0100

    update changeRequest & processChangeRequest

--- api/src/test/changeRequestDocuments.ts
+++ api/src/test/changeRequestDocuments.ts
@@ -16,6 +16,7 @@ export const changeRequest_post = () => {
             image: randomString(),
             tags: ["tag-category1", "tag-topicA"],
             publishDateVisible: true,
+            postType: "blog",
         },
     };
 };
