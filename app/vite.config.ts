2490b9e054c583737aed087bc19797258fc8affc
Oswald Slabbert <77624298+oslabbie@users.noreply.github.com>
2024-10-11 10:57:29 +0200

    update vite config to copy the analytics service worker to dist (#394)

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,10 +1,21 @@
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: "src/analytics/service-worker.js",
+                    dest: "./src/analytics/",
+                },
+            ],
+        }),
+    ],
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
