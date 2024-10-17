19a2812f76b17fd3b41af494484014c037810c60
Johan Bell <belljohan3@gmail.com>
2024-10-17 11:13:15 +0100

    add EditContentSeo in EditContent

--- cms/src/components/content/EditContent.vue
+++ cms/src/components/content/EditContent.vue
@@ -34,6 +34,7 @@ import ConfirmBeforeLeavingModal from "@/components/modals/ConfirmBeforeLeavingM
 import * as _ from "lodash";
 import router from "@/router";
 import { capitaliseFirstLetter } from "@/util/string";
+import EditContentSeo from "./EditContentSeo.vue";
 
 type Props = {
     id: Uuid;
@@ -287,6 +288,7 @@ watch(selectedLanguage, () => {
                         :disabled="!canTranslate"
                     />
                     <EditContentBasic v-model:content="selectedContent" :disabled="!canTranslate" />
+                    <EditContentSeo v-model:content="selectedContent" :disabled="!canTranslate" />
                     <EditContentText v-model:content="selectedContent" :disabled="!canTranslate" />
                     <EditContentVideo v-model:content="selectedContent" :disabled="!canTranslate" />
                 </div>
