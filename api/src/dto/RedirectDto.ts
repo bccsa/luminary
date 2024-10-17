95d168bf3d6f343a86c1892c2d7774535c17e34e
DirkDevelopmentFreelance <dirkdevbusiness@gmail.com>
2024-10-17 15:16:13 +0200

    Add modifiers to RedirectDto

--- api/src/dto/RedirectDto.ts
+++ api/src/dto/RedirectDto.ts
@@ -1,5 +1,9 @@
+import { IsString } from "class-validator";
 import { ContentDto } from "./ContentDto";
+import { Expose } from "class-transformer";
 
 export class RedirectDto extends ContentDto {
+    @IsString()
+    @Expose()
     redirectSlug: string;
 }
