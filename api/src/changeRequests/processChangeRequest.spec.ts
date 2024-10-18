eff7136d6ba31bd0c8586324ec258601fc607f48
Oswald Slabbert <77624298+oslabbie@users.noreply.github.com>
2024-10-18 14:36:31 +0200

    disabled test on changeDoc's

--- api/src/changeRequests/processChangeRequest.spec.ts
+++ api/src/changeRequests/processChangeRequest.spec.ts
@@ -2,7 +2,8 @@ import "reflect-metadata";
 import { DbService } from "../db/db.service";
 import { createTestingModule } from "../test/testingModule";
 import { processChangeRequest } from "./processChangeRequest";
-import { isDeepStrictEqual } from "util";
+// ******************* tmp disable changeDoc, until it will be implimented on the CMS (ATM the changeDoc's only slow down the api) ********************
+// import { isDeepStrictEqual } from "util";
 import { PermissionSystem } from "../permissions/permissions.service";
 import { changeRequest_content } from "../test/changeRequestDocuments";
 import { S3Service } from "../s3/s3.service";
@@ -35,68 +36,70 @@ describe("processChangeRequest", () => {
         );
     });
 
-    it("can insert a change document into the database for a correctly submitted new document with group membership", async () => {
-        const changeRequest = {
-            id: 84,
-            doc: {
-                _id: "new-language",
-                type: "language",
-                memberOf: ["group-languages"],
-                languageCode: "xho",
-                name: "Xhoza",
-            },
-        };
+    // ******************* tmp disable changeDoc, until it will be implimented on the CMS (ATM the changeDoc's only slow down the api) ********************
+    // it("can insert a change document into the database for a correctly submitted new document with group membership", async () => {
+    //     const changeRequest = {
+    //         id: 84,
+    //         doc: {
+    //             _id: "new-language",
+    //             type: "language",
+    //             memberOf: ["group-languages"],
+    //             languageCode: "xho",
+    //             name: "Xhoza",
+    //         },
+    //     };
 
-        const processResult = await processChangeRequest(
-            "test-user",
-            changeRequest,
-            ["group-super-admins"],
-            db,
-            s3,
-        );
-        const res = await db.getDoc(processResult.id);
+    //     const processResult = await processChangeRequest(
+    //         "test-user",
+    //         changeRequest,
+    //         ["group-super-admins"],
+    //         db,
+    //         s3,
+    //     );
+    //     const res = await db.getDoc(processResult.id);
 
-        // Remove non user-submitted fields
-        delete res.docs[0].changes.updatedTimeUtc;
+    //     // Remove non user-submitted fields
+    //     delete res.docs[0].changes.updatedTimeUtc;
 
-        expect(isDeepStrictEqual(res.docs[0].changes, changeRequest.doc)).toBe(true);
-        expect(res.docs[0].changedByUser).toBe("test-user");
-        expect(res.docs[0].memberOf).toEqual(["group-languages"]);
-    });
+    //     expect(isDeepStrictEqual(res.docs[0].changes, changeRequest.doc)).toBe(true);
+    //     expect(res.docs[0].changedByUser).toBe("test-user");
+    //     expect(res.docs[0].memberOf).toEqual(["group-languages"]);
+    // });
 
-    it("can insert a change document into the database for a correctly submitted new group document", async () => {
-        const changeRequest = {
-            id: 85,
-            doc: {
-                _id: "new-group",
-                type: "group",
-                acl: [
-                    {
-                        type: "language",
-                        groupId: "group-languages",
-                        permission: ["view", "publish", "edit"],
-                    },
-                ],
-                name: "new group",
-            },
-        };
+    // ******************* tmp disable changeDoc, until it will be implimented on the CMS (ATM the changeDoc's only slow down the api) ********************
+    // it("can insert a change document into the database for a correctly submitted new group document", async () => {
+    //     const changeRequest = {
+    //         id: 85,
+    //         doc: {
+    //             _id: "new-group",
+    //             type: "group",
+    //             acl: [
+    //                 {
+    //                     type: "language",
+    //                     groupId: "group-languages",
+    //                     permission: ["view", "publish", "edit"],
+    //                 },
+    //             ],
+    //             name: "new group",
+    //         },
+    //     };
 
-        const processResult = await processChangeRequest(
-            "test-user",
-            changeRequest,
-            ["group-super-admins"],
-            db,
-            s3,
-        );
-        const res = await db.getDoc(processResult.id);
+    //     const processResult = await processChangeRequest(
+    //         "test-user",
+    //         changeRequest,
+    //         ["group-super-admins"],
+    //         db,
+    //         s3,
+    //     );
+    //     const res = await db.getDoc(processResult.id);
 
-        // Remove non user-submitted fields
-        delete res.docs[0].changes.updatedTimeUtc;
+    //     // Remove non user-submitted fields
+    //     delete res.docs[0].changes.updatedTimeUtc;
 
-        expect(isDeepStrictEqual(res.docs[0].changes, changeRequest.doc)).toBe(true);
-        expect(res.docs[0].changedByUser).toBe("test-user");
-        expect(isDeepStrictEqual(res.docs[0].acl, changeRequest.doc.acl)).toBe(true);
-    });
+    //     expect(isDeepStrictEqual(res.docs[0].changes, changeRequest.doc)).toBe(true);
+    //     expect(res.docs[0].changedByUser).toBe("test-user");
+    //     expect(isDeepStrictEqual(res.docs[0].acl, changeRequest.doc.acl)).toBe(true);
+    // });
 
     it("is not creating a change request when updating a document to the same content", async () => {
         const changeRequest1 = {
