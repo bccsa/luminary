247c70865851a728b8c624fb6fc3118acc8df47d
DirkDevelopmentFreelance <dirkdevbusiness@gmail.com>
2024-10-21 10:22:38 +0200

    Update Datamodel

--- api/src/dto/RedirectDto.ts
+++ api/src/dto/RedirectDto.ts
@@ -1,9 +1,13 @@
 import { IsString } from "class-validator";
 import { ContentDto } from "./ContentDto";
 import { Expose } from "class-transformer";
+import { GroupDto } from "./GroupDto";
 
 export class RedirectDto extends ContentDto {
     @IsString()
     @Expose()
     redirectSlug: string;
+
+    @Expose()
+    membership: GroupDto;
 }
