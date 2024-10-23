2d21be861ee64240783a435375483014c74ef32f
Dirk <dirkdevbusiness@gmail.com>
2024-10-23 13:34:09 +0200

    Update cms/src/App.vue
    Co-authored-by: Ivan Slabbert <ivanslabbert@users.noreply.github.com>

--- cms/src/App.vue
+++ cms/src/App.vue
@@ -77,7 +77,7 @@ onBeforeMount(async () => {
             if (data.ack == "rejected") {
                 useNotificationStore().addNotification({
                     title: "Saving changes to server failed.",
-                    description: `Your recent request to save changes has failed. The changes have been reverted, both in your CMS and on the server. Error message: ${data.message}`,
+                    description: `Your recent request to save changes has failed. The changes have been reverted. Error message: ${data.message}`,
                     state: "error",
                     timer: 60000,
                 });
