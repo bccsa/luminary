39f4a8a9214c26559af5f4c787d0e453aad196f0
DirkDevelopmentFreelance <dirkdevbusiness@gmail.com>
2024-10-28 11:18:56 +0200

    Make logo changes

--- app/src/components/navigation/TopBar.vue
+++ app/src/components/navigation/TopBar.vue
@@ -3,17 +3,33 @@ import { ChevronLeftIcon } from "@heroicons/vue/24/solid";
 import ProfileMenu from "./ProfileMenu.vue";
 import { useRoute, useRouter } from "vue-router";
 import DesktopMenu from "./DesktopMenu.vue";
+import { computed, onMounted, ref } from "vue";
 
 const route = useRoute();
 const router = useRouter();
 const LOGO = import.meta.env.VITE_LOGO;
 const LOGO_SMALL = import.meta.env.VITE_LOGO_SMALL;
+
+// Reactive screen size check
+const isSmallScreen = ref(false);
+
+const logo = computed(() => (isSmallScreen.value ? LOGO_SMALL : LOGO));
+
+// Detect screen size on load and window resize
+const updateScreenSize = () => {
+    isSmallScreen.value = window.innerWidth < 300; // Tailwind 'md' breakpoint
+};
+
+onMounted(() => {
+    updateScreenSize();
+    window.addEventListener("resize", updateScreenSize);
+});
 </script>
 
 <template>
     <header>
         <div class="z-40 bg-zinc-100 dark:bg-slate-800">
-            <div class="flex flex-row items-center justify-between space-x-8 px-4 py-3">
+            <div class="flex flex-row items-center justify-between px-4 py-3">
                 <div @click="router.back()" class="flex items-center">
                     <div
                         class="mr-4 border-r border-zinc-400 pr-4"
@@ -25,9 +41,7 @@ const LOGO_SMALL = import.meta.env.VITE_LOGO_SMALL;
                         <ChevronLeftIcon class="h-6 w-6 text-zinc-600 dark:text-slate-50" />
                     </div>
 
-                    <div class="-ml-8 hidden items-center whitespace-nowrap min-[300px]:flex">
-                        <img :src="LOGO" />
-                    </div>
+                    <img :src="logo" />
                 </div>
 
                 <DesktopMenu class="hidden w-2/3 gap-2 lg:flex" />
