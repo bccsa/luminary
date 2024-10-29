e7529f17e521c494fc70dda3b133a135cfd53323
Johan Bell <50105980+belljohan3@users.noreply.github.com>
2024-10-29 13:56:28 +0100

    417 add seo title and summary fields (#427)

--- cms/src/components/content/EditContentBasic.spec.ts
+++ cms/src/components/content/EditContentBasic.spec.ts
@@ -52,4 +52,61 @@ describe("EditContentBasic.vue", () => {
         // Check if the content's summary was updated
         expect(content.value.summary).toBe("Updated Summary");
     });
+
+    it("can update the slug", async () => {
+        const content = ref<ContentDto>(mockData.mockEnglishContentDto);
+        const wrapper = mount(EditContentBasic, {
+            props: {
+                disabled: false,
+                content: content.value,
+            },
+        });
+
+        // Find and update the slug input field
+        const slugInput = wrapper.find('[name="slug"]');
+        await slugInput.setValue("updated-slug");
+
+        // Check if the content's slug was updated
+        expect(content.value.slug).toBe("updated-slug");
+    });
+
+    it("can update the seo title", async () => {
+        const content = ref<ContentDto>(mockData.mockEnglishContentDto);
+        const wrapper = mount(EditContentBasic, {
+            props: {
+                disabled: false,
+                content: content.value,
+            },
+        });
+
+        // Find and update the title input field
+        const options = wrapper.findAll("option");
+        await options[1].trigger("click");
+
+        const titleInput = wrapper.find('[name="seo-title"]');
+        await titleInput.setValue("Updated Seo Title");
+
+        // // Check if the content's title was updated
+        expect(content.value.seoTitle).toBe("Updated Seo Title");
+    });
+
+    it("can update the seo summary", async () => {
+        const content = ref<ContentDto>(mockData.mockEnglishContentDto);
+        const wrapper = mount(EditContentBasic, {
+            props: {
+                disabled: false,
+                content: content.value,
+            },
+        });
+
+        const options = wrapper.findAll("option");
+        await options[1].trigger("click");
+
+        // Find and update the summary input field
+        const summaryInput = wrapper.find('[name="seo-summary"]');
+        await summaryInput.setValue("Updated Seo Summary");
+
+        // Check if the content's summary was updated
+        expect(content.value.seoString).toBe("Updated Seo Summary");
+    });
 });
