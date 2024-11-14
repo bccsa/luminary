1614c87084553c78f0ee142076d3804a6f1b993a
Oswald Slabbert <77624298+oslabbie@users.noreply.github.com>
2024-11-14 12:29:55 +0200

    fix app and cms test to pass index along

--- app/vitest.setup.ts
+++ app/vitest.setup.ts
@@ -5,6 +5,9 @@ import { initLuminaryShared } from "luminary-shared";
 
 config.global.stubs["RouterLink"] = RouterLinkStub;
 
-beforeAll(() => {
-    initLuminaryShared({ cms: false });
+beforeAll(async () => {
+    await initLuminaryShared(
+        { cms: false },
+        "type, parentId, updatedTimeUtc, slug, language, docType, redirect, [parentId+type], [parentId+parentType], [type+tagType], publishDate, expiryDate, [type+language+status+parentPinned], [type+language+status], [type+postType], [type+docType], title, parentPinned",
+    );
 });
--- cms/vitest.setup.ts
+++ cms/vitest.setup.ts
@@ -5,6 +5,9 @@ import { beforeAll } from "vitest";
 
 config.global.stubs["RouterLink"] = RouterLinkStub;
 
-beforeAll(() => {
-    initLuminaryShared({ cms: true });
+beforeAll(async () => {
+    await initLuminaryShared(
+        { cms: true },
+        "type, parentId, updatedTimeUtc, slug, language, docType, redirect, [parentId+type], [parentId+parentType], [type+tagType], publishDate, expiryDate, [type+language+status+parentPinned], [type+language+status], [type+postType], [type+docType], title, parentPinned",
+    );
 });
