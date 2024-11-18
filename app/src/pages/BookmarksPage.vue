8c8d6c8e8f8cd53205286a4f51f5f6c75d0aa6f7
DirkDevFreelance <dirkdevbusiness@gmail.com>
2024-11-18 17:13:36 +0200

    Display bookmarks

--- app/src/pages/BookmarksPage.vue
+++ app/src/pages/BookmarksPage.vue
@@ -1,7 +1,15 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import ContentTile from "@/components/content/ContentTile.vue";
+import { appLanguageIdAsRef, userPreferences } from "@/globalConfig";
+import { db } from "luminary-shared";
+
+const bookmarkKeys = Object.keys(userPreferences.value.bookmarks);
+const bookmarkContent = db.whereParentAsRef(bookmarkKeys, undefined, appLanguageIdAsRef.value);
+</script>
 
 <template>
     <div>
         <h1>Bookmarks Page</h1>
+        <ContentTile v-for="content in bookmarkContent" :key="content._id" :content="content" />
     </div>
 </template>
