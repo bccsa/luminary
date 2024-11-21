59cbf299055c64394d740a403b1dbb551d2f78e0
Dirk <dirkdevbusiness@gmail.com>
2024-11-21 14:44:18 +0200

    Fix LModal test

--- app/src/components/form/LModal.spec.ts
+++ app/src/components/form/LModal.spec.ts
@@ -1,7 +1,6 @@
 import { mount } from "@vue/test-utils";
 import { describe, expect, it } from "vitest";
 import LModal from "./LModal.vue";
-import waitForExpect from "wait-for-expect";
 
 describe("LModal", () => {
     describe("display", () => {
@@ -13,12 +12,7 @@ describe("LModal", () => {
                 },
             });
 
-            const body = document.querySelector("body");
-
-            await wrapper.vm.$nextTick();
-
-            await waitForExpect(() => {});
-            expect(body!.innerHTML).toContain("Test Heading");
+            expect(wrapper.html()).toContain("Test Heading");
         });
     });
 });
