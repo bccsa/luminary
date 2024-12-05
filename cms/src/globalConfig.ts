6af964dafadfe28388a971da7c0f82268771345f
Johan Bell <belljohan3@gmail.com>
2024-12-05 23:56:50 +0200

    small fix

--- cms/src/globalConfig.ts
+++ cms/src/globalConfig.ts
@@ -2,7 +2,7 @@ import { ref, watch } from "vue";
 
 export const appName = import.meta.env.VITE_APP_NAME;
 export const apiUrl = import.meta.env.VITE_API_URL;
-export const clientAppUrl = import.meta.env.VITE_CLIENT_APP_URL;
+export const clientAppUrl = ref(import.meta.env.VITE_CLIENT_APP_URL);
 export const logo = import.meta.env.VITE_LOGO;
 export const isDevMode = import.meta.env.DEV;
 
