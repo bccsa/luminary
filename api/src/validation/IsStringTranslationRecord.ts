7914c4d7b568eb3084b734c5e7a1ba5bea28f12d
Johan Bell <belljohan3@gmail.com>
2024-12-10 16:00:32 +0200

    add `IsStringTranslationRecord.ts` validation file

---
+++ api/src/validation/IsStringTranslationRecord.ts
@@ -0,0 +1,16 @@
+import { registerDecorator } from "class-validator";
+
+export function IsStringTranslationRecord() {
+    return function (object: object, propertyName: string) {
+        registerDecorator({
+            name: "isStringTranslationRecord",
+            target: object.constructor,
+            propertyName: propertyName,
+            validator: {
+                validate(value: string) {
+                    return typeof value === "string" || Array.isArray(value);
+                },
+            },
+        });
+    };
+}
