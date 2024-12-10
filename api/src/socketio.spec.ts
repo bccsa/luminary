e97e980bb4e37f883071491e61bc94b1bd1e990e
Johan Bell <belljohan3@gmail.com>
2024-12-10 09:59:00 +0200

    fix `socketio.spec` file

--- api/src/socketio.spec.ts
+++ api/src/socketio.spec.ts
@@ -62,6 +62,10 @@ describe("Socketio", () => {
                     memberOf: ["group-languages"],
                     languageCode: "eng",
                     name: "English",
+                    default: 1,
+                    translations: {
+                        stringTranslation: "String Translation",
+                    },
                 },
             };
 
