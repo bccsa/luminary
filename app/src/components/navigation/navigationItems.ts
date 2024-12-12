677ca5bd0b48c3d75888b3c47e6fca679399a168
Johan Bell <50105980+belljohan3@users.noreply.github.com>
2024-12-12 10:00:00 +0200

    Hide "Explore" navigation item temporarily based on environment variable (#580)

--- app/src/components/navigation/navigationItems.ts
+++ app/src/components/navigation/navigationItems.ts
@@ -4,7 +4,7 @@ import {
     HomeIcon as FilledHomeIcon,
 } from "@heroicons/vue/24/solid";
 
-export const commonNavigation = [
+const navigationItems = [
     {
         name: "Home",
         defaultIcon: HomeIcon,
@@ -18,3 +18,11 @@ export const commonNavigation = [
         to: { name: "explore" },
     },
 ];
+
+// Exclude the "Explore" navigation item if the eenv variable "VITE_HIDE_EXPLORE" is set to "true"
+// Note that this is a temporary solution to hide the "Explore" navigation item
+
+export const commonNavigation =
+    import.meta.env.VITE_HIDE_EXPLORE === "true"
+        ? navigationItems.filter((item) => item.name !== "Explore")
+        : navigationItems;
