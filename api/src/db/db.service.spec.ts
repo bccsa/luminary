a6703c31bacd52af9e01784b1135db768bb9cc9f
Oswald Slabbert <77624298+oslabbie@users.noreply.github.com>
2025-01-06 08:58:22 +0000

    fix tests

--- api/src/db/db.service.spec.ts
+++ api/src/db/db.service.spec.ts
@@ -177,7 +177,7 @@ describe("DbService", () => {
         expect(docCount).toBe(5);
     });
 
-    it("can retrieve documents from a given time", async () => {
+    it.skip("can retrieve documents from a given time", async () => {
         // This test is not valid anymore as sync tolerance is implemented with a default value of 1000ms. Probably need a bigger dataset to effectively test this.
         // Update a document and query documents from a timestamp just before the updated time.
         // Only one document should be returned (which is newer than the update time)
--- api/src/docs/docs.service.spec.ts
+++ api/src/docs/docs.service.spec.ts
@@ -31,14 +31,12 @@ describe("Docs service", () => {
     let accessMap: AccessMap;
 
     beforeAll(async () => {
-        service = (await createTestingModule("db-service")).dbService;
+        service = (await createTestingModule("docs-service")).dbService;
         docsService = new DocsService(undefined, service);
         // Initialize permission system
         await PermissionSystem.init(service);
         accessMap = PermissionSystem.getAccessMap(["group-languages", "group-public-content"]);
     });
-
-    afterAll(async () => {});
 
     it("can query the api endpoint", async () => {
         const req = {
@@ -68,46 +66,46 @@ describe("Docs service", () => {
         expect(res.docs.length).toBe(8);
     });
 
-    // it("can get post data from api endpoint", async () => {
-    //     const req = {
-    //         apiVersion: "0.0.0",
-    //         gapEnd: 0,
-    //         cms: true,
-    //         accessMap: accessMap,
-    //         type: DocType.Post,
-    //     };
+    it("can get post data from api endpoint", async () => {
+        const req = {
+            apiVersion: "0.0.0",
+            gapEnd: 0,
+            cms: true,
+            accessMap: accessMap,
+            type: DocType.Post,
+        };
 
-    //     const res = await docsService.processReq(req, "");
+        const res = await docsService.processReq(req, "");
 
-    //     expect(res.docs.length).toBe(8);
-    // });
+        expect(res.docs.length).toBe(5);
+    });
 
-    // it("can get tag data from api endpoint", async () => {
-    //     const req = {
-    //         apiVersion: "0.0.0",
-    //         gapEnd: 0,
-    //         cms: true,
-    //         accessMap: accessMap,
-    //         type: DocType.Tag,
-    //     };
+    it("can get tag data from api endpoint", async () => {
+        const req = {
+            apiVersion: "0.0.0",
+            gapEnd: 0,
+            cms: true,
+            accessMap: accessMap,
+            type: DocType.Tag,
+        };
 
-    //     const res = await docsService.processReq(req, "");
+        const res = await docsService.processReq(req, "");
 
-    //     expect(res.docs.length).toBe(8);
-    // });
+        expect(res.docs.length).toBe(4);
+    });
 
-    // it("can get content data from api endpoint", async () => {
-    //     const req = {
-    //         apiVersion: "0.0.0",
-    //         gapEnd: 0,
-    //         cms: true,
-    //         accessMap: accessMap,
-    //         type: DocType.Post,
-    //         contentOnly: true,
-    //     };
+    it("can get content data from api endpoint", async () => {
+        const req = {
+            apiVersion: "0.0.0",
+            gapEnd: 0,
+            cms: true,
+            accessMap: accessMap,
+            type: DocType.Post,
+            contentOnly: true,
+        };
 
-    //     const res = await docsService.processReq(req, "");
+        const res = await docsService.processReq(req, "");
 
-    //     expect(res.docs.length).toBe(8);
-    // });
+        expect(res.docs.length).toBe(18);
+    });
 });
