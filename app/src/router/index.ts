fdabb1e0918967656bf78755e34d5ccf23198b6d
Johan Bell <belljohan3@gmail.com>
2025-01-07 10:31:02 +0100

    Add VideoPage route to the application

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -7,6 +7,7 @@ import SingleContent from "@/pages/SingleContent.vue";
 import { appName } from "@/globalConfig";
 import ExplorePage from "@/pages/ExplorePage.vue";
 import BookmarksPage from "@/pages/BookmarksPage.vue";
+import VideoPage from "@/pages/VideoPage.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -33,6 +34,14 @@ const router = createRouter({
             name: "explore",
             meta: {
                 title: "Explore",
+            },
+        },
+        {
+            path: "/video",
+            component: VideoPage,
+            name: "video",
+            meta: {
+                title: "Video",
             },
         },
         {
