17d352db26e0b51cae78bc56cb2d63fa0bdc2991
Oswald Slabbert <77624298+oslabbie@users.noreply.github.com>
2025-01-14 12:13:26 +0200

    added class validation on request

--- api/src/docs/docs.controller.ts
+++ api/src/docs/docs.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Headers, Get } from "@nestjs/common";
+import { Controller, Headers, Get, HttpException, HttpStatus } from "@nestjs/common";
 import { DocsReqDto } from "../dto/DocsReqDto";
 import { DocsService } from "./docs.service";
-import { Validate } from "class-validator";
+import { validateSync } from "class-validator";
 
 @Controller("docs")
 export class DocsController {
@@ -12,7 +12,15 @@ export class DocsController {
         @Headers("X-Query") doc: string,
         @Headers("Authorization") auth: string,
     ): Promise<any> {
-        Validate(DocsReqDto);
+        if (!doc) {
+            throw new HttpException(`X-Query header is required`, HttpStatus.BAD_REQUEST);
+        }
+        const docObj = JSON.parse(doc);
+        const docsReqDto = Object.assign(new DocsReqDto(), docObj);
+        const errors = validateSync(docsReqDto);
+        if (errors.length > 0) {
+            throw new HttpException(`Validation failed: ${errors}`, HttpStatus.BAD_REQUEST);
+        }
         return this.docsService.processReq(
             JSON.parse(doc),
             auth !== undefined ? auth.replace("Bearer ", "") : "",
