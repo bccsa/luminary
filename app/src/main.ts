3493bdfcbfe19852ca9f8d02a83950ccef9b68d4
Ivan Slabbert <ivanslabbert@users.noreply.github.com>
2025-01-14 16:16:25 +0200

    Only load Vue app when languages have been initialized

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,46 +1,24 @@
 import "./assets/main.css";
-
-import { createApp, watch } from "vue";
+import { createApp, ref, watch } from "vue";
 import { createPinia } from "pinia";
-
-// import { createAuth0 } from "@auth0/auth0-vue";
 import * as Sentry from "@sentry/vue";
-
 import App from "./App.vue";
 import router from "./router";
 import auth from "./auth";
-import { initLuminaryShared } from "luminary-shared";
+import {
+    db,
+    DocType,
+    initLuminaryShared,
+    useDexieLiveQuery,
+    type LanguageDto,
+} from "luminary-shared";
 // @ts-expect-error matomo does not have a typescript definition file
 import VueMatomo from "vue-matomo";
 import { loadPlugins } from "./util/pluginLoader";
 import { createI18n } from "vue-i18n";
-import { appLanguageAsRef } from "./globalConfig";
+import { appLanguageAsRef, initLanguage } from "./globalConfig";
 
-// Initialize i18n with empty messages
-const i18n = createI18n({
-    legacy: false,
-    locale: appLanguageAsRef.value?.languageCode || "en", // Default locale
-    fallbackLocale: appLanguageAsRef.value?.languageCode || "en", // Fallback locale
-    messages: {}, // Empty messages to start
-});
-
-// Watch `appLanguageAsRef` for changes and update i18n dynamically
-watch(
-    appLanguageAsRef,
-    (newLanguage) => {
-        const { languageCode, translations } = newLanguage || {};
-
-        if (languageCode && translations) {
-            // Add new translations to i18n
-            i18n.global.setLocaleMessage(languageCode, translations);
-
-            // Change the active locale
-            i18n.global.locale.value = languageCode;
-        }
-    },
-    { immediate: true },
-);
-
+// Startup
 if (import.meta.env.VITE_FAV_ICON) {
     const favicon = document.getElementById("favicon") as HTMLLinkElement;
     if (favicon) {
@@ -58,12 +36,6 @@ if (import.meta.env.PROD) {
     });
 }
 
-app.use(createPinia());
-
-app.use(router);
-app.use(i18n);
-
-// Startup
 async function Startup() {
     await initLuminaryShared({
         cms: false,
@@ -77,10 +49,79 @@ async function Startup() {
     // wait to load plugins before mounting the app
     await loadPlugins();
 
-    app.mount("#app");
+    initLanguage();
 }
+await Startup();
 
-Startup();
+// Initialize i18n with empty messages
+const i18n = createI18n({
+    legacy: false,
+    locale: appLanguageAsRef.value?.languageCode || "en", // Default locale
+    messages: {}, // Empty messages to start
+});
+
+// Get default language document. This should move to globalConfig.ts
+const defaultLanguage = useDexieLiveQuery(
+    () =>
+        db.docs
+            .where("type")
+            .equals(DocType.Language)
+            .and((doc) => {
+                const languageDoc = doc as LanguageDto;
+                return languageDoc.default == 1;
+            })
+            .first() as unknown as Promise<LanguageDto>,
+);
+
+// Create a list of localised strings with fallback to the default language if not existing in the preferred language
+const localeInitDone = ref(false);
+watch(
+    [appLanguageAsRef, defaultLanguage],
+    ([newLanguage, defaultLang]) => {
+        // TODO: This watcher is triggering multiple times on app startup. Need to investigate why
+
+        if (!newLanguage) return;
+        // copy translations in the preferred language
+        const messages: Record<string, string> = {};
+        Object.keys(newLanguage.translations).forEach((k: string) => {
+            messages[k] = newLanguage.translations[k];
+        });
+
+        // Fill in missing translations with default language strings
+        if (defaultLang && defaultLang.translations && newLanguage._id != defaultLang._id) {
+            Object.keys(defaultLang.translations).forEach((k: string) => {
+                if (!messages[k]) {
+                    messages[k] = defaultLang.translations[k];
+                }
+            });
+        }
+
+        // Add new translations to i18n
+        i18n.global.setLocaleMessage(newLanguage.languageCode, messages);
+
+        // Change the active locale
+        i18n.global.locale.value = newLanguage.languageCode;
+
+        localeInitDone.value = true;
+    },
+    { immediate: true },
+);
+
+app.use(createPinia());
+app.use(router);
+app.use(i18n);
+
+// Wait for locale to be initialized before mounting the app
+const appStartupHandler = watch(
+    localeInitDone,
+    (done) => {
+        if (done) {
+            app.mount("#app");
+            appStartupHandler(); // Stop watching after app is mounted
+        }
+    },
+    { immediate: true },
+);
 
 // Matomo Analytics
 if (import.meta.env.VITE_ANALYTICS_HOST && import.meta.env.VITE_ANALYTICS_SITEID)
