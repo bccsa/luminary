29f76172b819972742130065bde413cdcb2aeab6
Oswald Slabbert <77624298+oslabbie@users.noreply.github.com>
2025-01-14 10:10:59 +0200

    remove post on client side

--- shared/src/rest/http.ts
+++ shared/src/rest/http.ts
@@ -7,35 +7,6 @@ export class httpReq {
         this.apiUrl = apiUrl;
     }
 
-    async post(endpoint: string, body: any, isJSON: boolean = true) {
-        const headers: any = isJSON
-            ? {
-                  Accept: "application/json",
-                  "Content-Type": "application/json; charset=utf-8",
-              }
-            : {};
-        this.token && (headers.Authorization = `Bearer ${this.token}`);
-
-        try {
-            const schema = "https://";
-            const regex = /^https?:\/\//;
-            const url = regex.test(this.apiUrl) ? this.apiUrl : `${schema}${this.apiUrl}`;
-            const res = await fetch(`${url}/${endpoint}`, {
-                method: "POST",
-                headers: headers,
-                body: isJSON ? JSON.stringify(body) : body, // Convert the data to a JSON string
-            });
-            if (!res.ok) {
-                throw new Error(`HTTP error! Status: ${res.status}`);
-            }
-            return await res.json().catch((err) => {
-                console.error(err);
-            }); // Parse the JSON response
-        } catch (err) {
-            console.error(err);
-        }
-    }
-
     async get(endpoint: string, query: any) {
         const headers: any = {
             "X-Query": JSON.stringify(query),
