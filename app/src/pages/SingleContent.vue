b069e765150c4462fdef752a89a882445de65d4c
DirkDevelopmentFreelance <dirkdevbusiness@gmail.com>
2025-01-28 14:01:45 +0200

    Fix test?

--- app/src/pages/SingleContent.vue
+++ app/src/pages/SingleContent.vue
@@ -183,7 +183,7 @@ watch([content, is404], () => {
 });
 
 watch(
-    [appLanguagesPreferredAsRef, content],
+    [appLanguagesPreferredAsRef, content.value],
     async () => {
         if (!content.value) return;
         if (!content.value.language) return;
