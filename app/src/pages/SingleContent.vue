3fb653d9014f75d33d0f8c1c1301aa60db7fa8f8
Johan Bell <belljohan3@gmail.com>
2025-01-28 15:46:09 +0100

    Update modal max height calculation in SingleContent.vue

--- app/src/pages/SingleContent.vue
+++ app/src/pages/SingleContent.vue
@@ -365,7 +365,7 @@ const selectedCategory = computed(() => {
         @close="showCategoryModal = false"
         :heading="selectedCategory?.title || ''"
     >
-        <div class="max-h-[80vh] overflow-y-auto">
+        <div class="max-h-[calc(80%)] overflow-y-auto">
             <div class="">
                 <VerticalTagViewer v-if="selectedCategory" :tag="selectedCategory" class="" />
             </div>
