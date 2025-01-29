17c8da00971344f91bfa9dded8d7f1dcd73fb7d0
Johan Bell <belljohan3@gmail.com>
2025-01-29 09:57:23 +0100

    fix: improve styling of FormLabel component for better dark mode support

--- app/src/components/form/FormLabel.vue
+++ app/src/components/form/FormLabel.vue
@@ -10,7 +10,10 @@ const props = withDefaults(defineProps<Props>(), {
 </script>
 
 <template>
-    <label :for="props.for" class="flex gap-1 text-sm font-medium leading-6 text-zinc-900">
+    <label
+        :for="props.for"
+        class="flex gap-1 text-sm font-medium leading-6 text-zinc-900 dark:text-slate-100"
+    >
         <span><slot /></span>
         <span v-if="required">
             <span aria-hidden="true">*</span> <span class="sr-only">Required</span>
