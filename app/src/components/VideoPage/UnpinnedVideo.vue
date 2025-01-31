4860214e48d8252f7a3e3d6be44ae38125aafb65
Johan Bell <belljohan3@gmail.com>
2025-01-31 15:05:20 +0100

    Fix VideoPage to show pinned and unpinned categories with topics content to use multiplie language support feat

--- app/src/components/VideoPage/UnpinnedVideo.vue
+++ app/src/components/VideoPage/UnpinnedVideo.vue
@@ -1,97 +1,89 @@
 <script setup lang="ts">
-import { computed, watch } from "vue";
+import { watch } from "vue";
 import {
     type ContentDto,
     DocType,
-    db,
-    type Uuid,
-    useDexieLiveQueryWithDeps,
-    TagType,
     PostType,
+    PublishStatus,
+    TagType,
+    type Uuid,
+    db,
+    useDexieLiveQueryWithDeps,
 } from "luminary-shared";
 import { appLanguageIdsAsRef } from "@/globalConfig";
-import HorizontalContentTileCollection from "@/components/content/HorizontalContentTileCollection.vue";
 import { contentByTag } from "../contentByTag";
+import HorizontalContentTileCollection from "@/components/content/HorizontalContentTileCollection.vue";
 import { isPublished } from "@/util/isPublished";
 import IgnorePagePadding from "../IgnorePagePadding.vue";
 
-const unpinnedContent = useDexieLiveQueryWithDeps(
+const unpinnedCategories = useDexieLiveQueryWithDeps(
     appLanguageIdsAsRef,
-    (appLanguageId) =>
+    (appLanguageIds: Uuid[]) =>
         db.docs
-            .orderBy("publishDate")
-            .reverse()
+            .where({
+                type: DocType.Content,
+                parentPinned: 0, // 1 = true
+            })
             .filter((c) => {
-                const content = c as ContentDto;
-                if (content.type !== DocType.Content) return false;
-                if (content.parentPostType && content.parentPostType == PostType.Page) return false;
-                if (content.parentTagType && content.parentTagType !== TagType.Topic) return false;
-                if (content.language !== appLanguageId) return false;
-                if (!content.video) return false;
-
-                // Only include published content
-                return isPublished(content, [appLanguageId]);
-            })
-            .limit(50)
-            .toArray() as unknown as Promise<ContentDto[]>,
-    { initialValue: await db.getQueryCache<ContentDto[]>("videopage_unpinnedContent") },
-);
-
-watch(unpinnedContent, async (value) => {
-    db.setQueryCache<ContentDto[]>("videopage_unpinnedContent", value);
-});
-
-const categoryIds = computed(() =>
-    unpinnedContent.value
-        .map((content) => content.parentTags)
-        .flat()
-        .filter((value, index, array) => {
-            return array.indexOf(value) === index;
-        }),
-);
-
-const categories = useDexieLiveQueryWithDeps(
-    [categoryIds, appLanguageIdsAsRef],
-    ([_categoryIds, appLanguageId]: [Uuid[], Uuid]) =>
-        db.docs
-            .where("parentId")
-            .anyOf(_categoryIds)
-            .filter((content) => {
-                const _content = content as ContentDto;
-                if (_content.parentType !== DocType.Tag) return false;
-                if (!_content.parentTagType) return false;
-                if (_content.parentPinned) return false;
-
-                // Use the `isPublished` helper function
-                return (
-                    isPublished(_content, [appLanguageId]) &&
-                    _content.parentTagType === TagType.Category &&
-                    _content.language === appLanguageId
-                );
+                return isPublished(c as ContentDto, appLanguageIds);
             })
             .toArray() as unknown as Promise<ContentDto[]>,
-    { initialValue: await db.getQueryCache<ContentDto[]>("videopage_unpinnedCategories") },
-);
-
-watch(
-    () => categories.value,
-    (value) => {
-        db.setQueryCache<ContentDto[]>("videopage_unpinnedCategories", value);
+    {
+        initialValue: await db.getQueryCache<ContentDto[]>("videopage_unpinnedCategories"),
+        deep: true,
     },
 );
 
-const unpinnedNewestContentByCategory = contentByTag(unpinnedContent, categories);
+watch(unpinnedCategories, async (value) => {
+    db.setQueryCache<ContentDto[]>("videopage_unpinnedCategories", value);
+});
+
+const unpinnedCategoryContent = useDexieLiveQueryWithDeps(
+    [appLanguageIdsAsRef, unpinnedCategories],
+    ([appLanguageIds, pinnedCategories]: [Uuid[], ContentDto[]]) =>
+        db.docs
+            .where({
+                type: DocType.Content,
+                status: PublishStatus.Published,
+            })
+            .filter((c) => {
+                const content = c as ContentDto;
+                if (!content.video) return false;
+
+                if (content.parentPostType && content.parentPostType == PostType.Page) return false;
+                if (content.parentTagType && content.parentTagType !== TagType.Topic) return false;
+
+                for (const tagId of content.parentTags) {
+                    if (
+                        pinnedCategories.some((p) => p.parentId == tagId) &&
+                        isPublished(content, appLanguageIds)
+                    )
+                        return true;
+                }
+
+                return false;
+            })
+            .toArray() as unknown as Promise<ContentDto[]>,
+    { initialValue: await db.getQueryCache<ContentDto[]>("videopage_pinnedContent") },
+);
+
+watch(unpinnedCategoryContent, async (value) => {
+    db.setQueryCache<ContentDto[]>("videopage_pinnedContent", value);
+});
+
+// sort pinned content by category
+const unpinnedContentByCategory = contentByTag(unpinnedCategoryContent, unpinnedCategories);
 </script>
 
 <template>
     <IgnorePagePadding>
         <HorizontalContentTileCollection
-            v-for="c in unpinnedNewestContentByCategory"
+            v-for="c in unpinnedContentByCategory"
             :key="c.tag._id"
             :contentDocs="c.content"
             :title="c.tag.title"
             :summary="c.tag.summary"
-            class="pt-2"
+            :showPublishDate="false"
         />
     </IgnorePagePadding>
 </template>
