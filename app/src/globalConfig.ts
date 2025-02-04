01d9a1c3be5cfb2fa0543cb4436f38f850b3ce5b
Ivan Slabbert <ivanslabbert@users.noreply.github.com>
2025-02-04 09:58:18 +0200

    remove unused references

--- app/src/globalConfig.ts
+++ app/src/globalConfig.ts
@@ -1,5 +1,5 @@
 import { db, DocType, useDexieLiveQuery, type LanguageDto, type Uuid } from "luminary-shared";
-import { computed, ref, watch, type ShallowRef } from "vue";
+import { computed, ref, watch } from "vue";
 
 export const appName = import.meta.env.VITE_APP_NAME;
 export const apiUrl = import.meta.env.VITE_API_URL;
