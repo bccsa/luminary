57e6254a91562a7771b3f844d95c5208ff75ee3d
Johan Bell <belljohan3@gmail.com>
2025-02-17 19:04:26 +0100

    Add unit tests for UserRow component to verify user display and formatting

---
+++ cms/src/components/users/UserRow.spec.ts
@@ -0,0 +1,41 @@
+import "fake-indexeddb/auto";
+import { describe, it, expect, beforeEach, afterEach } from "vitest";
+import { mount } from "@vue/test-utils";
+import UserRow from "./UserRow.vue";
+import { setActivePinia } from "pinia";
+import { createTestingPinia } from "@pinia/testing";
+import { accessMap, db } from "luminary-shared";
+import { fullAccessToAllContentMap, mockLanguageDtoEng, mockUserDto } from "@/tests/mockdata";
+import { DateTime } from "luxon";
+
+describe("UserRow.vue", () => {
+    beforeEach(async () => {
+        setActivePinia(createTestingPinia());
+
+        accessMap.value = fullAccessToAllContentMap;
+    });
+
+    afterEach(async () => {
+        // Clear the database after each test
+        await db.docs.clear();
+        await db.localChanges.clear();
+    });
+
+    it("should display the passed user", async () => {
+        const wrapper = mount(UserRow, {
+            props: {
+                usersDoc: mockUserDto,
+            },
+        });
+
+        expect(wrapper.html()).toContain("John Doe");
+        expect(wrapper.html()).toContain("john@doe.com");
+
+        // check if the updated time is formatted correctly according the systems settings
+        expect(wrapper.html()).toContain(
+            db
+                .toDateTime(mockLanguageDtoEng.updatedTimeUtc)
+                .toLocaleString(DateTime.DATETIME_SHORT),
+        );
+    });
+});
