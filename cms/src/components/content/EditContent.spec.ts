ddbd6dc5b35b31c06e75e4a689ce3b549d03ea9f
DirkDevelopmentFreelance <dirkdevbusiness@gmail.com>
2025-02-20 10:07:32 +0200

    Remake language selector and start fixing test

--- cms/src/components/content/EditContent.spec.ts
+++ cms/src/components/content/EditContent.spec.ts
@@ -1,7 +1,14 @@
 import { describe, it, afterEach, beforeEach, expect, vi } from "vitest";
 import { mount } from "@vue/test-utils";
 import { createTestingPinia } from "@pinia/testing";
-import { db, DocType, type ContentDto, accessMap, PostType } from "luminary-shared";
+import {
+    db,
+    DocType,
+    type ContentDto,
+    accessMap,
+    PostType,
+    type LanguageDto,
+} from "luminary-shared";
 import * as mockData from "@/tests/mockdata";
 import { setActivePinia } from "pinia";
 import EditContent from "./EditContent.vue";
@@ -9,6 +16,7 @@ import waitForExpect from "wait-for-expect";
 import { useNotificationStore } from "@/stores/notification";
 import EditContentBasic from "./EditContentBasic.vue";
 import EditContentParent from "./EditContentParent.vue";
+import LanguageSelector from "./LanguageSelector.vue";
 
 const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
@@ -426,4 +434,47 @@ describe("EditContent.vue", () => {
             expect(wrapper.findComponent(EditContentParent).props().disabled).toBe(true);
         });
     });
+
+    it.only(
+        "only displays languages the user has Translate access to in languageSelector",
+        async () => {
+            await db.docs.bulkPut([
+                mockData.mockLanguageDtoEng,
+                mockData.mockLanguageDtoFra,
+                mockData.mockLanguageDtoSwa,
+            ]);
+            // accessMap.value = { ...mockData.viewAccessToAllContentMap };
+            // accessMap.value["group-public-content"].language = {
+            //     view: true,
+            //     translate: true,
+            //     edit: false,
+            //     publish: true,
+            // };
+
+            console.log(
+                (await db.docs.toArray()).filter((doc) => doc.docType === DocType.Language),
+            );
+
+            const wrapper = mount(EditContent, {
+                props: {
+                    docType: DocType.Post,
+                    id: mockData.mockPostDto._id,
+                    languageCode: "eng",
+                    tagOrPostType: PostType.Blog,
+                },
+            });
+
+            const languageSelector = wrapper.findAllComponents(LanguageSelector)[0];
+            const btn = languageSelector.find("[data-test='language-selector']");
+            btn.trigger("click");
+
+            await wrapper.vm.$nextTick();
+
+            await waitForExpect(async () => {
+                const languages = languageSelector.find("[data-test='languagePopup']");
+                expect(await languages.html()).toContain("English");
+            });
+        },
+        { timeout: 999999 },
+    );
 });
