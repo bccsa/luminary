48b8cacab818ac03ca2f9484677ac6b813050a7b
DirkDevelopmentFreelance <dirkdevbusiness@gmail.com>
2025-02-24 09:57:26 +0200

    Make some changes

--- cms/src/globalConfig.ts
+++ cms/src/globalConfig.ts
@@ -11,26 +11,25 @@ export const isDevMode = import.meta.env.DEV;
  * The preferred CMS language ID as Vue ref.
  */
 export const cmsLanguageIdAsRef = ref(localStorage.getItem("cms_selectedLanguage") || "");
-watch(
-    cmsLanguageIdAsRef,
-    (newVal) => {
-        localStorage.setItem("cms_selectedLanguage", newVal);
-    },
-    { deep: true },
-);
+watch(cmsLanguageIdAsRef, (newVal) => {
+    localStorage.setItem("cms_selectedLanguage", newVal);
+});
 
-const languageInitialised = localStorage.getItem("language_initialised") || false;
+const languageInitialised = JSON.parse(localStorage.getItem("language_initialised") || "false");
 
 export async function initLanguage() {
-    if (languageInitialised) return;
+    if (languageInitialised && cmsLanguageIdAsRef.value) return;
 
     const languages = (await db.docs.where("type").equals("language").toArray()) as LanguageDto[];
     const browserPreferredLanguage = navigator.languages[0];
 
     if (languages.some((lang) => lang.languageCode == browserPreferredLanguage)) {
-        cmsLanguageIdAsRef.value = languages.filter(
+        const preferredLang = languages.find(
             (lang) => lang.languageCode === browserPreferredLanguage,
-        )[0]._id;
+        );
+        const defaultLang = languages.find((lang) => lang.default === 1);
+
+        cmsLanguageIdAsRef.value = preferredLang?._id || defaultLang?._id || "";
     } else {
         cmsLanguageIdAsRef.value = languages.filter((lang) => lang.default === 1)[0]._id;
     }
