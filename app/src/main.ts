c61cea50e58fc7a24bb18f9a56a83f0de3cfd18b
DirkDevelopmentFreelance <dirkdevbusiness@gmail.com>
2025-03-03 13:35:03 +0200

    Make requested changes

--- app/src/main.ts
+++ app/src/main.ts
@@ -11,9 +11,6 @@ import { appLanguageIdsAsRef, initLanguage } from "./globalConfig";
 import { apiUrl } from "./globalConfig";
 import { initAppTitle, initI18n } from "./i18n";
 import { initAnalytics } from "./analytics";
-import postcss from "postcss";
-//@ts-expect-error -- no declaration file
-import doiuse from "doiuse";
 
 export const app = createApp(App);
 
@@ -78,40 +75,7 @@ async function Startup() {
 }
 
 Startup();
-async function getInjectedCSS() {
-    let css = "";
-    document.querySelectorAll("style").forEach((style) => {
-        css += style.innerText;
-    });
-    return css;
-}
-
-async function checkCSSCompatibility() {
-    const css = await getInjectedCSS();
-    if (css) {
-        console.log("Extracted CSS:", css);
-        postcss([
-            doiuse({
-                browsers: [
-                    "chrome >= 96",
-                    "firefox >= 94",
-                    "edge >= 96",
-                    "opera >= 81",
-                    "safari >= 15",
-                    "> 1%",
-                ],
-                onFeatureUsage: (usageInfo: any) => {
-                    console.warn("Unsupported CSS:", usageInfo.message);
-                },
-            }),
-        ])
-            .process(css, { from: undefined })
-            .catch((err) => {
-                console.error("PostCSS processing error", err);
-            });
-    }
-}
 
 if (import.meta.env.DEV == true) {
-    checkCSSCompatibility();
+    import("./util/cssCompatibilityTester");
 }
