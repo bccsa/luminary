ff54f6b02b0e6e9921a44626d85f84402a140796
Ivan Slabbert <ivanslabbert@users.noreply.github.com>
2025-03-06 12:44:45 +0200

    Add i18n title for Watch page

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -39,7 +39,7 @@ const router = createRouter({
             component: VideoPage,
             name: "watch",
             meta: {
-                title: "Watch",
+                title: "title.watch",
             },
         },
         {
