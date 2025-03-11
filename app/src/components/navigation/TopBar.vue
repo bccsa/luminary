818aada7cb5d8f7b27537455c62c4f6425649339
Ivan Slabbert <ivanslabbert@users.noreply.github.com>
2025-03-11 13:16:07 +0200

    Calculate available logo space in parent element

--- app/src/components/navigation/TopBar.vue
+++ app/src/components/navigation/TopBar.vue
@@ -4,7 +4,7 @@ import { MoonIcon } from "@heroicons/vue/24/outline";
 import ProfileMenu from "./ProfileMenu.vue";
 import { useRouter } from "vue-router";
 import DesktopMenu from "./DesktopMenu.vue";
-import { computed, onMounted, ref, watch } from "vue";
+import { computed, nextTick, onMounted, ref, watch } from "vue";
 import { isDarkTheme, showContentQuickControls, theme } from "@/globalConfig";
 
 const router = useRouter();
@@ -12,6 +12,18 @@ const LOGO = import.meta.env.VITE_LOGO;
 const LOGO_SMALL = import.meta.env.VITE_LOGO_SMALL;
 const LOGO_DARK = import.meta.env.VITE_LOGO_DARK;
 const LOGO_SMALL_DARK = import.meta.env.VITE_LOGO_SMALL_DARK;
+
+const logoWidth = ref(parseInt(sessionStorage.getItem("logoWidth") || "0"));
+if (!logoWidth.value) {
+    const img = new Image();
+    img.src = LOGO;
+    img.onload = () => {
+        logoWidth.value = img.width;
+        sessionStorage.setItem("logoWidth", img.width.toString());
+    };
+}
+
+const logoContainer = ref<HTMLElement | undefined>(undefined);
 
 const isSmallScreen = ref(false);
 
@@ -25,18 +37,16 @@ const logoCss = computed(
 
 // Detect screen size on load and window resize
 const updateScreenSize = () => {
-    if (showContentQuickControls.value) {
-        isSmallScreen.value = window.innerWidth < 355;
-        return;
-    }
-
-    isSmallScreen.value = window.innerWidth < 360;
+    if (!logoWidth.value) return;
+    if (!logoContainer.value) return;
+    isSmallScreen.value = logoWidth.value > logoContainer.value.clientWidth;
 };
 
 onMounted(() => {
     watch(
-        showContentQuickControls,
-        () => {
+        [showContentQuickControls, logoWidth],
+        async () => {
+            await nextTick();
             updateScreenSize();
         },
         { immediate: true },
@@ -50,7 +60,7 @@ onMounted(() => {
     <header>
         <div class="z-40 bg-zinc-100 dark:bg-slate-800">
             <div class="flex items-center py-5 pl-6 pr-6 lg:pr-5">
-                <div class="mr-4 flex items-center">
+                <div class="flex flex-1 items-center">
                     <div
                         class="mr-4 border-r border-zinc-400 pr-4"
                         v-if="showContentQuickControls"
@@ -62,18 +72,20 @@ onMounted(() => {
                         />
                     </div>
 
-                    <div
-                        :style="logoCss"
-                        class="bg-[image:var(--image-url)] bg-cover bg-center dark:bg-[image:var(--image-url-dark)] truncate"
-                    >
-                        <!-- Show the image with 0 opacity to set the outer div's size. We assume that the dark mode logo will have the same size as the light mode logo. -->
-                        <img class="h-8 opacity-0 w-full" :src="logo" />
+                    <div class="flex flex-1 items-center" ref="logoContainer">
+                        <div
+                            :style="logoCss"
+                            class="bg-[image:var(--image-url)] bg-cover bg-center dark:bg-[image:var(--image-url-dark)]"
+                        >
+                            <!-- Show the image with 0 opacity to set the outer div's size. We assume that the dark mode logo will have the same size as the light mode logo. -->
+                            <img class="h-8 opacity-0" :src="logo" />
+                        </div>
+
+                        <DesktopMenu class="ml-6 hidden lg:flex" />
                     </div>
                 </div>
 
-                <DesktopMenu class="hidden lg:flex" />
-                <div class="flex-1" />
-                <div class="mx-4" v-if="showContentQuickControls" data-test="quickControls">
+                <div class="ml-2 mr-6" v-if="showContentQuickControls" data-test="quickControls">
                     <div class="flex cursor-pointer items-center">
                         <div class="text-zinc-400 dark:text-slate-300">
                             <SunIcon class="h-6 w-6" v-if="isDarkTheme" @click="theme = 'light'" />
@@ -81,7 +93,7 @@ onMounted(() => {
                         </div>
                     </div>
                 </div>
-                <ProfileMenu class="ml-6" />
+                <ProfileMenu />
             </div>
         </div>
     </header>
