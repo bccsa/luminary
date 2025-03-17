61e1e95a0a9848cd12c9f0c31083146a9c97a424
Dirk <dirkdevbusiness@gmail.com>
2025-03-17 14:11:58 +0200

    try to make test succeed

--- cms/src/components/content/EditContent.spec.ts
+++ cms/src/components/content/EditContent.spec.ts
@@ -646,26 +646,47 @@ describe("EditContent.vue", () => {
                 });
             });
         });
+    });
 
-        it("only displays languages the user has Translate access to in languageSelector", async () => {
+    it.only(
+        "only displays languages the user has Translate access to in languageSelector",
+        async () => {
+            accessMap.value = { ...mockData.translateAccessToAllContentMap };
+            accessMap.value["group-public-content"].language = {
+                view: true,
+                translate: false,
+                edit: true,
+                publish: true,
+            };
+
+            await db.docs.bulkPut([
+                { ...mockData.mockLanguageDtoFra, memberOf: ["group-public-content"] },
+                { ...mockData.mockLanguageDtoSwa, memberOf: ["group-public-content"] },
+                { ...mockData.mockLanguageDtoEng, memberOf: ["group-languages"] },
+            ]);
+
             const wrapper = mount(EditContent, {
                 props: {
                     id: mockData.mockPostDto._id,
                     languageCode: "en",
-                    //@ts-expect-error --> Makes the test pass but incorrect prop type, so error is expected, as this only tests the language selector, this prop doesn't matter here.
-                    docType: DocType.Content,
+                    docType: DocType.Post,
                     tagOrPostType: PostType.Blog,
                 },
             });
 
-            const languageSelector = wrapper.findAllComponents(LanguageSelector)[0];
+            const languageSelector = wrapper.findComponent(LanguageSelector);
             const btn = languageSelector.find("[data-test='language-selector']");
             btn.trigger("click");
 
+            const languages = languageSelector.find("[data-test='languagePopup']");
+
             await waitForExpect(async () => {
-                const languages = languageSelector.find("[data-test='languagePopup']");
                 expect(await languages.html()).toContain("English");
+
+                expect(await languages.html()).not.toContain("Français");
+                expect(await languages.html()).not.toContain("Swahili");
             });
-        });
-    });
+        },
+        { timeout: 10000 },
+    );
 });
