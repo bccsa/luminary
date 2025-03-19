b61b7e8b9ad9153dd0664d48d670105bf199c594
Dirk <dirkdevbusiness@gmail.com>
2025-03-19 13:59:28 +0200

    Initial

--- app/src/main.ts
+++ app/src/main.ts
@@ -7,7 +7,7 @@ import router from "./router";
 import auth from "./auth";
 import { DocType, getSocket, init } from "luminary-shared";
 import { loadPlugins } from "./util/pluginLoader";
-import { appLanguageIdsAsRef, initLanguage } from "./globalConfig";
+import { appLanguageIdsAsRef, initLanguage, loadFallbackImages } from "./globalConfig";
 import { apiUrl } from "./globalConfig";
 import { initAppTitle, initI18n } from "./i18n";
 import { initAnalytics } from "./analytics";
@@ -72,6 +72,8 @@ async function Startup() {
     app.mount("#app");
     initAppTitle(i18n);
     initAnalytics();
+
+    console.info(await loadFallbackImages());
 }
 
 Startup();
