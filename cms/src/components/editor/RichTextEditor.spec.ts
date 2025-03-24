197bce2164e5f02711d37d509ac339d595293244
Dirk <dirkdevbusiness@gmail.com>
2025-03-24 11:46:54 +0200

    Add Test

--- cms/src/components/editor/RichTextEditor.spec.ts
+++ cms/src/components/editor/RichTextEditor.spec.ts
@@ -2,6 +2,7 @@ import { describe, it, expect } from "vitest";
 import { mount } from "@vue/test-utils";
 import RichTextEditor from "./RichTextEditor.vue";
 import waitForExpect from "wait-for-expect";
+import { EditorContent } from "@tiptap/vue-3";
 
 describe("RichTextEditor", () => {
     it("mounts with content", async () => {
@@ -14,6 +15,32 @@ describe("RichTextEditor", () => {
 
         await waitForExpect(() => {
             expect(wrapper.text()).toContain("Gandalf the Grey");
+        });
+    });
+
+    it("updates content correctly", async () => {
+        const wrapper = mount(RichTextEditor, {
+            props: {
+                disabled: false,
+                text: JSON.stringify({
+                    type: "doc",
+                    content: [{ type: "paragraph", content: [{ type: "text", text: "Test" }] }],
+                }),
+            },
+        });
+
+        //@ts-expect-error
+        const editor = wrapper.vm.editor;
+        expect(editor).toBeDefined();
+
+        editor.commands.setContent("Testing Testing 123");
+
+        editor.options.onUpdate?.({ editor });
+
+        await waitForExpect(() => {
+            //@ts-expect-error
+            const updatedText = JSON.parse(wrapper.vm.text);
+            expect(updatedText.content[0].content[0].text).toBe("Testing Testing 123");
         });
     });
 });
