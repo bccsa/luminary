ea41a63f7520ae4097aa0ee053fc0e955d122f61
Dirk <dirkdevbusiness@gmail.com>
2025-03-31 13:42:29 +0200

    Fix failing LCombobox test

--- cms/src/components/forms/LCombobox.spec.ts
+++ cms/src/components/forms/LCombobox.spec.ts
@@ -42,11 +42,12 @@ describe("LCombobox", () => {
                 options: [{ id: 0, label: "Test Label", value: "test-value" }],
                 selectedOptions: selected.value,
                 docType: DocType.Post,
+                isContentOverview: false,
             },
         });
 
-        await wrapper.findComponent(LInput).setValue("Test Label");
-        await wrapper.findComponent(LInput).trigger("change");
+        await wrapper.find("input").setValue("Test Label");
+        await wrapper.find("input").trigger("change");
 
         await wrapper.find('[name="options-open-btn"]').trigger("click");
 
