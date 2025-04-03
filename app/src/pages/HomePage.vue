490cbaa81f716a62cd22648b2c6b0417c047462a
Dirk <dirkdevbusiness@gmail.com>
2025-04-03 15:35:59 +0200

    Add doiuse to postcss as plugin

--- app/src/pages/HomePage.vue
+++ app/src/pages/HomePage.vue
@@ -7,10 +7,9 @@ import IgnorePagePadding from "@/components/IgnorePagePadding.vue";
 import HomePagePinned from "@/components/HomePage/HomePagePinned.vue";
 import HomePageNewest from "@/components/HomePage/HomePageNewest.vue";
 import { isPublished } from "@/util/isPublished";
-import BasePage from "@/components/BasePage.vue";
 import ContinueWatching from "@/components/HomePage/ContinueWatching.vue";
 
-const { isAuthenticated, loginWithRedirect } = useAuth0();
+const { isAuthenticated } = useAuth0();
 
 const hasPosts = db.toRef<boolean>(
     () =>
@@ -39,20 +38,6 @@ setTimeout(() => {
                     You don't have access to any content. If you believe this is an error, send your
                     contact person a message.
                 </p>
-            </div>
-            <div v-else>
-                <div v-if="noContentMessageDelay">
-                    <p>There is currently no content available.</p>
-
-                    <p class="mt-1">
-                        Please
-                        <span
-                            class="cursor-pointer text-yellow-600 underline hover:text-yellow-500"
-                            @click="loginWithRedirect()"
-                            >log in </span
-                        >if you have an account.
-                    </p>
-                </div>
             </div>
         </div>
         <IgnorePagePadding ignoreTop>
