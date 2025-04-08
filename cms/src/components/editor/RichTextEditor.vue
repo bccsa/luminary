8914a871933200525b341487346a4231b1c1edf8
Johan Bell <50105980+belljohan3@users.noreply.github.com>
2025-04-08 09:34:52 +0200

    802 cms improve tiptap based rich text editor (#822)
    * Add heading level buttons and enhance paste handling in RichTextEditor
    
    * Refactor RichTextEditor to streamline heading level handling and improve paste functionality
    
    * Add test to RichTextEditor for converting h1 to h2 on paste

--- cms/src/components/editor/RichTextEditor.vue
+++ cms/src/components/editor/RichTextEditor.vue
@@ -21,7 +21,6 @@ const editorText = computed(() => {
     try {
         return JSON.parse(text.value);
     } catch {
-        // If the JSON is invalid, return the text as is as it probably is in HTML format
         return text.value;
     }
 });
@@ -30,25 +29,52 @@ const editor = useEditor({
     content: editorText.value,
     extensions: [
         StarterKit.configure({
-            heading: {
-                levels: [2, 3, 4, 5, 6],
-            },
+            heading: { levels: [2, 3, 4, 5, 6] },
         }),
-        Link.configure({
-            openOnClick: false,
-        }),
+        Link.configure({ openOnClick: false }),
     ],
     editable: (() => !disabled.value as boolean | undefined)(),
     editorProps: {
         attributes: {
             class: "prose prose-zinc lg:prose-sm max-w-none p-3 ring-1 ring-inset border-0 focus:ring-2 focus:ring-inset focus:outline-none rounded-md ring-zinc-300 hover:ring-zinc-400 focus:ring-zinc-950",
         },
+        handlePaste(view, event) {
+            const clipboardData = event.clipboardData;
+            if (!clipboardData) return false;
+
+            const html = clipboardData.getData("text/html");
+            if (html) {
+                let cleanedHtml = html;
+                if (html.match(/<h([1])([^>]*)>/gi)) {
+                    cleanedHtml = html
+                        .replace(/<h([1-5])([^>]*)>/gi, (match, level, attrs) => {
+                            const newLevel = parseInt(level) + 1;
+                            return `<h${newLevel}${attrs}>`;
+                        })
+                        .replace(/<\/h([1-5])>/gi, (match, level) => {
+                            const newLevel = parseInt(level) + 1;
+                            return `</h${newLevel}>`;
+                        });
+                }
+
+                cleanedHtml = cleanedHtml.replace(/\r\n/gi, "");
+
+                editor.value?.commands.insertContent(cleanedHtml);
+                return true;
+            }
+
+            return false;
+        },
     },
-    onUpdate: ({ editor }) => (text.value = JSON.stringify(editor.getJSON())),
+    onUpdate: ({ editor }) => {
+        const raw = editor.getJSON();
+        // const cleaned = removeEmptyLineBreaks(raw);
+        text.value = JSON.stringify(raw);
+    },
 });
 
 watch(disabled, () => {
-    editor.value?.setEditable(disabled.value ? false : true);
+    editor.value?.setEditable(!disabled.value);
 });
 </script>
 
@@ -90,6 +116,54 @@ watch(disabled, () => {
                     <StrikethroughIcon class="h-5 w-5" />
                 </button>
             </div>
+
+            <div class="flex pb-2">
+                <button
+                    :class="[
+                        'rounded-l-md bg-zinc-100 px-2 py-1.5 hover:bg-zinc-200 active:bg-zinc-300',
+                        { 'bg-zinc-300': editor?.isActive('heading', { level: 2 }) },
+                    ]"
+                    @click="editor?.chain().focus().toggleHeading({ level: 2 }).run()"
+                    title="Heading 2"
+                    type="button"
+                >
+                    H2
+                </button>
+                <button
+                    :class="[
+                        'bg-zinc-100 px-2 py-1 hover:bg-zinc-200 active:bg-zinc-300',
+                        { 'bg-zinc-300': editor?.isActive('heading', { level: 3 }) },
+                    ]"
+                    @click="editor?.chain().focus().toggleHeading({ level: 3 }).run()"
+                    title="Heading 3"
+                    type="button"
+                >
+                    H3
+                </button>
+                <button
+                    :class="[
+                        'bg-zinc-100 px-2 py-1 hover:bg-zinc-200 active:bg-zinc-300',
+                        { 'bg-zinc-300': editor?.isActive('heading', { level: 4 }) },
+                    ]"
+                    @click="editor?.chain().focus().toggleHeading({ level: 4 }).run()"
+                    title="Heading 4"
+                    type="button"
+                >
+                    H4
+                </button>
+                <button
+                    :class="[
+                        'rounded-r-md bg-zinc-100 px-2 py-1 hover:bg-zinc-200 active:bg-zinc-300',
+                        { 'bg-zinc-300': editor?.isActive('heading', { level: 5 }) },
+                    ]"
+                    @click="editor?.chain().focus().toggleHeading({ level: 5 }).run()"
+                    title="Heading 5"
+                    type="button"
+                >
+                    H5
+                </button>
+            </div>
+
             <div class="flex pb-2">
                 <button
                     :class="[
@@ -102,7 +176,6 @@ watch(disabled, () => {
                 >
                     <BulletlistIcon class="h-5 w-5" />
                 </button>
-
                 <button
                     :class="[
                         'rounded-r-md bg-zinc-100 px-2 py-1 hover:bg-zinc-200 active:bg-zinc-300',
