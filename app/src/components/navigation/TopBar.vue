9118c77b7046ee7c92dd90529e336a82917b98b7
Rapotukganakga <159137617+Rapotukganakga@users.noreply.github.com>
2025-04-09 10:49:40 +0200

    Updated margin of back button & removed back button on SingleContent (#811)
    * Updated margin of back button & removed back button on SingleContent
    
    * Removed the ArrowLeftIcon import

--- app/src/components/navigation/TopBar.vue
+++ app/src/components/navigation/TopBar.vue
@@ -66,7 +66,7 @@ onMounted(() => {
                         data-test="backButton"
                     >
                         <ChevronLeftIcon
-                            class="h-6 w-6 cursor-pointer text-zinc-600 dark:text-slate-50"
+                            class="h-6 w-6 cursor-pointer text-zinc-600 dark:text-slate-50 -ml-2"
                             @click="router.back()"
                         />
                     </div>
