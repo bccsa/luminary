098ad2a970cad9591c69d0e1515cc537d8b0bb75
Dirk <dirkdevbusiness@gmail.com>
2025-04-10 11:36:47 +0200

    tmp

--- cms/src/components/common/LPaginator.vue
+++ cms/src/components/common/LPaginator.vue
@@ -5,34 +5,47 @@ import LButton from "../button/LButton.vue";
 type PaginatorProps = {
     disabled?: boolean;
     size?: number;
-    variant?: "secondary" | "primary" | "tertiary" | "muted" | undefined;
+    btnVariant?: "secondary" | "primary" | "tertiary" | "muted" | undefined;
+    variant?: "simple" | "extended";
 };
 
 const index = defineModel<number>("index", {
     required: true,
 });
 
-withDefaults(defineProps<PaginatorProps>(), {
+const props = withDefaults(defineProps<PaginatorProps>(), {
     disabled: false,
     size: 0,
-    variant: "secondary",
+    btnVariant: "secondary",
+    variant: "simple",
 });
+
+const indexes = Array.from({ length: props.size }, (_, i) => i + 1);
+console.info("Pagination Component indexes:", indexes);
 </script>
 
 <template>
     <div class="flex items-center gap-1">
         <LButton
             :disabled="disabled"
-            :variant="variant"
+            :variant="btnVariant"
             :icon="ArrowLeftIcon"
             @click="if (index > 0) index -= 1;"
         />
-        <span class="text-sm text-zinc-600"
+        <span v-if="variant == 'simple'" class="text-sm text-zinc-600"
             >Page <strong>{{ index }}</strong> of <strong>{{ size }}</strong></span
         >
+        <span
+            v-else-if="variant == 'extended'"
+            v-for="i in indexes"
+            :key="`index-${i}`"
+            :class="i == index ? 'font-bold text-yellow-500' : ''"
+        >
+            {{ i }}
+        </span>
         <LButton
             :disabled="disabled"
-            :variant="variant"
+            :variant="btnVariant"
             :icon="ArrowRightIcon"
             @click="if (index < size) index += 1;"
         />
