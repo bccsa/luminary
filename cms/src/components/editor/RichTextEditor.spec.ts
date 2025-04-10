faaca5c70ade5cf9994b1ad7ee82c3fca57d5566
Dirk <dirkdevbusiness@gmail.com>
2025-04-10 13:01:37 +0200

    Fix weird code linting in RichTextEditor.spec

--- cms/src/components/editor/RichTextEditor.spec.ts
+++ cms/src/components/editor/RichTextEditor.spec.ts
@@ -2,7 +2,6 @@ import { describe, it, expect } from "vitest";
 import { mount } from "@vue/test-utils";
 import RichTextEditor from "./RichTextEditor.vue";
 import waitForExpect from "wait-for-expect";
-
 describe("RichTextEditor", () => {
     it("mounts with content", async () => {
         const wrapper = mount(RichTextEditor, {
@@ -57,33 +56,31 @@ describe("RichTextEditor", () => {
 
         await waitForExpect(() => {
             expect(wrapper.html()).toContain("<h2>My Heading</h2>");
-            it("updates content correctly", async () => {
-                const wrapper = mount(RichTextEditor, {
-                    props: {
-                        disabled: false,
-                        text: JSON.stringify({
-                            type: "doc",
-                            content: [
-                                { type: "paragraph", content: [{ type: "text", text: "Test" }] },
-                            ],
-                        }),
-                    },
-                });
+        });
+    });
+    it("updates content correctly", async () => {
+        const wrapper = mount(RichTextEditor, {
+            props: {
+                disabled: false,
+                text: JSON.stringify({
+                    type: "doc",
+                    content: [{ type: "paragraph", content: [{ type: "text", text: "Test" }] }],
+                }),
+            },
+        });
 
-                //@ts-expect-error
-                const editor = wrapper.vm.editor;
-                expect(editor).toBeDefined();
+        //@ts-expect-error
+        const editor = wrapper.vm.editor;
+        expect(editor).toBeDefined();
 
-                editor.commands.setContent("Testing Testing 123");
+        editor.commands.setContent("Testing Testing 123");
 
-                editor.options.onUpdate?.({ editor });
+        editor.options.onUpdate?.({ editor });
 
-                await waitForExpect(() => {
-                    //@ts-expect-error
-                    const updatedText = JSON.parse(wrapper.vm.text);
-                    expect(updatedText.content[0].content[0].text).toBe("Testing Testing 123");
-                });
-            });
+        await waitForExpect(() => {
+            //@ts-expect-error
+            const updatedText = JSON.parse(wrapper.vm.text);
+            expect(updatedText.content[0].content[0].text).toBe("Testing Testing 123");
         });
     });
 });
