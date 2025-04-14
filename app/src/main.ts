5942e53c059ab1925340ff2fbcafc99ad2cfd3ae
Dirk <dirkdevbusiness@gmail.com>
2025-04-14 12:20:40 +0200

    Add redirect document types to sync map

--- app/src/main.ts
+++ app/src/main.ts
@@ -48,6 +48,7 @@ async function Startup() {
                 syncPriority: 1,
                 skipWaitForLanguageSync: true,
             },
+            { type: DocType.Redirect, contentOnly: false, syncPriority: 3 },
         ],
     }).catch((err) => {
         console.error(err);
