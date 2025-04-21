16f6e790fb2237dde8c62f1db9b2d7831d266d16
Dirk <dirkdevbusiness@gmail.com>
2025-04-21 10:46:09 +0200

    Update app-playwright file to also start the api for e2e tests

--- app/e2e/vue.spec.ts
+++ app/e2e/vue.spec.ts
@@ -1,8 +1,32 @@
 import { test, expect } from "@playwright/test";
 
-// See here how to get started:
+// Leaving link in file for reference to Playwright documentation
 // https://playwright.dev/docs/intro
-test("visits the app root url", async ({ page }) => {
+
+test("syncs correct document types", async ({ page }) => {
     await page.goto("/");
-    await expect(page.locator("div.greetings > h1")).toHaveText("You did it!");
+
+    const result = await page.evaluate(() => {
+        return new Promise((resolve, reject) => {
+            const dbRequest = indexedDB.open("luminary-db");
+
+            dbRequest.onerror = () => reject("Error opening database");
+
+            dbRequest.onsuccess = () => {
+                const db = dbRequest.result;
+
+                const transaction = db.transaction("docs", "readonly");
+                const store = transaction.objectStore("docs");
+
+                const allDocsRequest = store.getAll();
+                allDocsRequest.onerror = () => reject("Failed to read object store");
+                allDocsRequest.onsuccess = () => resolve(allDocsRequest.result);
+            };
+        }) as unknown as Promise<any>;
+    });
+    const docTypes = result.map((doc: any) => doc.type);
+
+    const expectedDocTypes = ["redirect", "tag", "post", "language"];
+
+    expect(docTypes).toContain(expectedDocTypes);
 });
