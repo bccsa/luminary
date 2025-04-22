72c11118a26a65ffa30d954128df32a42b8c42a2
Dirk <dirkdevbusiness@gmail.com>
2025-04-22 10:39:06 +0200

    Add locally working test

--- app/e2e/vue.spec.ts
+++ app/e2e/vue.spec.ts
@@ -1,14 +1,65 @@
-import { test, expect } from "@playwright/test";
-
 // Leaving link in file for reference to Playwright documentation
 // https://playwright.dev/docs/intro
+import { test as base, chromium, expect } from "@playwright/test";
 
-test("syncs correct document types", async ({ page }) => {
+//indexedDB.databases() is not supported in all browsers so make context chromium
+const test = base.extend({
+    // Playwright requires the first parameter in the context function to be an object
+    // "_" does not work here.
+    // eslint-disable-next-line no-empty-pattern
+    context: async ({}, use) => {
+        const context = await chromium.launchPersistentContext("", {
+            headless: true,
+        });
+        await use(context);
+        await context.close();
+    },
+});
+
+test("it syncs correct document types to the app(non-cms) client", async ({ context }) => {
+    const page = await context.newPage();
     await page.goto("/");
     await page.reload({ waitUntil: "networkidle" });
 
     await page.waitForTimeout(3000);
 
-    console.log(page.url());
-    console.log(await page.title());
+    expect(await page.title()).toContain("Home");
+
+    const result = await page.evaluate(async () => {
+        return new Promise((resolve, reject) => {
+            //Ensure that the browser supports indexedDB.databases() as it is not supported in all browsers
+            if ("databases" in indexedDB) {
+                const dbRequest = indexedDB.open("luminary-db");
+                dbRequest.onerror = (event) => {
+                    console.error("Error opening database", event);
+                    reject("Error opening database");
+                };
+                dbRequest.onsuccess = () => {
+                    const db = dbRequest.result;
+                    const transaction = db.transaction("docs", "readonly");
+                    const objectStore = transaction.objectStore("docs");
+
+                    const request = objectStore.getAll();
+
+                    request.onerror = (event) => {
+                        console.error("Error getting all documents", event);
+                        reject("Error getting all documents");
+                    };
+
+                    request.onsuccess = () => {
+                        const documents = request.result;
+                        resolve(documents);
+                    };
+                };
+            } else {
+                console.error("indexedDB.databases() not supported");
+                reject("indexedDB.databases() not supported");
+            }
+        }) as unknown as Promise<any[]>;
+    });
+    expect(result).toBeDefined();
+
+    const types = [...new Set(result.map((doc: any) => doc.type))];
+
+    expect(types).toEqual(expect.arrayContaining(["language", "redirect", "content"]));
 });
