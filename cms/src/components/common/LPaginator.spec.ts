422c729c80fdcb445086304fb81607669c869e0d
Dirk <dirkdevbusiness@gmail.com>
2025-04-29 12:50:16 +0200

    Fix LPaginator tests

--- cms/src/components/common/LPaginator.spec.ts
+++ cms/src/components/common/LPaginator.spec.ts
@@ -2,26 +2,38 @@ import { mount } from "@vue/test-utils";
 import { describe, expect, it } from "vitest";
 import LPaginator from "./LPaginator.vue";
 import LButton from "../button/LButton.vue";
+import { ref } from "vue";
 
 describe("LPaginator", () => {
-    it("correctly increases the index", async () => {
+    it.only("correctly increases the index", async () => {
+        const index = ref(0);
+        const pageSize = ref(20);
         const wrapper = mount(LPaginator, {
             props: {
-                size: 20,
-                index: 0,
+                index: index.value,
+                pageSize: pageSize.value,
+                amountOfDocs: 100,
+                "onUpdate:index": (val) => (index.value = val),
+                "onUpdate:pageSize": (val) => (pageSize.value = val),
             },
         });
 
         const buttons = wrapper.findAllComponents(LButton);
+
         await buttons[1].trigger("click");
 
-        expect(wrapper.vm.index).toBe(1);
+        expect(index.value).toBe(1);
     });
     it("correctly decreases the index", async () => {
+        const index = ref(0);
+        const pageSize = ref(20);
         const wrapper = mount(LPaginator, {
             props: {
-                size: 20,
-                index: 4,
+                index: index.value,
+                pageSize: pageSize.value,
+                amountOfDocs: 100,
+                "onUpdate:index": (val) => (index.value = val),
+                "onUpdate:pageSize": (val) => (pageSize.value = val),
             },
         });
 
