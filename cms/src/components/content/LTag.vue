80200c11010f6f22c1aef41903684e39932087df
Dirk <dirkdevbusiness@gmail.com>
2025-04-29 14:14:58 +0200

    Add icon to LTag

--- cms/src/components/content/LTag.vue
+++ cms/src/components/content/LTag.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { XMarkIcon } from "@heroicons/vue/16/solid";
+import type { Component } from "vue";
 
 withDefaults(
     defineProps<{
         disabled?: boolean;
+        icon?: Component | Function;
     }>(),
     {
         disabled: false,
@@ -16,10 +18,20 @@ const emit = defineEmits(["remove"]);
 <template>
     <div
         :class="[
-            'rounded-md border border-zinc-200 bg-zinc-50 py-1 pl-2  text-sm text-zinc-900 shadow-sm',
+            'flex gap-2 rounded-md border border-zinc-200 bg-zinc-50 py-1 pl-2  text-sm text-zinc-900 shadow-sm',
             disabled ? 'pr-2' : 'pr-1',
         ]"
     >
+        <div v-if="icon" class="z-10 flex items-center">
+            <component
+                :is="icon"
+                :class="{
+                    'text-zinc-400': !disabled,
+                    'text-zinc-300': disabled,
+                }"
+                class="h-5 w-5"
+            />
+        </div>
         <div class="flex items-center gap-2">
             <slot />
             <button @click="emit('remove')" data-test="removeTag" v-if="!disabled" type="button">
