29afd58da1001f59b54704e0471cdd281d4fb647
Dirk <dirkdevbusiness@gmail.com>
2025-04-30 08:59:39 +0200

    Add 3 tests to LCombobox.spec

--- cms/src/components/forms/LCombobox.vue
+++ cms/src/components/forms/LCombobox.vue
@@ -145,7 +145,7 @@ const selectedLabels = computed(() => {
 
         <div
             ref="dropdown"
-            v-show="showDropdown || query.trim().length > 0"
+            v-if="showDropdown || query.trim().length > 0"
             class="absolute z-10 mt-1 max-h-48 w-full overflow-y-auto rounded-md border-[1px] border-zinc-100 bg-white shadow-md"
             data-test="options"
         >
