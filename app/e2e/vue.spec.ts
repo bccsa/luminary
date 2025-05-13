cd166c65ce7938e02b76afd246ca4706550f2b45
Dirk <dirkdevbusiness@gmail.com>
2025-05-13 08:13:41 +0200

    Add JWT secret to the workflow file

--- app/e2e/vue.spec.ts
+++ app/e2e/vue.spec.ts
@@ -9,7 +9,7 @@ const test = base.extend({
     // eslint-disable-next-line no-empty-pattern
     context: async ({}, use) => {
         const context = await chromium.launchPersistentContext("", {
-            headless: false,
+            headless: true,
             locale: "en",
         });
         await use(context);
