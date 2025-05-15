6538bb79305acd0b50aed1f7100f0b9b9e2fe1a2
Dirk <dirkdevbusiness@gmail.com>
2025-05-15 16:06:51 +0200

    Add lastLoggedIn field to DTOs and update seeding docs

--- shared/src/types/dto.ts
+++ shared/src/types/dto.ts
@@ -77,6 +77,7 @@ export type UserDto = ContentBaseDto & {
     type: DocType.User;
     email: string;
     name: string;
+    lastLoggedIn: number;
 };
 
 export type ContentDto = ContentBaseDto & {
