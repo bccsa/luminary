beecb21bae73cb307f66b3ca66d8c8ca1910c7f2
Dirk <dirkdevbusiness@gmail.com>
2025-05-16 13:25:16 +0200

    refactor: improve layout and responsiveness across multiple components

--- cms/src/components/content/ContentOverview/ContentOverview.vue
+++ cms/src/components/content/ContentOverview/ContentOverview.vue
@@ -21,11 +21,12 @@ import { computed, ref, watch } from "vue";
 import ContentTable from "@/components/content/ContentTable.vue";
 import { capitaliseFirstLetter } from "@/util/string";
 import router from "@/router";
-import type { ContentOverviewQueryOptions } from "../query";
-import { cmsLanguageIdAsRef } from "@/globalConfig";
+import { contentOverviewQuery, type ContentOverviewQueryOptions } from "../query";
+import { cmsLanguageIdAsRef, isSmallScreen } from "@/globalConfig";
 import LTag from "../LTag.vue";
 import FilterOptions from "./FilterOptions.vue";
 import { TagIcon, UserGroupIcon } from "@heroicons/vue/24/outline";
+import LPaginator from "@/components/common/LPaginator.vue";
 
 type Props = {
     docType: DocType.Post | DocType.Tag;
@@ -108,9 +109,6 @@ const groups = useDexieLiveQuery(
     () => db.docs.where({ type: DocType.Group }).toArray() as unknown as Promise<GroupDto[]>,
     { initialValue: [] as GroupDto[] },
 );
-
-//Rather do calculation here, so it doesn't recalculate for each component
-const isSmallScreen = window.innerWidth < 430;
 </script>
 
 <template>
@@ -137,63 +135,68 @@ const isSmallScreen = window.innerWidth < 430;
             </div>
         </template>
 
-        <FilterOptions
-            :is-small-screen="isSmallScreen"
-            :groups="groups"
-            :tagContentDocs="tagContentDocs"
-            v-model:query-options="queryOptions"
-        />
+        <div class="scrollbar-hide overflow-y-scroll" style="max-height: calc(100vh - 4rem)">
+            <div class="sticky top-0 z-10 bg-white">
+                <FilterOptions
+                    :is-small-screen="isSmallScreen"
+                    :groups="groups"
+                    :tagContentDocs="tagContentDocs"
+                    v-model:query-options="queryOptions"
+                />
+            </div>
 
-        <div
-            v-if="queryOptions.tags && queryOptions.tags?.length > 0"
-            class="w-full bg-white px-2 pb-2 shadow"
-        >
-            <ul class="flex w-full flex-wrap gap-2">
-                <LTag
-                    :icon="TagIcon"
-                    v-for="tag in queryOptions.tags"
-                    :key="tag"
-                    @remove="
-                        () => {
-                            if (!queryOptions.tags) return;
-                            queryOptions.tags = queryOptions.tags.filter((v) => v != tag);
-                        }
-                    "
-                >
-                    {{ tagContentDocs.find((t) => t.parentId == tag)?.title }}
-                </LTag>
-            </ul>
-        </div>
-        <div
-            v-if="queryOptions.groups && queryOptions.groups?.length > 0"
-            class="w-full bg-white px-2 pb-2 shadow"
-        >
-            <ul class="flex w-full flex-wrap gap-2">
-                <LTag
-                    :icon="UserGroupIcon"
-                    v-for="group in queryOptions.groups"
-                    :key="group"
-                    @remove="
-                        () => {
-                            if (!queryOptions.groups) return;
-                            queryOptions.groups = queryOptions.groups.filter((v) => v != group);
-                        }
-                    "
-                >
-                    {{ groups.find((g) => g._id == group)?.name }}
-                </LTag>
-            </ul>
-        </div>
-        <div class="">
-            <ContentTable
-                :is-small-screen="isSmallScreen"
-                v-if="cmsLanguageIdAsRef"
-                v-model:page-index="queryOptions.pageIndex as number"
-                :key="tableRefreshKey"
-                :groups="groups"
-                :queryOptions="queryOptions"
-                data-test="content-table"
-            />
+            <div
+                v-if="queryOptions.tags && queryOptions.tags?.length > 0"
+                class="w-full bg-white px-2 pb-2 shadow"
+            >
+                <ul class="flex w-full flex-wrap gap-2">
+                    <LTag
+                        :icon="TagIcon"
+                        v-for="tag in queryOptions.tags"
+                        :key="tag"
+                        @remove="
+                            () => {
+                                if (!queryOptions.tags) return;
+                                queryOptions.tags = queryOptions.tags.filter((v) => v != tag);
+                            }
+                        "
+                    >
+                        {{ tagContentDocs.find((t) => t.parentId == tag)?.title }}
+                    </LTag>
+                </ul>
+            </div>
+            <div
+                v-if="queryOptions.groups && queryOptions.groups?.length > 0"
+                class="w-full bg-white px-2 pb-2 shadow"
+            >
+                <ul class="flex w-full flex-wrap gap-2">
+                    <LTag
+                        :icon="UserGroupIcon"
+                        v-for="group in queryOptions.groups"
+                        :key="group"
+                        @remove="
+                            () => {
+                                if (!queryOptions.groups) return;
+                                queryOptions.groups = queryOptions.groups.filter((v) => v != group);
+                            }
+                        "
+                    >
+                        {{ groups.find((g) => g._id == group)?.name }}
+                    </LTag>
+                </ul>
+            </div>
+
+            <div class="mt-1">
+                <ContentTable
+                    :is-small-screen="isSmallScreen"
+                    v-if="cmsLanguageIdAsRef"
+                    v-model:page-index="queryOptions.pageIndex as number"
+                    :key="tableRefreshKey"
+                    :groups="groups"
+                    :queryOptions="queryOptions"
+                    data-test="content-table"
+                />
+            </div>
         </div>
     </BasePage>
 </template>
