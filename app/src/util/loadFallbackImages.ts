7ac89e5e1fcc5d71aef65a54d80f8bd1d7115c6c
Dirk <dirkdevbusiness@gmail.com>
2025-05-19 11:53:59 +0200

    Refactor loadFallbackImageUrls to use eager loading and simplify fallback image retrieval

--- app/src/util/loadFallbackImages.ts
+++ app/src/util/loadFallbackImages.ts
@@ -1,11 +1,10 @@
 export const loadFallbackImageUrls = async () => {
-    // Can't use a dynamic path here, so had to resolve with a static path
-    const images = import.meta.glob("@/assets/fallbackImages/*.{png,jpg,jpeg,webp}");
-    const fallbackImages: string[] = [];
+    const images = import.meta.glob("@/assets/fallbackImages/*.{png,jpg,jpeg,webp}", {
+        eager: true,
+        import: "default",
+    });
 
-    for (const path in images) {
-        fallbackImages.push(path.replace("@/assets", "/src/assets"));
-    }
+    const fallbackImages = Object.values(images);
 
     return fallbackImages;
 };
