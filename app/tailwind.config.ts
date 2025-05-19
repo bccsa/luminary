4317c6598f54f4e920c9f7cf749d815b9ac0573f
Dirk <dirkdevbusiness@gmail.com>
2025-05-19 13:28:26 +0200

    Add "Inter var" to sans font family in Tailwind configuration

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -8,7 +8,7 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: [...defaultTheme.fontFamily.sans],
+                sans: ["Inter var", ...defaultTheme.fontFamily.sans],
             },
         },
     },
