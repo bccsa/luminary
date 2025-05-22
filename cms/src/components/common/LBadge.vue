2682b7756b898358e2931279f87d1bece8c5d211
Ivan Slabbert <ivanslabbert@users.noreply.github.com>
2025-05-22 10:13:41 +0200

    refactor: update LBadge variant from 'card' to 'blue' for consistent styling in ContentDisplayCard

--- cms/src/components/common/LBadge.vue
+++ cms/src/components/common/LBadge.vue
@@ -1,7 +1,7 @@
 <script lang="ts">
 export const variants = {
     default: "bg-zinc-100 text-zinc-600 ring-zinc-200",
-    card: "bg-zinc-400 text-white",
+    blue: "bg-blue-50 text-zinc-600 ring-zinc-200",
     success: "bg-green-100 text-green-700 ring-green-200",
     warning: "bg-yellow-100 text-yellow-800 ring-yellow-300",
     error: "bg-red-100 text-red-700 ring-red-200",
