d922511376666a0bb9596192e86053968f2b552c
Dirk <dirkdevbusiness@gmail.com>
2025-06-03 17:41:45 +0200

    refactor: adjust layout and styling in various components for improved usability and consistency

--- cms/src/components/navigation/ProfileMenu.vue
+++ cms/src/components/navigation/ProfileMenu.vue
@@ -2,7 +2,6 @@
 import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/vue";
 import {
     ArrowLeftEndOnRectangleIcon,
-    ChevronDownIcon,
     Cog6ToothIcon,
     LanguageIcon,
     UserIcon,
@@ -72,25 +71,29 @@ onMounted(() => {
 </script>
 
 <template>
-    <Menu as="div" class="relative">
-        <MenuButton class="-m-1.5 flex items-center p-1.5">
-            <span class="sr-only">Open user menu</span>
-            <img
-                class="h-8 w-8 rounded-full bg-zinc-50"
-                :src="user.picture"
-                v-if="user?.picture"
-                alt=""
-            />
-            <div class="flex h-8 w-8 items-center justify-center rounded-full bg-zinc-300" v-else>
-                <UserIcon class="h-6 w-6 text-zinc-600" />
+    <Menu as="div" class="relative w-full">
+        <MenuButton class="-m-1.5 flex w-full items-center justify-between p-1.5">
+            <div class="w-max">
+                <span class="sr-only">Open user menu</span>
+                <img
+                    class="h-8 w-8 rounded-full bg-zinc-50"
+                    :src="user.picture"
+                    v-if="user?.picture"
+                    alt=""
+                />
+                <div
+                    class="flex h-8 w-8 items-center justify-center rounded-full bg-zinc-300"
+                    v-else
+                >
+                    <UserIcon class="h-6 w-6 text-zinc-600" />
+                </div>
             </div>
-            <span class="flex items-center justify-center">
+            <span class="flex w-full items-center justify-center">
                 <span
                     class="ml-4 text-sm font-semibold leading-6 text-zinc-900"
                     aria-hidden="true"
                     >{{ user?.name }}</span
                 >
-                <ChevronDownIcon class="ml-2 h-5 w-5 text-zinc-400" aria-hidden="true" />
             </span>
         </MenuButton>
         <transition
@@ -102,7 +105,7 @@ onMounted(() => {
             leave-to-class="transform opacity-0 scale-95"
         >
             <MenuItems
-                class="absolute right-0 z-50 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-zinc-900/5 focus:outline-none"
+                class="absolute bottom-0 right-0 z-50 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-zinc-900/5 focus:outline-none"
             >
                 <MenuItem v-for="item in userNavigation" :key="item.name" v-slot="{ active }">
                     <button
