1478a4007513d03f10d249dad19ba058b4de3c1c
Johan Bell <belljohan3@gmail.com>
2025-06-04 22:43:21 +0100

    upgrade tailwindcss to v4

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,6 +4,10 @@ import vue from "@vitejs/plugin-vue";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 import util from "util";
 import child_process from "child_process";
+import tailwindcss from '@tailwindcss/vite'
+
+
+
 const exec = util.promisify(child_process.exec);
 const env = loadEnv("", process.cwd());
 
@@ -11,6 +15,7 @@ const env = loadEnv("", process.cwd());
 export default defineConfig({
     plugins: [
         vue(),
+        tailwindcss(),
         viteStaticCopy({
             targets: [
                 {
