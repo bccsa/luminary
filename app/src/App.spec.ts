56e7e8b84b7b4ff2fd5b42568a965575c3f7c951
Rapotukganakga <kganakga.rapotu@gmail.com>
2025-06-24 10:06:19 +0200

    Removed test in App.spec. Test was created in new BasePage.spec file

--- app/src/App.spec.ts
+++ app/src/App.spec.ts
@@ -98,25 +98,6 @@ describe("App", () => {
                 );
             }, 8000);
         }, 9000);
-
-        it("shows whether notification are displayed and visible", async () => {
-
-            (auth0 as any).useAuth0 = vi.fn().mockReturnValue({
-                isLoading: ref(false),
-                isAuthenticated: ref(false),
-            });
-
-            const notificationStore = useNotificationStore();
-
-            mount(App, {
-                shallow: true,
-            });
-
-            await waitForExpect(() => {
-                expect(notificationStore.addNotification).toHaveBeenCalled();
-                expect(notificationStore.removeNotification).toHaveBeenCalled();
-            }, 8000)
-        }, 15000);
     });
 
     describe("Theme config", () => {
