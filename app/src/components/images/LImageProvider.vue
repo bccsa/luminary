5a85526f1541cd6622110f1d2aebfda613b992cb
Johan Bell <belljohan3@gmail.com>
2025-06-25 12:59:22 +0100

    fix

--- app/src/components/images/LImageProvider.vue
+++ app/src/components/images/LImageProvider.vue
@@ -139,8 +139,9 @@ const srcset2 = computed(() => {
     const effectiveWidth = props.parentWidth > 0 ? props.parentWidth : 400;
 
     return props.image.fileCollections
-        .filter((collection) => collection.aspectRatio !== closestAspectRatio)
+        .filter((collection) => collection?.aspectRatio !== closestAspectRatio)
         .map((collection) => {
+            if (!collection.imageFiles.length) return "";
             return collection.imageFiles
                 .filter((imgFile) => imgFile.width <= effectiveWidth)
                 .sort((a, b) => a.width - b.width)
