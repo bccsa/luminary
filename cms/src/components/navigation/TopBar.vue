5141cf2a8d1c74ea6fe685be94725845f7362ea7
Johan Bell <belljohan3@gmail.com>
2025-07-01 19:07:15 +0100

    Refactor TopBar component layout for improved spacing and slot usage

--- cms/src/components/navigation/TopBar.vue
+++ cms/src/components/navigation/TopBar.vue
@@ -4,11 +4,12 @@ import OnlineIndicator from "../OnlineIndicator.vue";
 
 <template>
     <div class="flex flex-1 gap-x-4 self-stretch lg:gap-x-6">
-        <div class="relative flex flex-1 items-center justify-between gap-2">
+        <div class="relative flex flex-1 items-center justify-between gap-1">
             <slot name="quickActions" />
         </div>
-        <div class="flex items-center">
+        <div class="flex items-center gap-2">
             <OnlineIndicator />
+            <slot name="contentActions" />
         </div>
     </div>
 </template>
