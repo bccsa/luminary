72a098479cfbc5c3b6d8e4c71d1e2876f30c4f5c
Johan Bell <belljohan3@gmail.com>
2025-07-02 13:22:01 +0100

    Add unit tests for EditContentImage component

---
+++ cms/src/components/content/EditContentImage.spec.ts
@@ -0,0 +1,23 @@
+import "fake-indexeddb/auto";
+import { describe, it, expect } from "vitest";
+import { mount } from "@vue/test-utils";
+import * as mockData from "@/tests/mockdata";
+import EditContentImage from "./EditContentImage.vue";
+import { DocType, TagType, type TagDto } from "luminary-shared";
+import { ref } from "vue";
+
+describe("EditContentValidation.vue", () => {
+    it("can display an image thumbnail", async () => {
+        const parent = ref<TagDto>({ ...mockData.mockCategoryDto });
+        const wrapper = mount(EditContentImage, {
+            props: {
+                docType: DocType.Tag,
+                parent: parent.value,
+                tagOrPostType: TagType.Category,
+                disabled: false,
+            },
+        });
+
+        expect(wrapper.html()).toContain("test-image.webp");
+    });
+});
