279d8ce2c5b6a5eb268b4d4ffdccc95294ff68af
Johan Bell <belljohan3@gmail.com>
2025-07-03 13:26:36 +0100

    removed card and implement collapse functionality

--- cms/src/components/content/EditContentVideo.vue
+++ cms/src/components/content/EditContentVideo.vue
@@ -4,30 +4,38 @@ import LCard from "@/components/common/LCard.vue";
 import LInput from "@/components/forms/LInput.vue";
 import { VideoCameraIcon, LinkIcon } from "@heroicons/vue/20/solid";
 import { type ContentDto } from "luminary-shared";
+import { ref, watch } from "vue";
 
 type Props = {
     disabled: boolean;
 };
 defineProps<Props>();
 const content = defineModel<ContentDto>("content");
+
+const collapsed = ref(false);
+const hasInitialized = ref(false);
+
+// Collapse the card only initially if there's no video
+watch(
+    () => content.value?.video,
+    (video) => {
+        if (!hasInitialized.value) {
+            collapsed.value = video == null || video === "";
+            hasInitialized.value = true;
+        }
+        // DO NOTHING after initial render
+    },
+    { immediate: true },
+);
 </script>
 
 <template>
-    <LButton
-        type="button"
-        variant="tertiary"
-        :icon="VideoCameraIcon"
-        v-if="content && content?.video == undefined && !disabled"
-        @click="content.video = ''"
-        data-test="addVideo"
-    >
-        Add Video
-    </LButton>
     <LCard
         title="Video"
         :icon="VideoCameraIcon"
         collapsible
-        v-if="content?.video != undefined"
+        v-if="content"
+        :collapsed="collapsed"
         data-test="videoContent"
     >
         <LInput
