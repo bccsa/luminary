1e1f248931ddd8433c6dd6462475ba55c13c6741
Johan Bell <belljohan3@gmail.com>
2025-07-07 12:45:49 +0100

    remove card backgound-color and decrease the shadow

--- cms/src/components/common/LCard.vue
+++ cms/src/components/common/LCard.vue
@@ -39,7 +39,7 @@ function collapse() {
 
 <template>
     <div
-        class="border-y-2 border-zinc-200 bg-zinc-200/10 px-3 shadow-md shadow-zinc-300/60 sm:mx-0 sm:rounded-md sm:border-2"
+        class="border-y-2 border-zinc-200 px-3 shadow-sm shadow-zinc-300/60 sm:mx-0 sm:rounded-md sm:border-2"
     >
         <div
             v-if="title || icon"
