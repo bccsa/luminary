bb9c6d0eca2885299e3ac14f9f8a31c1101c34a1
Johan Bell <belljohan3@gmail.com>
2025-07-17 18:37:52 +0100

    make selectedLanguage optional

--- cms/src/components/content/EditContentText.vue
+++ cms/src/components/content/EditContentText.vue
@@ -4,7 +4,7 @@ import LCard from "@/components/common/LCard.vue";
 import RichTextEditor from "../editor/RichTextEditor.vue";
 
 type Props = {
-    selectedLanguage: LanguageDto;
+    selectedLanguage?: LanguageDto;
     disabled: boolean;
 };
 defineProps<Props>();
@@ -13,7 +13,7 @@ const content = defineModel<ContentDto>("content");
 
 <template>
     <div v-if="content">
-        <LCard :title="`${selectedLanguage.name}`" class="bg-white">
+        <LCard :title="selectedLanguage ? selectedLanguage.name : 'Text'" class="bg-white">
             <RichTextEditor
                 v-model:text="content.text"
                 :disabled="disabled"
