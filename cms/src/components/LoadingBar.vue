d804a79ea49ec7b0bba5a8bc3b7a1014aa51064b
Rapotukganakga <kganakga.rapotu@gmail.com>
2025-07-23 14:55:10 +0200

    Changed border color for visibility

--- cms/src/components/LoadingBar.vue
+++ cms/src/components/LoadingBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="relative h-3 w-1/2 overflow-hidden rounded-full border border-white">
+    <div class="relative h-3 w-1/2 overflow-hidden rounded-full border border-[#736d6d]">
         <div class="animate-loading-bar absolute left-0 top-0 h-full bg-[#e2d003]"></div>
     </div>
 </template>
