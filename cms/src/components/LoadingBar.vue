ecf50a8c0151bf081344a5578c5d363a134530e7
Rapotukganakga <kganakga.rapotu@gmail.com>
2025-07-31 11:41:01 +0200

    Changed height and removed border for loading bar

--- cms/src/components/LoadingBar.vue
+++ cms/src/components/LoadingBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="relative h-3 w-1/2 overflow-hidden rounded-full border border-zinc-500">
-        <div class="animate-loading-bar absolute left-0 top-0 h-full bg-yellow-400"></div>
+    <div class="relative h-2 w-1/2 overflow-hidden rounded-full border bg-zinc-200">
+        <div class="absolute left-0 top-0 h-full animate-loading-bar bg-yellow-500"></div>
     </div>
 </template>
