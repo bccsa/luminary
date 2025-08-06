b60649b0c01d1478a8257f03721b0799b379d7f7
Dirk <dirkdevbusiness@gmail.com>
2025-08-06 17:14:55 +0200

    Implement privacy policy acceptance flow in authentication process

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,17 +1,37 @@
 <script setup lang="ts">
-import { useAuth0 } from "@auth0/auth0-vue";
 import { RouterView } from "vue-router";
 import { computed, onErrorCaptured, watch } from "vue";
 import { isConnected } from "luminary-shared";
-import { showPrivacyPolicyModal, userPreferencesAsRef } from "./globalConfig";
+import { userPreferencesAsRef } from "./globalConfig";
 import { useNotificationStore } from "./stores/notification";
 import { ExclamationCircleIcon, SignalSlashIcon } from "@heroicons/vue/20/solid";
 import * as Sentry from "@sentry/vue";
 import { useRouter } from "vue-router";
-import PrivacyPolicyModal from "./components/navigation/PrivacyPolicyModal.vue";
+import PrivacyPolicyModal from "@/components/navigation/PrivacyPolicyModal.vue";
+import { useAuthWithPrivacyPolicy } from "@/composables/useAuthWithPrivacyPolicy";
 
 const router = useRouter();
-const { isAuthenticated, user, loginWithRedirect } = useAuth0();
+const {
+    isAuthenticated,
+    user,
+    loginWithRedirect,
+    isPrivacyPolicyAccepted,
+    showPrivacyPolicyModal,
+    completePendingLogin,
+    cancelPendingLogin,
+} = useAuthWithPrivacyPolicy();
+
+// Watch for privacy policy acceptance to complete pending login
+watch(isPrivacyPolicyAccepted, (accepted) => {
+    if (accepted) {
+        completePendingLogin();
+    }
+});
+
+// Handle modal close
+const handleModalClose = () => {
+    cancelPendingLogin();
+};
 
 // Wait 5 seconds to allow the socket connection to be established before checking the connection status
 setTimeout(() => {
@@ -54,6 +74,7 @@ setTimeout(() => {
                     link: () => loginWithRedirect(),
                 });
             }
+
             if (!isConnected.value || isAuthenticated.value) {
                 useNotificationStore().removeNotification("accountBanner");
             }
@@ -89,5 +110,7 @@ onErrorCaptured((err) => {
             <component :is="Component" :key="routeKey" />
         </KeepAlive>
     </RouterView>
-    <PrivacyPolicyModal v-model:show="showPrivacyPolicyModal" />
+
+    <!-- Privacy Policy Modal for authentication flow -->
+    <PrivacyPolicyModal v-model:show="showPrivacyPolicyModal" @close="handleModalClose" />
 </template>
