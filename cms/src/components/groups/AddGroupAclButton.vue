c8149e3053744ea46c92bc5e3e8198916f1d87f7
Dirk <dirkdevbusiness@gmail.com>
2025-08-14 08:51:41 +0200

    Refactor AddGroupAclButton to use a div
    Replaced headlessui Menu with a div and LButton. Also replaced MenuItem
    with button elements.

--- cms/src/components/groups/AddGroupAclButton.vue
+++ cms/src/components/groups/AddGroupAclButton.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
 import type { GroupDto } from "luminary-shared";
-import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
 import { ChevronDownIcon } from "@heroicons/vue/20/solid";
-import { toRaw, toRefs } from "vue";
+import { ref, toRaw, toRefs } from "vue";
 import LButton from "../button/LButton.vue";
 import { sortByName } from "@/util/sortByName";
 
@@ -19,20 +18,22 @@ const emit = defineEmits(["select"]);
 const selectGroup = (group: GroupDto) => {
     emit("select", toRaw(group));
 };
+
+const showGroups = ref(false);
 </script>
 
 <template>
     <div class="relative">
-        <Menu as="div" class="inline-block text-left">
+        <div class="inline-block text-left">
             <div>
-                <MenuButton
-                    :as="LButton"
-                    :icon="ChevronDownIcon"
-                    iconRight
+                <LButton
+                    @click="() => (showGroups = !showGroups)"
                     data-test="addGroupButton"
+                    :icon="ChevronDownIcon"
+                    icon-right
                 >
                     Add access
-                </MenuButton>
+                </LButton>
             </div>
 
             <transition
@@ -43,33 +44,33 @@ const selectGroup = (group: GroupDto) => {
                 leave-from-class="transform scale-100 opacity-100"
                 leave-to-class="transform scale-95 opacity-0"
             >
-                <MenuItems
+                <div
+                    v-if="showGroups"
                     class="absolute left-0 z-20 mt-2 w-52 origin-top-left divide-y divide-zinc-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none"
                 >
-                    <div class="px-1 py-1">
-                        <MenuItem
-                            v-slot="{ active }"
+                    <ul class="px-1 py-1">
+                        <!-- v-slot="{ active }" -->
+                        <li
                             v-for="group in groups.sort(sortByName)"
                             :key="group._id"
                             data-test="group-items"
                         >
                             <button
                                 :class="[
-                                    'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                                    { 'bg-zinc-100': active },
+                                    'group flex w-full items-center rounded-md px-2 py-2 text-sm hover:bg-zinc-100',
                                 ]"
                                 @click="() => selectGroup(group)"
                                 data-test="selectGroupButton"
                             >
                                 {{ group.name }}
                             </button>
-                        </MenuItem>
-                        <MenuItem v-if="groups.length == 0">
+                        </li>
+                        <li v-if="groups.length == 0">
                             <div class="px-2 py-2 text-sm text-zinc-500">All groups added</div>
-                        </MenuItem>
-                    </div>
-                </MenuItems>
+                        </li>
+                    </ul>
+                </div>
             </transition>
-        </Menu>
+        </div>
     </div>
 </template>
