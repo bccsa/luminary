ba0794714f4ba519761a6678a57d7e31497018cf
Dirk <dirkdevbusiness@gmail.com>
2025-08-22 16:13:16 +0200

    Fix a test

--- app/src/components/contentByTag.spec.ts
+++ app/src/components/contentByTag.spec.ts
@@ -35,17 +35,17 @@ describe("contentByTag", () => {
         await nextTick();
         expect(result.tagged.value).toEqual([
             {
-                tag: { _id: "b", parentId: "b" },
-                newestContentDate: 2,
-                content: [{ _id: "2", publishDate: 2, parentTags: ["b"] }],
-            },
-            {
                 tag: { _id: "a", parentId: "a" },
-                newestContentDate: 1,
+                newestContentDate: 3,
                 content: [
                     { _id: "1", publishDate: 1, parentTags: ["a"] },
                     { _id: "3", publishDate: 3, parentTags: ["a"] },
                 ],
+            },
+            {
+                tag: { _id: "b", parentId: "b" },
+                newestContentDate: 2,
+                content: [{ _id: "2", publishDate: 2, parentTags: ["b"] }],
             },
         ]);
 
