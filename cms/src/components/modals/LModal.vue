37d55d21ed4e8fd28135de39a8cafdd3cb9da990
Dirk <dirkdevbusiness@gmail.com>
2025-08-22 13:55:16 +0200

    Refactor LModal to ensure consistent rendering by moving Teleport logic and cleaning up template structure

--- cms/src/components/modals/LModal.vue
+++ cms/src/components/modals/LModal.vue
@@ -11,8 +11,28 @@ const isTestEnviroment = import.meta.env.MODE === "test";
 </script>
 
 <template>
-    <Teleport v-if="!isTestEnviroment && isVisible" to="body">
-        <div @click="isVisible = false">
+    <div>
+        <Teleport v-if="!isTestEnviroment && isVisible" :to="App">
+            <div @click="isVisible = false">
+                <div class="fixed inset-0 z-50 bg-zinc-800 bg-opacity-50 backdrop-blur-sm"></div>
+                <div class="fixed inset-0 z-50 flex items-center justify-center rounded-lg p-2">
+                    <div
+                        class="max-h-screen w-full max-w-md rounded-lg bg-white/90 p-5 shadow-xl"
+                        @click.stop
+                    >
+                        <h2 class="mb-4 text-lg font-semibold">{{ heading }}</h2>
+                        <div class="divide-y divide-zinc-200">
+                            <slot></slot>
+                        </div>
+                        <div class="mt-4">
+                            <slot name="footer"></slot>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Teleport>
+        <div v-if="isTestEnviroment && isVisible" @click="isVisible = false">
+            <!-- Same modal content as above, but not teleported in test mode -->
             <div class="fixed inset-0 z-50 bg-zinc-800 bg-opacity-50 backdrop-blur-sm"></div>
             <div class="fixed inset-0 z-50 flex items-center justify-center rounded-lg p-2">
                 <div
@@ -26,24 +46,6 @@ const isTestEnviroment = import.meta.env.MODE === "test";
                     <div class="mt-4">
                         <slot name="footer"></slot>
                     </div>
-                </div>
-            </div>
-        </div>
-    </Teleport>
-    <div v-if="isTestEnviroment && isVisible" @click="isVisible = false">
-        <!-- Same modal content as above, but not teleported in test mode -->
-        <div class="fixed inset-0 z-50 bg-zinc-800 bg-opacity-50 backdrop-blur-sm"></div>
-        <div class="fixed inset-0 z-50 flex items-center justify-center rounded-lg p-2">
-            <div
-                class="max-h-screen w-full max-w-md rounded-lg bg-white/90 p-5 shadow-xl"
-                @click.stop
-            >
-                <h2 class="mb-4 text-lg font-semibold">{{ heading }}</h2>
-                <div class="divide-y divide-zinc-200">
-                    <slot></slot>
-                </div>
-                <div class="mt-4">
-                    <slot name="footer"></slot>
                 </div>
             </div>
         </div>
