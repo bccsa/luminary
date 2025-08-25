67172e755d19bf3ce97542417c516923f1d98f73
Dirk <dirkdevbusiness@gmail.com>
2025-08-25 12:40:16 +0200

    Add new LTeleport component to centralise teleporting to our needs

---
+++ cms/src/components/common/LTeleport.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import App from "@/App.vue";
+import { Component } from "vue";
+
+type Props = {
+    to?: string | Component;
+};
+
+withDefaults(defineProps<Props>(), {
+    to: App,
+});
+
+const isTestEnviroment = import.meta.env.MODE === "test";
+</script>
+
+<template>
+    <Teleport v-if="isTestEnviroment" :to="to">
+        <div>
+            <slot />
+        </div>
+    </Teleport>
+    <div v-else>
+        <slot />
+    </div>
+</template>
