333783cb96e3a045518ea982b7b8db201f28a0c8
Dirk <dirkdevbusiness@gmail.com>
2025-08-25 11:08:32 +0200

    Handle detecting h1 elements better and replacing h6 elements with normal text

--- cms/src/util/formatPastedHtml.ts
+++ cms/src/util/formatPastedHtml.ts
@@ -8,14 +8,14 @@
 export default function formatPastedHtml(html: string) {
     html = html
         .replace(/[\r\n\u2028\u2029]+/g, "") // Remove all line breaks that get added by text editors like word before processing
-        .replace(/\u00AD/g, "")
+        .replace(/\u00AD/g, "") // remove soft hyphens
         .replace(/>\s+</g, "><") // Remove spaces between tags
         .replace(/<br\s*\/?>/gi, "") // Remove standalone <br>
         .replace(/<p>\s*<\/p>/gi, "") // Remove empty paragraphs
         .replace(/&nbsp;/g, " "); // Clean non breaking spaces
 
-    //Only patch headings when an h1 is present
-    if (html.includes("<h1>")) {
+    // Only patch headings when an h1 is present to preserve hierarchy.
+    if (/<h1/i.test(html)) {
         html = html
             .replace(/<h([1-6])([^>]*)>/gi, (_, level, attrs) => {
                 const newLevel = Math.min(parseInt(level) + 1, 6);
@@ -27,9 +27,8 @@ export default function formatPastedHtml(html: string) {
             });
     }
 
-    // Convert <h6> headings to normal text as we don't support it.
-    if (html.includes("h6")) {
-        html = html.replace(/<h6>/gi, "").replace(/<\/h6>/gi, "");
+    if (html.includes("<h6>")) {
+        html = html.replace(/<h6([^>]*)>/gi, "").replace(/<\/h6>/gi, "");
     }
 
     return html;
