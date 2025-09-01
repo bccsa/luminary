6fbf87e8e74a5c41eb84235688bb515be247cc61
Dirk <dirkdevbusiness@gmail.com>
2025-09-01 13:11:25 +0200

    Refactor code structure for improved readability and maintainability

---
+++ cms/src/components/common/LDropdown.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import { onClickOutside } from "@vueuse/core";
+import { ref } from "vue";
+
+const shouldShowDropdown = defineModel<boolean>("shouldShowDropdown", { required: true });
+const dropdownElementRef = ref<HTMLElement | null>(null);
+
+onClickOutside(dropdownElementRef, () => {
+    shouldShowDropdown.value = false;
+});
+</script>
+
+<template>
+    <div ref="dropdownElementRef">
+        <div
+            v-show="shouldShowDropdown"
+            class="w-48 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+        >
+            <slot />
+        </div>
+    </div>
+</template>
--- cms/src/components/content/LanguageSelector.vue
+++ cms/src/components/content/LanguageSelector.vue
@@ -4,6 +4,7 @@ import { type ContentDto, type ContentParentDto, type LanguageDto } from "lumina
 import { ArrowRightIcon } from "@heroicons/vue/16/solid";
 import { onClickOutside } from "@vueuse/core";
 import { ref } from "vue";
+import LDropdown from "../common/LDropdown.vue";
 
 type Props = {
     parent?: ContentParentDto;
@@ -14,7 +15,7 @@ defineProps<Props>();
 
 const emit = defineEmits(["createTranslation"]);
 
-const showSelector = defineModel<boolean>("showSelector");
+const showSelector = defineModel<boolean>("showSelector", { required: true });
 const languages = defineModel<LanguageDto[]>("languages");
 
 const languagePopup = ref();
@@ -25,48 +26,36 @@ onClickOutside(languagePopup, () => {
 </script>
 
 <template>
-    <div ref="languagePopup" class="relative inline-block text-left" data-test="languagePopup">
-        <transition
-            enter-active-class="transition ease-out duration-100"
-            enter-from-class="transform opacity-0 scale-95"
-            enter-to-class="transform opacity-100 scale-100"
-            leave-active-class="transition ease-in duration-75"
-            leave-from-class="transform opacity-100 scale-100"
-            leave-to-class="transform opacity-0 scale-95"
-        >
-            <ul
-                v-show="showSelector"
-                class="absolute z-10 w-56 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:-right-28 sm:left-auto sm:origin-top-right"
-            >
-                <div class="py-1">
-                    <li v-for="language in languages" :key="language.languageCode">
-                        <button
-                            @click="
-                                () => {
-                                    emit('createTranslation', language);
-                                    showSelector = false;
-                                }
-                            "
-                            class="text-zinc-700 hover:bg-zinc-100 hover:text-zinc-900"
-                            :class="[
-                                'group flex w-full items-center justify-between gap-2 px-4 py-2 text-left text-sm',
-                            ]"
-                            :data-test="`select-language-${language.languageCode}`"
-                        >
-                            <span class="flex items-center gap-2">
-                                <LBadge type="language">
-                                    {{ language.languageCode }}
-                                </LBadge>
-                                {{ language.name }}
-                            </span>
+    <LDropdown data-test="languagePopup" :should-show-dropdown="showSelector">
+        <ul>
+            <div class="py-1">
+                <li v-for="language in languages" :key="language.languageCode">
+                    <button
+                        @click="
+                            () => {
+                                emit('createTranslation', language);
+                                showSelector = false;
+                            }
+                        "
+                        class="text-zinc-700 hover:bg-zinc-100 hover:text-zinc-900"
+                        :class="[
+                            'group flex w-full items-center justify-between gap-2 px-4 py-2 text-left text-sm',
+                        ]"
+                        :data-test="`select-language-${language.languageCode}`"
+                    >
+                        <span class="flex items-center gap-2">
+                            <LBadge type="language">
+                                {{ language.languageCode }}
+                            </LBadge>
+                            {{ language.name }}
+                        </span>
 
-                            <ArrowRightIcon
-                                class="hidden h-4 w-4 text-zinc-600 sm:group-hover:inline-block sm:group-active:inline-block"
-                            />
-                        </button>
-                    </li>
-                </div>
-            </ul>
-        </transition>
-    </div>
+                        <ArrowRightIcon
+                            class="hidden h-4 w-4 text-zinc-600 sm:group-hover:inline-block sm:group-active:inline-block"
+                        />
+                    </button>
+                </li>
+            </div>
+        </ul>
+    </LDropdown>
 </template>
