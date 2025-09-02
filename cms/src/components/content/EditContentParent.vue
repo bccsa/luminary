69fbace13bc5c6811288c72d46f019bf3186f7f8
Johan Bell <belljohan3@gmail.com>
2025-09-02 17:49:16 +0100

    remove newDocument prop

--- cms/src/components/content/EditContentParent.vue
+++ cms/src/components/content/EditContentParent.vue
@@ -21,7 +21,6 @@ type Props = {
     tagOrPostType: TagType | PostType;
     language?: LanguageDto;
     disabled: boolean;
-    newDocument?: boolean;
 };
 defineProps<Props>();
 const parent = defineModel<ContentParentDto>("parent");
@@ -43,7 +42,6 @@ const pinned = computed({
     <LCard
         :title="`${capitaliseFirstLetter(tagOrPostType)} settings`"
         :icon="Cog6ToothIcon"
-        :collapsed="newDocument ? false : true"
         collapsible
         v-if="parent"
         class="bg-white"
