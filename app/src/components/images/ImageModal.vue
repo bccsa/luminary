6b0e18850a137fcb74015a849cb31bb27b1f35cb
Dirk <dirkdevbusiness@gmail.com>
2025-09-03 16:14:51 +0200

    Update LImage and ImageModal components to improve layout responsiveness

--- app/src/components/images/ImageModal.vue
+++ app/src/components/images/ImageModal.vue
@@ -280,7 +280,7 @@ onBeforeUnmount(() => {
         </div>
         <div
             ref="container"
-            class="relative w-max origin-center touch-none select-none overflow-hidden rounded-lg"
+            class="relative origin-center touch-none select-none overflow-hidden rounded-lg"
             :style="{
                 transform: `translate(${translateX}px, ${translateY}px) scale(${scale})`,
                 transition: isMouseDragging || isTouchDragging ? 'none' : 'transform 0.1s ease-out',
