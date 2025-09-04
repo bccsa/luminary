f8af7363b885f30c9f87e44d6c51eb222414abf7
Johan Bell <belljohan3@gmail.com>
2025-09-04 08:45:23 +0100

    revert unsed changes

--- app/src/components/form/LModal.vue
+++ app/src/components/form/LModal.vue
@@ -4,24 +4,15 @@ import App from "@/App.vue";
 type Props = {
     heading: string;
     withBackground?: boolean;
-    position?: "center" | "bottom" | "top";
-    fullWidth?: boolean;
 };
 const props = withDefaults(defineProps<Props>(), {
     withBackground: true,
-    position: "center",
-    fullWidth: false,
+    size: "default",
 });
 
 const isVisible = defineModel<boolean>("isVisible");
 const isTestEnviroment = import.meta.env.MODE === "test";
 const emit = defineEmits(["close"]);
-
-const positionClasses = {
-    center: "items-center justify-center",
-    bottom: "items-end justify-center",
-    top: "items-start justify-center",
-};
 </script>
 
 <template>
@@ -33,19 +24,13 @@ const positionClasses = {
         <div
             class="fixed inset-0 z-50 bg-zinc-800 bg-opacity-50 backdrop-blur-sm dark:bg-slate-800 dark:bg-opacity-50"
         ></div>
-        <div
-            class="fixed inset-0 z-50 flex rounded-lg p-0"
-            :class="positionClasses[props.position]"
-        >
+        <div class="fixed inset-0 z-50 flex items-center justify-center rounded-lg p-2">
             <div
-                class="max-h-screen w-full rounded-lg"
+                class="max-h-screen w-full max-w-md rounded-lg"
                 :class="[
-                    props.fullWidth ? ' max-w-7xl' : 'max-w-md',
                     props.withBackground !== false
                         ? 'bg-white/90 p-5 shadow-xl dark:bg-slate-700/85'
                         : '',
-                    props.position === 'bottom' ? 'mb-8' : '',
-                    props.position === 'top' ? 'mt-5' : '',
                 ]"
                 @click.stop
             >
