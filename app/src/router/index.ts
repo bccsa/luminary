eddfcbcfdbec3873f39a87d03a557d42fa9caaeb
RapotuKoketjoKganakga <kganakga.rapotu@gmail.com>
2025-09-10 11:15:28 +0200

    Remove unnecessary blank line in router beforeEach guard

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -85,7 +85,6 @@ const router = createRouter({
 
 router.beforeEach((to, from, next) => {
     if (!from.name && to.name !== "home") {
-        
         router.replace({ name: "home" }).then(() => {
             router.push(to.fullPath);
         });
