56ec771630cc75996b8e8495d08f6fb5a32908c3
RapotuKoketjoKganakga <kganakga.rapotu@gmail.com>
2025-09-10 11:24:38 +0200

    Add removed beforeEach to imports in SingleContent.spec.ts

--- app/src/pages/SingleContent.spec.ts
+++ app/src/pages/SingleContent.spec.ts
@@ -1,5 +1,5 @@
 import "fake-indexeddb/auto";
-import { describe, it, expect, afterEach, vi } from "vitest";
+import { describe, it, expect, afterEach, beforeEach, vi } from "vitest";
 import { mount, shallowMount } from "@vue/test-utils";
 import { setActivePinia } from "pinia";
 import { createTestingPinia } from "@pinia/testing";
