f69638ece0dbd856b94b762b77543a200274ddc2
Dirk <dirkdevbusiness@gmail.com>
2025-09-17 15:54:48 +0200

    feat: add PWA support and update fallback image loading
    - Added `vite-plugin-pwa` to enable Progressive Web App features.
    - Removed several unused fallback images from the public directory.
    - Updated `loadFallbackImages.ts` to dynamically load fallback images from the assets directory.
    - Configured Vite to include assets for PWA and set up Workbox for caching.

--- app/src/util/loadFallbackImages.ts
+++ app/src/util/loadFallbackImages.ts
@@ -1,38 +1,17 @@
 export const loadFallbackImageUrls = async () => {
-    // const images = import.meta.glob<string>("@/assets/fallbackImages/*.{png,jpg,jpeg,webp}", {
-    //     eager: true,
-    //     import: "default",
-    // });
+    const images = import.meta.glob<string>("@/assets/fallbackImages/*.{png,jpg,jpeg,webp}", {
+        eager: true,
+        import: "default",
+    });
 
-    const fallbackImages = [
-      "/bernd.webp",
-      "/blossoms.webp",
-      "/david.webp",
-      "/georgia.webp",
-      "/jura.webp",
-      "/lake.webp",
-      "/landscape.webp",
-      "/logo_small.svg",
-      "/logo.svg",
-      "/mock-image.webp",
-      "/namibia.webp",
-      "/river.webp",
-      "/sea.webp",
-      "/sergey.webp",
-      "/sunset.webp",
-      "/sunset2.webp",
-      "/sunset3.webp",
-    ]
+    const fallbackImages: string[] = Object.values(images);
 
-    // const fallbackImages: string[] = Object.values(images);
+    const preloadImage = (src: string) => {
+        const img = new Image();
+        img.src = src;
+    };
 
-    // const preloadImage = (src: string) => {
-    //   const img = new Image();
-    //   img.src = src;
-    // };
-
-    // Preload all fallback images
-    // fallbackImages.forEach(preloadImage);
+    fallbackImages.forEach(preloadImage);
 
     return fallbackImages;
 };
