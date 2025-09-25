031d836f96d56fb1f8b762460c1748f745a892c1
Johan Bell <belljohan3@gmail.com>
2025-09-25 22:27:26 +0100

    Add upgrade step for schema version 7 to 8 in upgradeDbSchema function

--- api/src/db/db.upgrade.ts
+++ api/src/db/db.upgrade.ts
@@ -42,7 +42,7 @@ export async function upgradeDbSchema(db: DbService, s3: S3Service, s3Audio: S3A
         await v7(db);
 
         // Upgrade from schema version 7 to 8
-        await v8(db);
+        await v8(db, s3, s3Audio);
 
         // Upgrade from schema version 8 to 9
         await v9(db);
