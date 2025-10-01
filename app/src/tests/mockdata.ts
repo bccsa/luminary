57be013d1f3ac57eb5fa9c1e8b5454db1fafe88a
Johan Bell <belljohan3@gmail.com>
2025-10-01 01:18:55 +0100

    refactor: update mock English, French, and Swahili content DTOs to include parentMedia structure

--- app/src/tests/mockdata.ts
+++ app/src/tests/mockdata.ts
@@ -105,8 +105,6 @@ export const mockEnglishContentDto: ContentDto = {
     author: "ChatGPT",
     text: '{"type":"doc","content":[{"type":"paragraph","content":[{"type":"text","text":"In the quiet town of Willowdale, little Lily wept as her beloved cat, Whiskers, went missing. Frantically searching the neighborhood, she stumbled upon Fireman Jake, known for his kind heart. With a reassuring smile, he promised to help. Lily clung to hope as they combed the streets together. Beneath a dusty porch, they found Whiskers, scared but unharmed. Grateful tears filled Lily\'s eyes as Fireman Jake handed her the rescued feline. Their small town echoed with cheers as Lily hugged her furry friend, and from that day forward, Fireman Jake became a hero in her heart and the community\'s beloved guardian"}]}]}',
     localisedImage: "",
-    audio: "",
-    video: "",
     publishDate: 1704114000000,
     expiryDate: undefined,
     parentImageData: {
@@ -127,6 +125,10 @@ export const mockEnglishContentDto: ContentDto = {
     seoTitle: "Test Seo Title",
     seoString: "Test seo summary",
     availableTranslations: ["lang-eng", "lang-fra"],
+    parentMedia: {
+        hlsUrl: "https://example.com/video.m3u8",
+        fileCollections: [],
+    },
 };
 export const mockFrenchContentDto: ContentDto = {
     _id: "content-post1-fra",
@@ -144,8 +146,6 @@ export const mockFrenchContentDto: ContentDto = {
     author: "ChatGPT",
     text: '{"type":"doc","content":[{"type":"paragraph","content":[{"type":"text","text":"Dans la paisible ville de Willowdale, la petite Lily pleurait la disparition de son cher chat, Whiskers. Cherchant frénétiquement dans le quartier, elle tomba sur le pompier Jake, réputé pour son cœur généreux. Avec un sourire rassurant, il promit de l\'aider. Lily s\'accrocha à l\'espoir alors qu\'ils parcouraient les rues ensemble. Sous un porche poussiéreux, ils trouvèrent Whiskers, effrayé mais sain et sauf. Des larmes de gratitude remplirent les yeux de Lily lorsque le pompier Jake lui remit le félin sauvé. Leur petite ville résonna de joie tandis que Lily serrait son ami à fourrure dans ses bras, et dès ce jour, le pompier Jake devint un héros dans son cœur et le gardien bien-aimé de la communauté."}]}]}',
     localisedImage: "",
-    audio: "",
-    video: "",
     publishDate: 1704114000000,
     expiryDate: undefined,
     parentImageData: {
@@ -164,6 +164,10 @@ export const mockFrenchContentDto: ContentDto = {
     } as ImageDto,
     parentPublishDateVisible: true,
     availableTranslations: ["lang-eng", "lang-fra"],
+    parentMedia: {
+        hlsUrl: "",
+        fileCollections: [],
+    },
 };
 export const mockSwahiliContentDto: ContentDto = {
     _id: "content-post1-swa",
@@ -181,8 +185,7 @@ export const mockSwahiliContentDto: ContentDto = {
     author: "ChatGPT",
     text: '{"type":"doc","content":[{"type":"paragraph","content":[{"type":"text","text":"Dans la paisible ville de Willowdale, la petite Lily pleurait la disparition de son cher chat, Whiskers. Cherchant frénétiquement dans le quartier, elle tomba sur le pompier Jake, réputé pour son cœur généreux. Avec un sourire rassurant, il promit de l\'aider. Lily s\'accrocha à l\'espoir alors qu\'ils parcouraient les rues ensemble. Sous un porche poussiéreux, ils trouvèrent Whiskers, effrayé mais sain et sauf. Des larmes de gratitude remplirent les yeux de Lily lorsque le pompier Jake lui remit le félin sauvé. Leur petite ville résonna de joie tandis que Lily serrait son ami à fourrure dans ses bras, et dès ce jour, le pompier Jake devint un héros dans son cœur et le gardien bien-aimé de la communauté."}]}]}',
     localisedImage: "",
-    audio: "",
-    video: "",
+
     publishDate: 1704114000000,
     expiryDate: undefined,
     parentImageData: {
@@ -200,6 +203,10 @@ export const mockSwahiliContentDto: ContentDto = {
         ],
     } as ImageDto,
     parentPublishDateVisible: true,
+    parentMedia: {
+        hlsUrl: "",
+        fileCollections: [],
+    },
 };
 export const mockCategoryContentDto: ContentDto = {
     _id: "content-tag-category1",
