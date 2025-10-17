d6bfc71b8e90dad118a1617f30335ce23123fd2c
Dirk <dirkdevbusiness@gmail.com>
2025-10-17 09:55:17 +0200

    Add LHighlightable component for text highlighting in SingleContent

--- app/src/pages/SingleContent/SingleContent.vue
+++ app/src/pages/SingleContent/SingleContent.vue
@@ -52,6 +52,7 @@ import { markLanguageSwitch } from "@/util/isLangSwitch";
 import LoadingSpinner from "@/components/LoadingSpinner.vue";
 import { activeImageCollection } from "@/components/images/LImageProvider.vue";
 import { isExternalNavigation } from "@/router";
+import LHighlightable from "@/components/common/LHighlightable.vue";
 
 const VideoPlayer = defineAsyncComponent({
     loader: () => import("@/components/content/VideoPlayer.vue"),
@@ -682,14 +683,16 @@ const quickLanguageSwitch = (languageId: string) => {
                         </span>
                     </div>
 
-                    <div
-                        v-if="content.text"
-                        v-html="text"
-                        class="prose prose-zinc mt-3 max-w-full dark:prose-invert"
-                        :class="{
-                            'border-t-2 border-yellow-500/25 pt-2': categoryTags.length == 0,
-                        }"
-                    ></div>
+                    <LHighlightable>
+                        <div
+                            v-if="content.text"
+                            v-html="text"
+                            class="prose prose-zinc mt-3 max-w-full dark:prose-invert"
+                            :class="{
+                                'border-t-2 border-yellow-500/25 pt-2': categoryTags.length == 0,
+                            }"
+                        ></div>
+                    </LHighlightable>
                 </article>
             </div>
 
