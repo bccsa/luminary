76277d1a3c384ab4e4a11b6cf2de00a782f55686
Dirk <dirkdevbusiness@gmail.com>
2025-10-20 10:03:54 +0200

    refactor: remove unused language forcing logic in SingleContent component

--- app/src/pages/SingleContent.vue
+++ app/src/pages/SingleContent.vue
@@ -373,19 +373,6 @@ const selectedCategory = computed(() => {
     return tags.value.find((t) => t.parentId == selectedCategoryId.value);
 });
 
-const langToForce = queryParams.get("lang");
-// If lang query param is set, force that language if available
-watch([availableTranslations, languages], () => {
-    if (!langToForce || !availableTranslations.value.length || !languages.value.length) return;
-    const lang = languages.value.find((l) => l.languageCode === langToForce);
-    if (!lang) return;
-    const translation = availableTranslations.value.find((c) => c.language === lang._id);
-    if (!translation) return;
-    selectedLanguageId.value = lang._id;
-    // Update content without triggering a route change by replacing the slug
-    content.value = translation;
-});
-
 /**
  * Watches for changes in the `content` reactive property.
  * When `content` is updated, it sets the `selectedLanguageId`
