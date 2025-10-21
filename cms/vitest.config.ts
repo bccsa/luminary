bf96bbc0d40f5cba0bdd57628de246d9cb5a2e9e
Dirk <dirkdevbusiness@gmail.com>
2025-10-21 14:04:20 +0200

    Increase test timeout to 10 seconds in vitest configuration

--- cms/vitest.config.ts
+++ cms/vitest.config.ts
@@ -11,6 +11,7 @@ export default mergeConfig(
             exclude: [...configDefaults.exclude, "e2e/*"],
             root: fileURLToPath(new URL("./", import.meta.url)),
             setupFiles: ["vitest.setup.ts"],
+            testTimeout: 10_000,
         },
     }),
 );
