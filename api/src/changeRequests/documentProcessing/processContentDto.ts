9df0d1a8b9e7ee629457afc9a87de824d45e329b
Johan Bell <belljohan3@gmail.com>
2025-10-24 13:46:16 +0100

    feat: add parentImageBucketId and parentMediaBucketId to processContentDto

--- api/src/changeRequests/documentProcessing/processContentDto.ts
+++ api/src/changeRequests/documentProcessing/processContentDto.ts
@@ -22,6 +22,8 @@ export default async function processContentDto(doc: ContentDto, db: DbService) 
         doc.memberOf = parentDoc.memberOf;
         doc.parentTags = parentDoc.tags;
         doc.parentImageData = parentDoc.imageData;
+        doc.parentImageBucketId = parentDoc.imageBucketId;
+        doc.parentMediaBucketId = parentDoc.mediaBucketId;
 
         if (parentDoc.type == DocType.Post) {
             doc.parentPostType = (parentDoc as PostDto).postType;
