d135262055167df921b89e2268851f9e998987f5
Johan Bell <belljohan3@gmail.com>
2025-10-31 00:27:59 +0100

    feat: implement bucket creation button and refactor S3ManagementPage layout

--- cms/src/pages/S3ManagementPage.vue
+++ cms/src/pages/S3ManagementPage.vue
@@ -1,11 +1,40 @@
 <script setup lang="ts">
 import BasePage from "@/components/BasePage.vue";
 import S3BucketOverview from "@/components/s3/S3BucketOverview.vue";
+import LButton from "@/components/button/LButton.vue";
+import { PlusIcon } from "@heroicons/vue/24/outline";
+import { ref } from "vue";
+import { isSmallScreen } from "@/globalConfig";
+
+const bucketOverviewRef = ref<InstanceType<typeof S3BucketOverview> | null>(null);
+
+const createNew = () => {
+    bucketOverviewRef.value?.openCreateModal();
+};
 </script>
 
 <template>
     <BasePage title="S3 Storage Overview" :should-show-page-title="true">
-        <div class="space-y-6">
+        <template #pageNav>
+            <div class="flex gap-4">
+                <LButton
+                    v-if="!isSmallScreen"
+                    variant="primary"
+                    :icon="PlusIcon"
+                    @click="createNew"
+                    name="createBucketBtn"
+                >
+                    Add Bucket
+                </LButton>
+                <PlusIcon
+                    v-else-if="isSmallScreen"
+                    class="h-6 w-6 text-zinc-500"
+                    @click="createNew"
+                />
+            </div>
+        </template>
+
+        <div class="space-y-1">
             <div>
                 <p class="text-gray-600">
                     Manage S3 bucket configurations for file storage and uploads.
@@ -13,7 +42,7 @@ import S3BucketOverview from "@/components/s3/S3BucketOverview.vue";
             </div>
 
             <!-- S3 Bucket Overview Component -->
-            <S3BucketOverview />
+            <S3BucketOverview ref="bucketOverviewRef" />
         </div>
     </BasePage>
 </template>
