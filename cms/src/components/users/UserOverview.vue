f02667de9f26d92c3230e08c4d56915566a8399a
Christian <touoyim14christian@gmail.com>
2025-11-04 13:20:10 +0200

    Refactor MobileSideBar component by removing unused TransitionChild elements and adjusting styles for improved clarity and performance. Update UserOverview and DashboardPage components to enhance text styling for better readability.

--- cms/src/components/users/UserOverview.vue
+++ cms/src/components/users/UserOverview.vue
@@ -35,7 +35,7 @@ const createNew = () => {
                 />
             </div>
         </template>
-        <p class="mb-4 text-gray-500">
+        <p class="mb-4 p-2 text-gray-500">
             Users only need to be created when they require special permissions that are not already
             automatically granted. It's possible to add multiple user objects with the same email
             address. This allows different administrators to independently assign access to the same
