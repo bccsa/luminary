5ed09c575e3f9120e8a88a7deb912b7f4b7172f3
Dirk <dirkdevbusiness@gmail.com>
2025-11-06 13:18:18 +0200

    SideBar and MobileSideBar moved from the BasePage to the App.vue (#1194)

--- cms/src/App.vue
+++ cms/src/App.vue
@@ -2,12 +2,15 @@
 import { useAuth0 } from "@auth0/auth0-vue";
 import { RouterView } from "vue-router";
 import LoadingBar from "@/components/LoadingBar.vue";
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import { appName } from "@/globalConfig";
 import NotificationManager from "./components/notifications/NotificationManager.vue";
 import router from "./router";
+import MobileSideBar from "@/components/navigation/MobileSideBar.vue";
+import SideBar from "@/components/navigation/SideBar.vue";
 
 const { isAuthenticated } = useAuth0();
+const sidebarOpen = ref(false);
 
 const routeKey = computed(() => {
     let routeKey = router.currentRoute.value.fullPath;
@@ -24,14 +27,20 @@ const routeKey = computed(() => {
 
 <template>
     <template v-if="isAuthenticated">
-        <div class="relative h-screen min-h-screen w-full overflow-hidden">
-            <main class="absolute left-0 top-0 h-full w-full overflow-hidden pl-0">
+        <div class="relative flex h-screen w-full overflow-hidden">
+            <MobileSideBar v-model:open="sidebarOpen" />
+
+            <div class="hidden lg:fixed lg:inset-y-0 lg:z-30 lg:flex lg:w-72 lg:flex-col">
+                <SideBar @close="sidebarOpen = false" />
+            </div>
+
+            <div class="flex flex-1 flex-col lg:pl-72">
                 <div class="h-full w-full overflow-hidden">
-                    <!-- The routeKey disables component reuse in cases where data needs to be reloaded for dynamic
-                    routes (e.g. Post / Tag overviews) -->
-                    <RouterView :key="routeKey" />
+                    <RouterView :key="routeKey" v-slot="{ Component }">
+                        <component :is="Component" @open-mobile-sidebar="sidebarOpen = true" />
+                    </RouterView>
                 </div>
-            </main>
+            </div>
         </div>
     </template>
 
