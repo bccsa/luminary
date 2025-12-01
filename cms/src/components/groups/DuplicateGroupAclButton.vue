44bb588b074ed7b44810b804a9bc8027790cd683
Christian <touoyim14christian@gmail.com>
2025-12-01 14:35:14 +0200

    refactor: Remove Headless UI components from DuplicateGroupAclButton.vue and replace with custom dropdown

--- cms/src/components/groups/DuplicateGroupAclButton.vue
+++ cms/src/components/groups/DuplicateGroupAclButton.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import type { GroupDto } from "luminary-shared";
-import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
 import { DocumentDuplicateIcon } from "@heroicons/vue/20/solid";
-import { toRaw, toRefs } from "vue";
+import { toRaw, toRefs, ref } from "vue";
 import LButton from "../button/LButton.vue";
+import LDropdown from "../common/LDropdown.vue";
 
 type Props = {
     groups: GroupDto[];
@@ -12,6 +12,7 @@ type Props = {
 const props = defineProps<Props>();
 
 const { groups } = toRefs(props);
+const show = ref(false);
 
 const emit = defineEmits(["select"]);
 
@@ -22,49 +23,39 @@ const selectGroup = (group: GroupDto) => {
 
 <template>
     <div class="relative">
-        <Menu as="div" class="inline-block text-left">
-            <div>
-                <MenuButton
-                    :as="LButton"
-                    :icon="DocumentDuplicateIcon"
-                    class="gap-x-0"
-                    variant="tertiary"
-                    size="sm"
-                    title="Duplicate"
-                    data-test="duplicateAclIcon"
-                />
-            </div>
-
-            <transition
-                enter-active-class="transition duration-100 ease-out"
-                enter-from-class="transform scale-95 opacity-0"
-                enter-to-class="transform scale-100 opacity-100"
-                leave-active-class="transition duration-75 ease-in"
-                leave-from-class="transform scale-100 opacity-100"
-                leave-to-class="transform scale-95 opacity-0"
-            >
-                <MenuItems
+        <div class="inline-block text-left">
+            <LButton
+                :icon="DocumentDuplicateIcon"
+                class="gap-x-0"
+                variant="tertiary"
+                size="sm"
+                title="Duplicate"
+                data-test="duplicateAclIcon"
+                @click.stop="show = !show"
+            />
+            <div v-if="show">
+                <LDropdown
                     class="absolute right-0 z-20 mt-2 w-52 origin-top-left divide-y divide-zinc-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none"
+                    :show="true"
                 >
                     <div class="px-1 py-1">
-                        <MenuItem v-slot="{ active }" v-for="group in groups" :key="group._id">
+                        <div v-for="group in groups" :key="group._id">
                             <button
                                 :class="[
-                                    'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                                    { 'bg-zinc-100': active },
+                                    'group flex w-full items-center rounded-md px-2 py-2 text-sm hover:bg-zinc-100 focus:bg-zinc-100',
                                 ]"
                                 @click="() => selectGroup(group)"
                                 data-test="selectGroupIcon"
                             >
                                 {{ group.name }}
                             </button>
-                        </MenuItem>
-                        <MenuItem v-if="groups.length == 0">
+                        </div>
+                        <div v-if="groups.length == 0">
                             <div class="px-2 py-2 text-sm text-zinc-500">All groups added</div>
-                        </MenuItem>
+                        </div>
                     </div>
-                </MenuItems>
-            </transition>
-        </Menu>
+                </LDropdown>
+            </div>
+        </div>
     </div>
 </template>
