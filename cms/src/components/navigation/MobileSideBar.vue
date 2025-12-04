b00466986ae68f34e9086b7cdd281cececa112b1
Christian <touoyim14christian@gmail.com>
2025-12-04 15:58:36 +0200

    Remove headlessui from the MobileSidebar

--- cms/src/components/navigation/MobileSideBar.vue
+++ cms/src/components/navigation/MobileSideBar.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Dialog, DialogPanel, TransitionRoot } from "@headlessui/vue";
+import { Dialog, DialogPanel } from "@headlessui/vue";
 import { XMarkIcon } from "@heroicons/vue/24/outline";
 import SideBar from "@/components/navigation/SideBar.vue";
 
@@ -15,30 +15,25 @@ const emit = defineEmits(["update:open"]);
 </script>
 
 <template>
-    <TransitionRoot as="template" :show="open">
-        <Dialog
-            as="div"
-            class="relative z-50 overflow-hidden lg:hidden"
-            @close="emit('update:open', false)"
-        >
-            <div class="fixed inset-0 bg-zinc-900/50" />
+    <Dialog
+        as="div"
+        :open="open"
+        class="relative z-50 overflow-hidden lg:hidden"
+        @close="emit('update:open', false)"
+    >
+        <div class="fixed inset-0 bg-zinc-900/50" />
 
-            <div class="fixed inset-0 flex">
-                <DialogPanel class="relative mr-16 flex w-full max-w-xs flex-1">
-                    <div class="absolute left-full top-0 flex w-16 justify-center pt-5">
-                        <button
-                            type="button"
-                            class="-m-2.5 p-2.5"
-                            @click="emit('update:open', false)"
-                        >
-                            <span class="sr-only">Close sidebar</span>
-                            <XMarkIcon class="h-6 w-6 text-white" aria-hidden="true" />
-                        </button>
-                    </div>
+        <div class="fixed inset-0 flex">
+            <DialogPanel class="relative mr-16 flex w-full max-w-xs flex-1">
+                <div class="absolute left-full top-0 flex w-16 justify-center pt-5">
+                    <button type="button" class="-m-2.5 p-2.5" @click="emit('update:open', false)">
+                        <span class="sr-only">Close sidebar</span>
+                        <XMarkIcon class="h-6 w-6 text-white" aria-hidden="true" />
+                    </button>
+                </div>
 
-                    <SideBar @close="emit('update:open', false)" />
-                </DialogPanel>
-            </div>
-        </Dialog>
-    </TransitionRoot>
+                <SideBar @close="emit('update:open', false)" />
+            </DialogPanel>
+        </div>
+    </Dialog>
 </template>
