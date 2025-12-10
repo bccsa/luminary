26949f599507cc28c5b2c644f59deb11c85317a5
Johan Bell <belljohan3@gmail.com>
2025-12-10 21:11:22 +0100

    feat: Introduce UserDisplayCard component for user details and update UserOverview to utilize it

--- cms/src/components/users/UserOverview.vue
+++ cms/src/components/users/UserOverview.vue
@@ -1,14 +1,33 @@
 <script setup lang="ts">
 import BasePage from "@/components/BasePage.vue";
-import UserTable from "@/components/users/UserTable.vue";
+import UserDisplayCard from "@/components/users/UserDisplayCard.vue";
 import { PlusIcon } from "@heroicons/vue/24/outline";
-import { AclPermission, db, DocType, hasAnyPermission } from "luminary-shared";
-import { computed } from "vue";
+import {
+    AclPermission,
+    db,
+    DocType,
+    hasAnyPermission,
+    type UserDto,
+    type ApiSearchQuery,
+    ApiLiveQuery,
+} from "luminary-shared";
+import { computed, ref, onBeforeUnmount } from "vue";
 import LButton from "../button/LButton.vue";
 import { isSmallScreen } from "@/globalConfig";
 import router from "@/router";
 
 const canCreateNew = computed(() => hasAnyPermission(DocType.User, AclPermission.Edit));
+
+const usersQuery = ref<ApiSearchQuery>({
+    types: [DocType.User],
+});
+
+const apiLiveQuery = new ApiLiveQuery<UserDto>(usersQuery);
+const users = apiLiveQuery.toArrayAsRef();
+
+onBeforeUnmount(() => {
+    apiLiveQuery.stopLiveQuery();
+});
 
 const createNew = () => {
     router.push({ name: "user", params: { id: db.uuid() } });
@@ -16,7 +35,7 @@ const createNew = () => {
 </script>
 
 <template>
-    <BasePage :is-full-width="true" title="User overview" :should-show-page-title="false">
+    <BasePage title="User overview" :should-show-page-title="false" :is-full-width="true">
         <template #pageNav>
             <div class="flex gap-4" v-if="canCreateNew">
                 <LButton
@@ -35,12 +54,10 @@ const createNew = () => {
                 />
             </div>
         </template>
-        <p class="mb-4 p-2 text-gray-500">
-            Users only need to be created when they require special permissions that are not already
-            automatically granted. It's possible to add multiple user objects with the same email
-            address. This allows different administrators to independently assign access to the same
-            individual for different groups they manage.
-        </p>
-        <UserTable />
+        <UserDisplayCard
+            v-for="user in users"
+            :key="user._id"
+            :usersDoc="user"
+        />
     </BasePage>
 </template>
