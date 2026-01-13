85f62427004d8ad952f092eb8abfabc480c808f4
Johan Bell <belljohan3@gmail.com>
2026-01-13 17:34:26 +0100

    refactor: simplify ChangeRequestService instantiation by removing unnecessary parameters for improved clarity

--- api/src/endpoints/changeRequest.service.spec.ts
+++ api/src/endpoints/changeRequest.service.spec.ts
@@ -22,7 +22,7 @@ describe("ChangeRequest service", () => {
         }`;
 
         service = (await createTestingModule("changereq-service")).dbService;
-        changeRequestService = new ChangeRequestService(undefined, service, undefined, undefined);
+        changeRequestService = new ChangeRequestService(undefined, service);
     });
 
     afterAll(async () => {
