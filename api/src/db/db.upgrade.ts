29b22033e5dd117b0883908cc83dc0b9e255904c
Dirk <dirkdevbusiness@gmail.com>
2026-02-03 09:46:26 +0200

    fix: Remove historical schema upgrade notes as all production databases are now at schema version 9

--- api/src/db/db.upgrade.ts
+++ api/src/db/db.upgrade.ts
@@ -4,12 +4,6 @@ import v9 from "./schemaUpgrade/v9";
 /**
  * Upgrade the database schema
  *
- * Historical schema upgrades (v1-v9) have been removed as all production databases
- * are now at schema version 9. New schema upgrades should be added here when needed.
- *
- * Note: If there are several instances of the API, they will all try to upgrade
- * the database schema at the same time if they are all started at the same time.
- * It might be needed to add a lock mechanism to prevent this.
  */
 export async function upgradeDbSchema(db: DbService) {
     try {
