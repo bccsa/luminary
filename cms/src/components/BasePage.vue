5a8044034108b36c7ab96ad908eca1639c82db5f
Christian Touoyim <touoyim14christian@gmail.com>
2026-02-06 09:08:33 +0200

    some visual improvments

--- cms/src/components/BasePage.vue
+++ cms/src/components/BasePage.vue
@@ -46,7 +46,7 @@ const isMobileScreen = breakpoints.smaller("lg");
             >
                 <button
                     type="button"
-                    class="-m-2.5 p-2.5 text-zinc-700"
+                    class="-m-2.5 p-2.5 text-zinc-500"
                     @click="
                         !isEditContentPage
                             ? onOpenMobileSidebar?.()
@@ -61,7 +61,7 @@ const isMobileScreen = breakpoints.smaller("lg");
                     />
                     <ChevronLeftIcon
                         v-else-if="isEditContentPage"
-                        class="h-6 w-6"
+                        class="size-5"
                         aria-hidden="true"
                     />
                 </button>
