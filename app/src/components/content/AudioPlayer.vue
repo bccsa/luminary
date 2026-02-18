33c82cd92afaa1d2ba5b1b38faa83a9470d0108d
Johan Bell <belljohan3@gmail.com>
2026-02-18 20:36:39 +0100

    fix shadow

--- app/src/components/content/AudioPlayer.vue
+++ app/src/components/content/AudioPlayer.vue
@@ -979,7 +979,7 @@ watch(matchAudioFileUrl, async (newUrl, oldUrl) => {
         <transition name="slide-up">
             <div
                 v-show="isExpanded"
-                class="expanded-player fixed bottom-[70px] left-0 right-0 z-50 flex max-h-[80vh] w-full flex-col justify-items-end overflow-auto bg-amber-50 scrollbar-hide dark:bg-slate-600 sm:shadow-2xl sm:shadow-black/20 md:shadow-2xl md:shadow-black/20 lg:bottom-5 lg:left-auto lg:right-5 lg:max-h-none lg:w-80 lg:rounded-2xl"
+                class="expanded-player fixed bottom-[70px] left-0 right-0 z-50 flex max-h-[80vh] w-full flex-col justify-items-end overflow-auto bg-amber-50 scrollbar-hide dark:bg-slate-600 lg:bottom-5 lg:left-auto lg:right-5 lg:max-h-none lg:w-80 lg:rounded-2xl lg:shadow-2xl lg:shadow-black/20"
                 :style="{
                     transform: currentY ? `translateY(${currentY}px)` : 'none',
                     transition: isDragging ? 'none' : 'transform 0.3s ease-out',
@@ -1430,7 +1430,7 @@ watch(matchAudioFileUrl, async (newUrl, oldUrl) => {
         <div
             v-if="!isExpanded"
             @click="toggleExpand"
-            class="fixed bottom-[70px] left-0 right-0 z-50 flex w-full cursor-pointer items-center justify-between bg-amber-50 p-2 dark:bg-slate-600 md:shadow-lg lg:bottom-5 lg:left-auto lg:right-5 lg:w-80 lg:rounded-lg"
+            class="fixed bottom-[70px] left-0 right-0 z-50 flex w-full cursor-pointer items-center justify-between bg-amber-50 p-2 dark:bg-slate-600 lg:bottom-5 lg:left-auto lg:right-5 lg:w-80 lg:rounded-lg lg:shadow-lg"
         >
             <div class="flex min-w-0 items-center space-x-2">
                 <LImage
