bf401f2865e4f1a877fcd14bd54bc955cf4a74e7
Christian Touoyim <touoyim14christian@gmail.com>
2026-02-25 17:31:11 +0200

    fix: Enhance mobile responsiveness for ACL group editing

--- cms/src/components/groups/EditAclEntry.vue
+++ cms/src/components/groups/EditAclEntry.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import { type GroupAclEntryDto, AclPermission, type GroupDto } from "luminary-shared";
-import { defineModel, defineProps, toRaw } from "vue";
+import { toRaw } from "vue";
 import { capitaliseFirstLetter } from "@/util/string";
 import { isPermissionAvailable, hasChangedPermission, validateAclEntry } from "./permissions";
 import { CheckCircleIcon, XCircleIcon } from "@heroicons/vue/20/solid";
 import _ from "lodash";
+import { isMobileScreen } from "@/globalConfig";
 
 type Props = {
     /**
@@ -35,7 +36,7 @@ const setPermission = (aclPermission: AclPermission) => {
 </script>
 
 <template>
-    <tr v-if="aclEntry" class="border-b border-zinc-200 last:border-none">
+    <tr v-if="aclEntry && !isMobileScreen" class="border-b border-zinc-200 last:border-none">
         <th
             scope="row"
             :class="['py-3 pl-6 pr-10 text-left font-medium', { 'text-zinc-400': disabled }]"
@@ -91,4 +92,38 @@ const setPermission = (aclPermission: AclPermission) => {
             </template>
         </td>
     </tr>
+
+    <div v-else-if="aclEntry" class="rounded-md border border-zinc-200 bg-white p-4 shadow-sm">
+        <div class="mb-3 font-medium text-zinc-900">
+            {{ capitaliseFirstLetter(aclEntry.type) }}
+        </div>
+        <div class="flex flex-wrap gap-2">
+            <button
+                v-for="aclPermission in AclPermission"
+                :key="aclPermission"
+                v-show="isPermissionAvailable(aclEntry.type, aclPermission)"
+                @click="
+                    () => {
+                        if (!disabled) setPermission(aclPermission);
+                    }
+                "
+                :disabled="disabled"
+                class="flex items-center gap-1 rounded-md border-zinc-300 bg-zinc-100 px-3 py-1 text-xs font-medium text-zinc-600 transition-colors"
+            >
+                <CheckCircleIcon
+                    v-if="aclEntry.permission.includes(aclPermission)"
+                    :class="[
+                        'inline h-3 w-3',
+                        isPermissionAvailable(aclEntry.type, aclPermission)
+                            ? disabled
+                                ? 'text-zinc-300'
+                                : 'text-zinc-500'
+                            : 'text-zinc-200',
+                    ]"
+                />
+                <div v-else class="h-3 w-3 rounded-md border border-zinc-400"></div>
+                {{ capitaliseFirstLetter(aclPermission) }}
+            </button>
+        </div>
+    </div>
 </template>
