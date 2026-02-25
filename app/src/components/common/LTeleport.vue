35517122e73f371accbf4a70e10af95d21d6ded8
Christian Touoyim <touoyim14christian@gmail.com>
2026-02-25 11:33:56 +0200

    Improve the BaseModal element by adding the LTeleport component

---
+++ app/src/components/common/LTeleport.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { type Component } from "vue";
+
+type Props = {
+    to?: string | Component;
+};
+
+defineProps<Props>();
+
+const isTestEnviroment = import.meta.env.MODE === "test";
+</script>
+
+<template>
+    <Teleport
+        v-if="!isTestEnviroment"
+        :to="to ?? 'body'"
+    >
+        <slot />
+    </Teleport>
+    <div v-else>
+        <slot />
+    </div>
+</template>
--- app/src/components/form/BaseModal.vue
+++ app/src/components/form/BaseModal.vue
@@ -1,25 +1,23 @@
 <script setup lang="ts">
-import App from "@/App.vue";
+import LTeleport from "../common/LTeleport.vue";
 
 const isVisible = defineModel<boolean>("isVisible");
-const isTestEnviroment = import.meta.env.MODE === "test";
 const emit = defineEmits(["close"]);
 </script>
 
 <template>
-    <div
-        v-if="isVisible"
-        v-bind="isTestEnviroment ? {} : { teleport: { to: App } }"
-    >
-        <div
-            class="fixed inset-0 z-[100] bg-zinc-800 bg-opacity-50 backdrop-blur-sm dark:bg-slate-800 dark:bg-opacity-50"
-            @click="emit('close')"
-        ></div>
-        <div
-            class="fixed inset-0 z-[100] flex items-center justify-center rounded-lg p-2"
-            @click.self="emit('close')"
-        >
-            <slot></slot>
+    <LTeleport v-if="isVisible">
+        <div class="relative z-[100]">
+            <div
+                class="fixed inset-0 bg-zinc-800 bg-opacity-50 backdrop-blur-sm dark:bg-slate-800 dark:bg-opacity-50"
+                @click="emit('close')"
+            ></div>
+            <div
+                class="fixed inset-0 flex items-center justify-center rounded-lg p-2"
+                @click.self="emit('close')"
+            >
+                <slot />
+            </div>
         </div>
-    </div>
+    </LTeleport>
 </template>
