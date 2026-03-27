8158c3b2ded534288f820507fd0d2d76deb0a9c6
Dirk <dirkdevbusiness@gmail.com>
2026-03-27 16:37:33 +0200

    Fix failing tests and upgrade rte-vue

--- cms/src/components/content/__tests__/EditContent/EditContent.editor.spec.ts
+++ cms/src/components/content/__tests__/EditContent/EditContent.editor.spec.ts
@@ -81,18 +81,8 @@ describe("EditContent - Rich Text Editor and Slug Management", () => {
         expect(authorInput.exists()).toBe(true);
         await authorInput.setValue("New Author");
 
-        const updatedContent = JSON.stringify({
-            type: "doc",
-            content: [
-                {
-                    type: "paragraph",
-                    content: [{ type: "text", text: "New Content" }],
-                },
-            ],
-        });
-
         //@ts-ignore -- valid code
-        richTextEditor.vm.text = updatedContent;
+        richTextEditor.vm.text = "<p>New Content</p>";
 
         await waitForExpect(() => {
             expect(richTextEditor.vm.text).toContain("New Content");
@@ -107,9 +97,7 @@ describe("EditContent - Rich Text Editor and Slug Management", () => {
             const res = await db.localChanges.toArray();
 
             expect(res.length).toBe(2);
-            expect(JSON.parse((res[1].doc as any).text).content[0].content[0].text).toBe(
-                "New Content",
-            );
+            expect((res[1].doc as any).text).toContain("New Content");
         });
     });
 
