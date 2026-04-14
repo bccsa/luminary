112be4d430c3433768a1d8266d8ecd20916e5047
Johan Bell <belljohan3@gmail.com>
2026-04-14 13:30:35 +0100

    format

--- api/src/db/db.service.spec.ts
+++ api/src/db/db.service.spec.ts
@@ -850,7 +850,7 @@ describe("DbService", () => {
                 postType: PostType.Blog,
                 tags: [],
                 publishDateVisible: true,
-    showComingSoon: false,
+                showComingSoon: false,
             };
 
             const insertResult = await service.insertDeleteCmd({
