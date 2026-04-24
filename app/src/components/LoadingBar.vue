dc62801d1b161312bc90c293d0f37dae476ddb6b
Dirk <dirkdevbusiness@gmail.com>
2026-04-24 14:37:20 +0200

    Fix conflicts

--- app/src/components/LoadingBar.vue
+++ app/src/components/LoadingBar.vue
@@ -1,13 +1,20 @@
+<script setup lang="ts">
+type Props = {
+    label?: string;
+};
+defineProps<Props>();
+</script>
+
 <template>
     <div class="flex w-full flex-col items-center gap-4">
         <div class="text-2xl font-semibold text-zinc-500 dark:text-slate-400 max-lg:text-xl">
-            Loading...
+            {{ label }}
         </div>
         <div
-            class="relative h-3 w-2/5 max-w-md overflow-hidden rounded-full bg-zinc-200 dark:bg-slate-700"
+            class="relative h-3 w-4/5 max-w-md overflow-hidden rounded-full bg-zinc-200 dark:bg-slate-700"
         >
             <div
-                class="animate-running-slug absolute bottom-0 top-0 w-2/5 rounded-full bg-zinc-500 dark:bg-slate-400"
+                class="absolute bottom-0 top-0 w-2/5 animate-running-slug rounded-full bg-zinc-500 dark:bg-slate-400"
             ></div>
         </div>
     </div>
