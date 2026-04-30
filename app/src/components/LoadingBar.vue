30854c6a391a9c407e3e75a12d681b9800a86558
Dirk <dirkdevbusiness@gmail.com>
2026-04-30 11:00:07 +0200

    Update loading label to show default text

--- app/src/components/LoadingBar.vue
+++ app/src/components/LoadingBar.vue
@@ -8,7 +8,7 @@ defineProps<Props>();
 <template>
     <div class="flex w-full flex-col items-center gap-4">
         <div class="text-2xl font-semibold text-zinc-500 dark:text-slate-400 max-lg:text-xl">
-            {{ label }}
+            {{ label ? label : 'Loading...' }}
         </div>
         <div
             class="relative h-3 w-4/5 max-w-md overflow-hidden rounded-full bg-zinc-200 dark:bg-slate-700"
