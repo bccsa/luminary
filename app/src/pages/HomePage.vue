7819876c9b91b00f7da115b4a38c0be8e38a6363
Dirk <dirkdevbusiness@gmail.com>
2026-05-12 17:29:32 +0200

    APP: Add search shortcute to homepage (#1609)
    * Add search component to homepage
    
    Introduces a new `HomePageSearch` component that displays a search bar
    on the homepage. The component includes an animated pulse effect on
    first load to draw user attention. It also supports keyboard shortcuts
    for opening the search overlay. The search heading and hint are now
    localized.
    
    * Remove redundant heading from search
    
    * Refactor: Rename search pulse to shimmer
    
    Changed the animation name from `pulse-once` to `shimmer-once` and
    updated the related CSS and JavaScript to reflect this change. The
    animation itself has also been adjusted to provide a more subtle shimmer
    effect.

--- app/src/pages/HomePage.vue
+++ app/src/pages/HomePage.vue
@@ -5,11 +5,14 @@ import HomePageNewest from "@/components/HomePage/HomePageNewest.vue";
 import BasePage from "@/components/BasePage.vue";
 import ContinueWatching from "@/components/HomePage/ContinueWatching.vue";
 import ContinueListening from "@/components/HomePage/ContinueListening.vue";
+import HomePageSearch from "@/components/HomePage/HomePageSearch.vue";
+import { isMdScreen } from "@/globalConfig";
 </script>
 
 <template>
     <BasePage>
         <IgnorePagePadding ignoreTop>
+            <HomePageSearch v-if="isMdScreen" />
             <Suspense>
                 <HomePagePinned />
             </Suspense>
