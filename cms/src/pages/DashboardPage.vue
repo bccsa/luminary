46df13c5dbcc8136f17fe782f39e7256e1f4a014
Christian Touoyim <touoyim14christian@gmail.com>
2026-05-20 09:56:16 +0200

    format the file

--- cms/src/pages/DashboardPage.vue
+++ cms/src/pages/DashboardPage.vue
@@ -412,11 +412,7 @@ const canViewGroups = hasAnyPermission(DocType.Group, AclPermission.View);
             </div>
 
             <!-- Language coverage (mobile only) -->
-            <LCard
-                title="Language coverage"
-                :icon="GlobeEuropeAfricaIcon"
-                class="lg:hidden"
-            >
+            <LCard title="Language coverage" :icon="GlobeEuropeAfricaIcon" class="lg:hidden">
                 <div
                     v-if="cmsLanguages.length === 0"
                     class="py-4 text-center text-sm text-zinc-400"
@@ -443,9 +439,7 @@ const canViewGroups = hasAnyPermission(DocType.Group, AclPermission.View);
                                 {{ contentCountPerLanguage[lang._id] ?? 0 }}
                             </span>
                         </div>
-                        <div
-                            class="mt-0.5 h-1 w-full overflow-hidden rounded-full bg-zinc-100"
-                        >
+                        <div class="mt-0.5 h-1 w-full overflow-hidden rounded-full bg-zinc-100">
                             <div
                                 class="h-full rounded-full transition-all"
                                 :class="
@@ -513,55 +507,56 @@ const canViewGroups = hasAnyPermission(DocType.Group, AclPermission.View);
                             <li
                                 v-for="doc in recentContent"
                                 :key="doc._id"
-                                class="p-1.5 hover:bg-zinc-100 rounded-lg"
+                                class="rounded-lg p-1.5 hover:bg-zinc-100"
                             >
-                            <RouterLink
-                                class="grid grid-cols-[auto_1fr_auto_auto] items-center sm:grid-cols-[20px_1fr_150px_100px_80px] "
-                                :to="parentRoute(doc)!"
-                            >
-                                <component
-                                    :is="
-                                        doc.parentType === DocType.Post ? DocumentTextIcon : TagIcon
-                                    "
-                                    class="h-4 w-4 shrink-0 text-zinc-300"
-                                />
-                                <span v-if="parentRoute(doc)"
-                                 class="min-w-0 truncate text-sm  font-medium text-zinc-900 hover:text-yellow-600">                             
-                                    {{ doc.title || "Untitled" }}
-                                </span>
-                                <span
-                                    v-else
-                                    class="min-w-0 truncate text-sm font-medium text-zinc-900"
+                                <RouterLink
+                                    class="grid grid-cols-[auto_1fr_auto_auto] items-center sm:grid-cols-[20px_1fr_150px_100px_80px]"
+                                    :to="parentRoute(doc)!"
                                 >
-                                    {{ doc.title || "Untitled" }}
-                                </span>
-                                <span
-                                    v-if="doc.author"
-                                    class="hidden truncate text-right text-xs text-zinc-400 sm:col-start-3 sm:inline"
-                                >
-                                    by {{ doc.author }}
-                                </span>
-                                <div v-else class="hidden sm:col-start-3 sm:block"></div>
-                                <span
-                                    class="text-right text-xs text-zinc-400 sm:col-start-4"
-                                >
-                                    {{ formatRelativeTime(doc.updatedTimeUtc) }}
-                                </span>
-                                <LBadge
-                                    :variant="
-                                        doc.status === PublishStatus.Published
-                                            ? 'success'
-                                            : 'default'
-                                    "
-                                    paddingY="py-0.5"
-                                    paddingX="px-1.5"
-                                    class="justify-self-end sm:col-start-5"
-                                >
-                                    {{ doc.status }}
-                                </LBadge>
-                            </RouterLink>
+                                    <component
+                                        :is="
+                                            doc.parentType === DocType.Post
+                                                ? DocumentTextIcon
+                                                : TagIcon
+                                        "
+                                        class="h-4 w-4 shrink-0 text-zinc-300"
+                                    />
+                                    <span
+                                        v-if="parentRoute(doc)"
+                                        class="min-w-0 truncate text-sm font-medium text-zinc-900 hover:text-yellow-600"
+                                    >
+                                        {{ doc.title || "Untitled" }}
+                                    </span>
+                                    <span
+                                        v-else
+                                        class="min-w-0 truncate text-sm font-medium text-zinc-900"
+                                    >
+                                        {{ doc.title || "Untitled" }}
+                                    </span>
+                                    <span
+                                        v-if="doc.author"
+                                        class="hidden truncate text-right text-xs text-zinc-400 sm:col-start-3 sm:inline"
+                                    >
+                                        by {{ doc.author }}
+                                    </span>
+                                    <div v-else class="hidden sm:col-start-3 sm:block"></div>
+                                    <span class="text-right text-xs text-zinc-400 sm:col-start-4">
+                                        {{ formatRelativeTime(doc.updatedTimeUtc) }}
+                                    </span>
+                                    <LBadge
+                                        :variant="
+                                            doc.status === PublishStatus.Published
+                                                ? 'success'
+                                                : 'default'
+                                        "
+                                        paddingY="py-0.5"
+                                        paddingX="px-1.5"
+                                        class="justify-self-end sm:col-start-5"
+                                    >
+                                        {{ doc.status }}
+                                    </LBadge>
+                                </RouterLink>
                             </li>
-
                         </ul>
                     </LCard>
 
@@ -703,31 +698,29 @@ const canViewGroups = hasAnyPermission(DocType.Group, AclPermission.View);
                                     <li
                                         v-for="item in missingTranslations"
                                         :key="item.parentId"
-                                        class="hover:bg-zinc-100 rounded-lg p-1.5"
+                                        class="rounded-lg p-1.5 hover:bg-zinc-100"
                                     >
-                                    <RouterLink
-                                        
-                                        :to="parentRoute(item)!"
-                                        class="min-w-0 truncate text-sm text-zinc-900 hover:text-yellow-600"
-                                    >
-                                        <div class="flex items-center justify-between gap-2">
-                                            <span v-if="parentRoute(item)">
-
-                                                {{ item.title }}
-                                            </span>
+                                        <RouterLink
+                                            :to="parentRoute(item)!"
+                                            class="min-w-0 truncate text-sm text-zinc-900 hover:text-yellow-600"
+                                        >
+                                            <div class="flex items-center justify-between gap-2">
+                                                <span v-if="parentRoute(item)">
+                                                    {{ item.title }}
+                                                </span>
                                                 <span
-                                                v-else
-                                                class="min-w-0 truncate text-sm text-zinc-900"
+                                                    v-else
+                                                    class="min-w-0 truncate text-sm text-zinc-900"
                                                 >
-                                                {{ item.title }}
-                                            </span>
-                                            <span
-                                            class="shrink-0 rounded-full bg-amber-100 px-2 py-0.5 text-xs font-medium text-amber-700"
-                                            >
-                                            {{ item.translated }}/{{ item.total }}
-                                        </span>
-                                    </div>
-                                </RouterLink>
+                                                    {{ item.title }}
+                                                </span>
+                                                <span
+                                                    class="shrink-0 rounded-full bg-amber-100 px-2 py-0.5 text-xs font-medium text-amber-700"
+                                                >
+                                                    {{ item.translated }}/{{ item.total }}
+                                                </span>
+                                            </div>
+                                        </RouterLink>
                                     </li>
                                 </ul>
                             </div>
