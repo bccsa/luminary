8ffca46a7ca949a27abe9aeb0c94ddd70f56fa94
Christian Touoyim <touoyim14christian@gmail.com>
2026-07-30 17:13:24 +0200

    Enhance dark mode support across components with improved styling and color adjustments

--- cms/src/components/common/LBadge.vue
+++ cms/src/components/common/LBadge.vue
@@ -1,13 +1,28 @@
 <script lang="ts">
 export const variants = {
-    default: "bg-zinc-100 text-zinc-600 ring-zinc-200",
-    blue: "bg-blue-50 text-zinc-600 ring-zinc-200",
-    success: "bg-green-100 text-green-700 ring-green-200",
-    warning: "bg-yellow-100 text-yellow-800 ring-yellow-300",
-    error: "bg-red-100 text-red-700 ring-red-200",
-    info: "bg-blue-100 text-blue-700 ring-blue-200",
-    scheduled: "bg-purple-100 text-purple-700 ring-purple-200",
-    expired: "bg-cyan-100 text-cyan-700 ring-cyan-200",
+    default: `bg-zinc-100 text-zinc-600 ring-zinc-200 
+              dark:bg-zinc-800 dark:text-zinc-300 dark:ring-zinc-700`,
+
+    blue: `bg-blue-50 text-zinc-600 ring-zinc-200 
+           dark:bg-blue-900/30 dark:text-blue-200 dark:ring-blue-800`,
+
+    success: `bg-green-100 text-green-700 ring-green-200 
+              dark:bg-green-900/40 dark:text-green-300 dark:ring-green-800`,
+
+    warning: `bg-yellow-100 text-yellow-800 ring-yellow-300 
+              dark:bg-yellow-900/40 dark:text-yellow-200 dark:ring-yellow-800`,
+
+    error: `bg-red-100 text-red-700 ring-red-200 
+            dark:bg-red-900/40 dark:text-red-200 dark:ring-red-800`,
+
+    info: `bg-blue-100 text-blue-700 ring-blue-200 
+           dark:bg-indigo-900/40 dark:text-indigo-200 dark:ring-indigo-800`,
+
+    scheduled: `bg-purple-100 text-purple-700 ring-purple-200 
+                dark:bg-purple-900/40 dark:text-purple-200 dark:ring-purple-800`,
+
+    expired: `bg-cyan-100 text-cyan-700 ring-cyan-200 
+              dark:bg-cyan-900/40 dark:text-cyan-200 dark:ring-cyan-800`,
 };
 </script>
 
@@ -77,7 +92,11 @@ const defaultIcon = computed(() => {
             },
         ]"
     >
-        <component :is="icon || defaultIcon" class="-ml-0.5 mr-1 h-3 w-3 shrink-0" v-if="withIcon" />
+        <component
+            :is="icon || defaultIcon"
+            class="-ml-0.5 mr-1 h-3 w-3 shrink-0 opacity-80"
+            v-if="withIcon"
+        />
         <slot />
     </span>
 </template>
