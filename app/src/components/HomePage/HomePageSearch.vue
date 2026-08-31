fae81bb14f8c14259fb90802f27f869d29df2c50
Christian Touoyim <touoyim14christian@gmail.com>
2026-08-31 07:43:37 +0100

    fix(HomePageSearch): add missing bottom padding to search bar on mobile screen

--- app/src/components/HomePage/HomePageSearch.vue
+++ app/src/components/HomePage/HomePageSearch.vue
@@ -37,7 +37,7 @@ onMounted(() => {
 </script>
 
 <template>
-    <section class="bg-yellow-500/10 px-4 pt-2 dark:bg-yellow-500/5">
+    <section class="bg-yellow-500/10 px-4 py-2 dark:bg-yellow-500/5">
         <div class="mx-auto max-w-xl">
             <button
                 type="button"
