42055c44d63176118129ed4b5ae69879667dce21
Bruce <brucefrankstad26@gmail.com>
2026-09-04 08:50:30 +0200

    Requested Changes

--- cms/src/components/content/EditContent.vue
+++ cms/src/components/content/EditContent.vue
@@ -449,7 +449,7 @@ watch(isLgScreen, (isLg) => {
                 <LDropdown
                     v-model:show="showQuickLang"
                     placement="bottom-start"
-                    panel-class="!w-40 mt-1"
+                    panel-class="!w-40 mt-2"
                 >
                     <template #trigger>
                         <button
